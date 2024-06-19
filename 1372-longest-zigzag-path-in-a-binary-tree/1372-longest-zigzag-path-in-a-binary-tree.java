fc9e87fc41585d306640930539cb231e69374f02
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-06-18 18:00:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -14,27 +14,28 @@
  * }
  */
 class Solution {
-    int res = 0;
+    int res = 0
 
     public int longestZigZag(TreeNode root) {
-        DFS(root, false, 0);
         DFS(root, true, 0);
+        DFS(root, false, 0);
         return res;
-    }   
+    }
 
     public void DFS(TreeNode node, boolean goLeft, int count) {
         if (node == null) {
             return;
         }
 
+        res = Math.max(res, count);
+
         if (goLeft) {
-            DFS(node.left, false, count+1);
+            DFS(node.left,false, count+1);
             DFS(node.right, true, 1);
         } else {
             DFS(node.right, true, count+1);
             DFS(node.left, false, 1);
         }
 
-        res = Math.max(res, count);
     }
 }
