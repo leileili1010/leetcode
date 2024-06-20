a5735a60bcbe7e4195cb88fa2d6a36290ce42188
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-06-20 10:05:19 -0700

    Time: 1 ms (98.89%), Space: 41.7 MB (17.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        
+        Deque<Character> stack = new ArrayDeque<>();
+        int idx = 0;
+
+        while (idx < s.length()) {
+            char c = s.charAt(idx);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char ch = stack.pop();
+                if (ch != '(' && c == ')') return false;
+                if (ch != '[' && c == ']') return false;
+                if (ch != '{' && c == '}') return false;
+            }
+            idx++;
+        }
+
+        return stack.isEmpty();
+    }
+}
