6f25d3a99501b368c94e7c150929a9d3238bd6f2
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-06-25 20:18:51 -0700

    Time: 2 ms (50.05%), Space: 43.8 MB (86.94%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         Deque<Integer> que = new ArrayDeque<>();
         Set<Integer> visited = new HashSet<>();
-
-        que.offer(0);
         visited.add(0);
+        que.offer(0);
 
         while (!que.isEmpty()) {
-            int cur = que.poll();
-            
-            for (int v: rooms.get(cur)) {
-                if (visited.add(v)) {
-                    que.offer(v);
+            int curr = que.poll();
+
+            for (int num: rooms.get(curr)) {
+                if (visited.add(num)) {
+                    que.offer(num);
                 }
             }
         }
+
         return visited.size() == rooms.size();
     }
 }
