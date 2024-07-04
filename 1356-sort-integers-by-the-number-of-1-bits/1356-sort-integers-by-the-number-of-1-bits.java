2ebae6390a87439e7078942adb7dbeeb02315b89
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-04 11:22:48 -0700

    Time: 10 ms (55.7%), Space: 44.6 MB (52.59%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -20,12 +20,12 @@ class Solution {
         });
         
         // Convert sorted list back to array
-        int[] sortedArray = new int[arr.length];
+    
         for (int i = 0; i < arr.length; i++) {
-            sortedArray[i] = list.get(i);
+            arr[i] = list.get(i);
         }
         
-        return sortedArray;
+        return arr;
     }
     
     // Helper function to count number of 1 bits in an integer
