d042ce4a38b218c1a131f61a6ebbc1a32bfde265
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-03 20:21:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+
+        for ( int i = 0; i < ans.length; i++) {
+            ans[i] = bits(i);
+        }
+        return ans;
+    }
+
+    pulic int bits(int n) {
+        int count = 0;
+        
+        while (n != 0) {
+            if ( n % 2 == 1) {
+                count++;
+            }
+
+            n = n / 2;
+        }
+
+        return count;
+    }
+}
