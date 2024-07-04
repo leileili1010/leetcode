59b9422c98fc4bcb3c87adc2506d23197efd14b7
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-04 11:19:38 -0700

    Time: 10 ms (55.7%), Space: 44.3 MB (93.55%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -3,7 +3,28 @@ import java.util.*;
 class Solution {
     public int[] sortByBits(int[] arr) {
         // Define a custom comparator
-        Comparator<Integer> comparator = new Comparator<Integer>() {
+        // Comparator<Integer> comparator = new Comparator<Integer>() {
+        //     @Override
+        //     public int compare(Integer a, Integer b) {
+        //         int countA = countBits(a);
+        //         int countB = countBits(b);
+                
+        //         if (countA != countB) {
+        //             return countA - countB; // Sort by number of 1 bits
+        //         } else {
+        //             return a - b; // If counts are the same, sort by value
+        //         }
+        //     }
+        // };
+        
+        // Convert array to list for sorting with custom comparator
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            list.add(num);
+        }
+        
+        // Sort the list using the custom comparator
+        Collections.sort(list, new Comparator<Integer>(){
             @Override
             public int compare(Integer a, Integer b) {
                 int countA = countBits(a);
@@ -15,16 +36,7 @@ class Solution {
                     return a - b; // If counts are the same, sort by value
                 }
             }
-        };
-        
-        // Convert array to list for sorting with custom comparator
-        List<Integer> list = new ArrayList<>();
-        for (int num : arr) {
-            list.add(num);
-        }
-        
-        // Sort the list using the custom comparator
-        Collections.sort(list, comparator);
+        });
         
         // Convert sorted list back to array
         int[] sortedArray = new int[arr.length];
