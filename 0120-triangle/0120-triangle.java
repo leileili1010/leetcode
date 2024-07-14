38e19d3e75c901686205d595b65d8c842a0f3263
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-14 10:26:21 -0700

    Time: 3 ms (64.91%), Space: 43.8 MB (78.57%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,22 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
+        int n = triangle.size(); // n =4;
         int[][] dp = new int[n][];
 
-        // dp代表状态
         for (int i = 0; i < n; i++) {
             dp[i] = new int[i+1];
         }
 
-        // 初始化： 终点
-        for (int i = 0; i < n; i++) {
-            dp[n-1][i] = triangle.get(n-1).get(i);
+        for (int j = 0; j < n; j++) {
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
 
-        for (int i = n-2; i >=0; i--) {
-            for (int j = 0; j <= i; j++) {
-                //方程：到哪去
-                dp[i][j] = triangle.get(i).get(j)+ Math.min(dp[i+1][j], dp[i+1][j+1]);
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < i+1; j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
 
-        // 答案： 起点
         return dp[0][0];
     }
 }
