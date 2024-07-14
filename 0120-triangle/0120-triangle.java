814e67507b2f348d339e08a16b18e720794d353e
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-14 12:21:30 -0700

    Time: 3 ms (64.91%), Space: 43.3 MB (91.11%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,22 +1,35 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size(); // n =4;
+        int n = triangle.size();
         int[][] dp = new int[n][];
 
+        // state
         for (int i = 0; i < n; i++) {
             dp[i] = new int[i+1];
         }
 
-        for (int j = 0; j < n; j++) {
-            dp[n-1][j] = triangle.get(n-1).get(j);
-        }
+        dp[0][0] = triangle.get(0).get(0);
 
-        for (int i = n-2; i >= 0; i--) {
-            for (int j = 0; j < i+1; j++) {
-                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+        //function...
+        for (int i = 1; i < n; i++) {
+            // leftmost
+            dp[i][0] = triangle.get(i).get(0) + dp[i-1][0];
+            
+            for (int j = 1; j < i; j++) {
+                // middle
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j-1], dp[i-1][j]);
             }
+            
+            // rightmost
+            dp[i][i] = triangle.get(i).get(i) + dp[i-1][i-1];
         }
 
-        return dp[0][0];
+        // answer
+        int min = dp[n-1][0];
+        for (int i = 1; i < n; i++) {
+            min = Math.min(min, dp[n-1][i]);
+        }
+
+        return min;
     }
 }
