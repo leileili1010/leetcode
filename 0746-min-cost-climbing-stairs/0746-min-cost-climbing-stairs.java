68665518da755ef3b4fa1bbcf4286fed8b38330e
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-14 13:31:00 -0700

    Time: 0 ms (100%), Space: 42.9 MB (53.44%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        // The array's length should be 1 longer than the length of cost
-        // This is because we can treat the "top floor" as a step to reach
-        int minimumCost[] = new int[cost.length + 1];
-        
-        // Start iteration from step 2, since the minimum cost of reaching
-        // step 0 and step 1 is 0
-        for (int i = 2; i < minimumCost.length; i++) {
-            int takeOneStep = minimumCost[i - 1] + cost[i - 1];
-            int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
-            minimumCost[i] = Math.min(takeOneStep, takeTwoSteps);
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        dp[n-1] = cost[n-1];
+
+        for (int i = n-2; i >= 0; i--) {
+            dp[i] = cost[i] + Math.min(dp[i+1], dp[i+2]);
         }
-        
-        // The final element in minimumCost refers to the top floor
-        return minimumCost[minimumCost.length - 1];
+
+        return Math.min(dp[0], dp[1]);
+
     }
 }
