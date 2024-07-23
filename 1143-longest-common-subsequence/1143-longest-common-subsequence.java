cf8a11349ddd5edc8381d7d7212856ced523daea
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-07-23 11:31:42 -0700

    Time: 19 ms (87.69%), Space: 51.1 MB (12.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,21 +3,20 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
 
-        // state
-        int[][] dp = new int[n + 1][m + 1];
+        // state dp[i][j] means length of LCS between first i characters of text1 and  first j characters of text2.
+        int[][] dp = new int[n+1][m+1];
 
         // function
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
 
-        // answer
         return dp[n][m];
     }
 }
