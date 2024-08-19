8c049efcc8b6a4d719d8b4f024aa398cb36c5489
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-08-19 11:09:21 -0700

    Time: 2 ms (99.11%), Space: 42.8 MB (75.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,31 +3,29 @@ class Solution {
         int left = 0, right = s.length()-1;
 
         while (left < right) {
-           while (left < right && !isValid(s, left)) {
+            while (left < right && !isValid(left, s)) {
                 left++;
-           } 
+            } 
 
-           while (left < right && !isValid(s, right)) {
+            while (left < right && !isValid(right, s)) {
                 right--;
-           }
+            }
 
-           if (!isEqual(left, right, s)) {
+            if (!isEqual(left, right, s)) {
                 return false;
-           }
-           left++;
-           right--;
-        }
+            }
 
+            left++;
+            right--;
+        }
         return true;
     }
 
-    public boolean isValid(String s, int idx) {
+    public boolean isValid(int idx, String s) {
         return Character.isLetterOrDigit(s.charAt(idx));
     }
 
     public boolean isEqual(int left, int right, String s) {
-        char char1 = Character.toLowerCase(s.charAt(left));
-        char char2 = Character.toLowerCase(s.charAt(right));
-        return char1 == char2;
+        return Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right));
     }
 }
