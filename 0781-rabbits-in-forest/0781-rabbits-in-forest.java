f4bc40a285d3b7eeaf8de0d203216359f8f03209
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-11-11 19:11:08 -0800

    Time: 3 ms (61.78%), Space: 41.9 MB (90.15%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.java
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.java
@@ -4,16 +4,19 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int ans: answers) {
-            int n = ans+1;
-            if (!map.containsKey(n) || map.get(n) < n) {
-                map.put(n, map.getOrDefault(n, 0)+1);
-            } else {
-                map.put(n, 1);
-            }
+           map.put(ans, map.getOrDefault(ans, 0)+1);
+        }
 
-            if (map.get(n) == 1) {
-                count += n;
+        for (HashMap.Entry<Integer, Integer> e: map.entrySet()) {
+            int numPerGroup = e.getKey()+1;
+            int replyCnt = e.getValue();
+            int numOfGroup = replyCnt / numPerGroup;
+           
+            if (replyCnt % numPerGroup != 0) {
+                numOfGroup++;
             }
+            
+            count += numPerGroup * numOfGroup;
         }
 
         return count;
