7c448f103aae4f8942662a321b4f6125a555f945
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-11-11 21:47:02 -0800

    Time: 2 ms (94.92%), Space: 43 MB (27.54%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.java
+++ 0734-sentence-similarity/0734-sentence-similarity.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean areSentencesSimilar(String[] sentence1, String[] sentence2, List<List<String>> similarPairs) {
+        // Per constraints, both are non-empty String[], so we dont have to consider edge case null;
         if (sentence1.length != sentence2.length) {
             return false;
         }
         
+        // value of HashMap could be set
         HashMap<String, HashSet<String>> map = new HashMap<>();
 
        for (List<String> pair: similarPairs) {
             String word1 = pair.get(0);
             String word2 = pair.get(1);
             
-            map.putIfAbsent(word1, new HashSet<>());
+            // putifabsent does nothing if key already exists
+            map.putIfAbsent(word1, new HashSet<>()); // only initiate hashset if word1 does not exist as key, or do nothing
             map.get(word1).add(word2);
 
             map.putIfAbsent(word2, new HashSet<>());
@@ -21,6 +24,8 @@ class Solution {
             String word1 = sentence1[i];
             String word2 = sentence2[i];
 
+            // for immutable type, use "=="
+            // for object, "==" compares reference and equals() compares content
             if (word1.equals(word2)) {
                 continue;
             }
