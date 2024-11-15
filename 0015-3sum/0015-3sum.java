674acdca8afa018e65e92f367c87d0f3904a91c0
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-11-14 21:59:17 -0800

    Time: 25 ms (95.68%), Space: 51.2 MB (75.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-	// 找出a + b + c = 0
-        // a = nums[i], b = nums[left], c = nums[right]
-        for (int i = 0; i < nums.length; i++) {
-	    // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
-            if (nums[i] > 0) { 
-                return result;
+
+       for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return res;
             }
 
-            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
+            if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
 
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (right > left) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
+            int left = i+1, right = n-1, target = 0-nums[i];
+            while (left < right) {
+                if (nums[left] + nums[right] < target) {
                     left++;
+                } else if (nums[left] + nums[right] > target) {
+                    right--;
                 } else {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-		    // 去重逻辑应该放在找到一个三元组之后，对b 和 c去重
-                    while (right > left && nums[right] == nums[right - 1]) right--;
-                    while (right > left && nums[left] == nums[left + 1]) left++;
-                    
-                    right--; 
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    right--;
                     left++;
                 }
             }
-        }
-        return result;
+       }
+        return res;
     }
 }
