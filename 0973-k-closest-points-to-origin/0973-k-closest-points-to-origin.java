a134c37370270048a329ee26210a30e27c5039d7
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-11-14 21:08:05 -0800

    Time: 23 ms (77.99%), Space: 53.1 MB (46.32%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,11 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> squareDistance(a) - squareDistance(b));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int[][] res = new int[k][2];
 
+        for (int i = 0; i < points.length; i++) {
+            int distance = squareDistance(points[i]);
+            int[] pair = {distance, i};
+            minHeap.add(pair);
+        }
+        
+        for (int i = 0; i < k; i++) {
+            int[] pair = minHeap.poll();
+            int idx = pair[1];
+            res[i] = points[idx]; 
+        }
 
-        return Arrays.copyOf(points, k);
-
+        return res;
+        
     }
+
     private int squareDistance(int[] point) {
         return point[0] * point[0] + point[1] * point[1];
     }
