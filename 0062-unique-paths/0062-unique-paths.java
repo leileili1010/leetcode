f264ad6978bfa2765f7118bc533eb7f613f837d6
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-11-16 08:39:11 -0800

    Time: 0 ms (100%), Space: 40.4 MB (38.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // state, dp[i][j] means No.of paths
+        // definiation: dp[i][j] means sum of unique path to cur cell        
         int[][] dp = new int[m][n];
+        dp[0][0] = 1;
 
-        // initialize row 0 and col 0;
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
+        // initialization
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = 1;
         }
 
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
         }
 
         // function
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-
-        // answer
         return dp[m-1][n-1];
     }
 }
