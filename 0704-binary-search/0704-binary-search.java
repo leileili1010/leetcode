778329a955109ee7e0389732ca1640547e70bbbd
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-06 23:53:50 -0800

    Time: 2 ms (11.29%), Space: 45.5 MB (67.55%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,14 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
         Arrays.sort(nums);
-        int left = 0, right = nums.length-1;
+        int left = 0, right = nums.length;
         
         while (left < right) {
             int mid = (left + right) >>> 1;
             if (target == nums[mid]) {
                 return mid;
             } else if (target < nums[mid]) {
-                right = mid-1;
+                right = mid;
             } else {
                 left = mid+1;
             }
