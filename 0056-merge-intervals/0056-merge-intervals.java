e5d5309b645670fed303d98b3159044fb6658c75
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-06 22:53:55 -0800

    Time: 8 ms (87.97%), Space: 47 MB (8.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,25 +5,11 @@ class Solution {
             return intervals;
         }
 
-        // Arrays.sort(intervals, new Comparator<int[]>() {
-        //     @Override
-        //     public int compare(int[] a, int[] b) {
-        //         if (a[0] == b[0]) {
-        //         return Integer.compare(a[1], b[1]);
-        //     }
-        //     return Integer.compare(a[0], b[0]);
-        // }
-
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-
         int start = intervals[0][0], end = intervals[0][1];
 
         for (int[] interval: intervals) {
-            if (interval[1] < start) {
-                res.add(interval);
-                start = interval[0];
-                end = interval[1];
-            } else if (interval[0] > end) {
+            if (interval[0] > end) {
                 res.add(new int[] {start, end});
                 start = interval[0];
                 end = interval[1];
