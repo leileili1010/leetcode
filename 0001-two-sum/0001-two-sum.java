3c1367f9e54add8004e121a249dacbab465d2789
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-11 06:47:33 -0800

    Time: 10 ms (50.78%), Space: 45.1 MB (14.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Arrays.sort(nums);
-       int left = 0;
-       int right = nums.length-1;
+        List<int[]> list = new ArrayList<>();
 
-       while (left < right) {
-         if (nums[left] + nums[right] > target) {
-            right--;
-         } else if (nums[left] + nums[right] < target) {
-            left++;
-         } else {
-            return new int[] {left, right};
-         }
-       }
-       return new int[] {left, right};
+        for (int i = 0; i < nums.length; i++) {
+            list.add(new int[] {nums[i], i});
+        }
+
+        Collections.sort(list, (a, b) -> (a[0] - b[0]));
+
+        int i = 0, j = nums.length-1;
+ 
+        while (i < j) {
+            int num1 = list.get(i)[0];
+            int num2 = list.get(j)[0];
+
+            if (num1 + num2 > target) {
+                j--;
+            } else if (num1 + num2 < target) {
+                i++;
+            } else {
+                return new int[] {list.get(i)[1], list.get(j)[1]};
+            } 
+
+        }
+        return new int[] {-1,-1};
+
     }
 }
