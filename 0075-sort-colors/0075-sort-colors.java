56c1e71e299b33d6c13d7ce0d8fd27a97ba2dcc6
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-14 08:19:36 -0800

    Time: 1 ms (22.02%), Space: 41.9 MB (56.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,20 @@
 class Solution {
-    /*
-  Dutch National Flag problem solution.
-  */
     public void sortColors(int[] nums) {
-        // For all idx < i : nums[idx < i] = 0
-        // j is an index of elements under consideration
-        int p0 = 0, curr = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
 
-        // For all idx > k : nums[idx > k] = 2
-        int p2 = nums.length - 1;
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
 
-        int tmp;
-        while (curr <= p2) {
-            if (nums[curr] == 0) {
-                // Swap p0-th and curr-th elements
-                // i++ and j++
-                tmp = nums[p0];
-                nums[p0++] = nums[curr];
-                nums[curr++] = tmp;
-            } else if (nums[curr] == 2) {
-                // Swap k-th and curr-th elements
-                // p2--
-                tmp = nums[curr];
-                nums[curr] = nums[p2];
-                nums[p2--] = tmp;
-            } else curr++;
+        for (int i = 0; i <= 2; i++) {
+            if (map.containsKey(i)) {
+                int count = map.get(i);
+                while (count > 0) {
+                    nums[idx++] = i;
+                    count--;
+                }
+            }
         }
     }
 }
