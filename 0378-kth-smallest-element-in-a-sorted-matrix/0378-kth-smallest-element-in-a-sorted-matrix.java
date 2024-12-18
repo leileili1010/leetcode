821cf50f97b797858f3d95616f11a07f0fcd3929
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-17 17:14:37 -0800

    Time: 0 ms (100%), Space: 49.5 MB (38.62%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,18 +1,34 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityBlockingQueue<Integer> que = new PriorityBlockingQueue<>();
-        
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                que.offer(matrix[i][j]);
+        int n = matrix.length;
+        int low = matrix[0][0], high = matrix[n-1][n-1];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = countLessEqual(matrix, mid);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
+        return low;
+    }
 
-        int res = 0;
-        for (int i = 0; i < k; i++) {
-            res = que.poll();
+    private int countLessEqual(int[][] matrix, int target) {
+        int n = matrix.length;
+        int count = 0;
+        int row = n - 1, col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= target) {
+                count += row + 1; // All elements in this column are <= target
+                col++;
+            } else {
+                row--;
+            }
         }
-
-        return res;
+        return count;
     }
 }
