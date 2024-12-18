573bfdf80e756511566a1761984da39341bc18d8
Fiona(Lei) Li <143532361+leileili1010@users.noreply.github.com>
2024-12-17 20:50:14 -0800

    Time: 25 ms (72.51%), Space: 40.5 MB (36.75%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,12 +5,12 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1, right = n;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right-left)/2;
             if (!isBadVersion(mid)) {
                left = mid+1;
             } else {
-                right = mid;
+                right = mid-1;
             }
         }
 
