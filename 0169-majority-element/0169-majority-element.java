7acf14fe40815143f605d9677edc21feba559dd6
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2024-12-21 15:33:31 -0800

    Time: 3 ms (58.82%), Space: 53 MB (47.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        int count = 0;
+        Integer candidate = null;
+        
         for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0)+1);
-            if (map.get(num) > nums.length/2) {
-                return num;
-                
-            } 
+            if(count == 0) {
+                candidate = num;
+            }
+
+            count += candidate == num? 1: -1;
         }
 
-        return 0;
+        return candidate;
     }
 }
