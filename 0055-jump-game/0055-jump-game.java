555e59d3ebf59892b5a4ceee58aacb6c330fa47f
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2024-12-27 22:16:36 -0800

    Time: 674 ms (5.01%), Space: 45.5 MB (82.3%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        
-        // state, dp[i] means if we can reach dp[i] from dp[0]
         boolean[] dp = new boolean[n];
         dp[0] = true;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && j + nums[j] >= i) {
+                if (dp[j] && nums[j] + j >= i) {
                     dp[i] = true;
                     break;
                 }
             }
         }
+
         return dp[n-1];
+
     }
 }
