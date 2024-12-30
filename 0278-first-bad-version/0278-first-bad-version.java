a1d844c50d0eafebeac317b1279b5395bbc0d6cc
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2024-12-30 12:45:06 -0800

    Time: 27 ms (15.07%), Space: 40.5 MB (31.5%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,27 +1,22 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        int res = -1;
-        
-        while (start <= end) {
-            int mid = start + (end - start)/2;
+        int start = 1, end = n;
 
+        while (start + 1 < end) {
+            int mid = start + (end - start)/2;
             if (isBadVersion(mid)) {
-                res = mid;
-                end = mid-1;
+                end = mid;
             } else {
-                start = mid+1;
+                start = mid;
             }
         }
 
-        return start;
+        if (isBadVersion(start)) return start;
+        if (isBadVersion(end)) return end;
 
+        return -1;
     }
 }
