51b32ff4986eb6fbec4d40dec2c35e1af6491e79
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2024-12-29 17:14:24 -0800

    Time: 21 ms (38.01%), Space: 45 MB (31.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -6,13 +6,13 @@ class Solution {
 
         // initialzation
         for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE/2);
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
             dp[i][0] = 0;
         }
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j >= coins[i-1]) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i-1] && dp[i][j - coins[i - 1]] != Integer.MAX_VALUE) {
                     dp[i][j] = Math.min(
                         dp[i-1][j], 
                         dp[i][j - coins[i-1]] + 1);
@@ -22,6 +22,6 @@ class Solution {
             }
         }
 
-        return dp[n][amount] == Integer.MAX_VALUE/2? -1: dp[n][amount]; 
+        return dp[n][amount] == Integer.MAX_VALUE? -1: dp[n][amount]; 
     }
 }
