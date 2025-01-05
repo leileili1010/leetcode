8193742f048bbbff575d307cff68c5af76b934c8
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-05 15:55:19 -0800

    Time: 1229 ms (7%), Space: 55.6 MB (79.13%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,53 +1,31 @@
 class Solution {
-    class Job {
-        int startTime;
-        int endTime;
-        int profit;
-
-        public Job (int startTime, int endTime, int profit) {
-            this.startTime = startTime;
-            this.endTime = endTime;
-            this.profit = profit;
-        }
-    }
-
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = profit.length;
-        Job[] jobs = new Job[n];
+        // step 1: 构造jobs 并按照endTime 来sort
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
 
         for (int i = 0; i < n; i++) {
-            Job job = new Job(startTime[i], endTime[i], profit[i]);
-            jobs[i] = job;
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
+        Arrays.sort(jobs, (a,b) -> (a[1] - b[1]));
 
-        Arrays.sort(jobs, (a,b) -> (a.endTime - b.endTime));
-
-        // state:dp[i] means maximum profit achievable by considering the first i jobs in the sorted jobs array
-        // initialization: dp[0] = 0;
+        // step2: dp
+        // state: dp[i] = max profit considering the first i jobs
         int[] dp = new int[n+1];
 
-        // function
-        for (int i = 1; i < n+1; i++) {
-            int k = binarySearch(jobs, i-1, jobs[i-1].startTime);
-            dp[i] = Math.max(dp[i-1], jobs[i-1].profit + dp[k]);
+        for (int i = 1; i <= n; i++) {
+            int start = jobs[i-1][0];
+            int curProfit = jobs[i-1][2];
+            int compatible = -1;
+            for (int j = i - 2; j >= 0; j--) {
+                if (jobs[j][1] <= start) {
+                    compatible = dp[j+1];
+                    break;
+                }
+            }
+            dp[i] = Math.max(dp[i - 1], curProfit +(compatible == -1? 0: compatible));
         }
 
-        // answer
         return dp[n];
-    }
-
-    private int binarySearch(Job[] jobs, int right, int target) {
-        int left = 0;
-
-        while (left < right) {
-            int mid = left + (right - left)/2;
-            if (jobs[mid].endTime > target) {
-                right = mid;
-            } else {
-                left = mid+1;
-            }
-        } 
-        
-        return left;
     }
 }
