da5d47d6dc23de81b471899610aa19a28b532276
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-04 17:19:27 -0800

    Time: 4 ms (51.73%), Space: 48.9 MB (91.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,38 +4,38 @@ class Solution {
     int[][] steps = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int numIslands(char[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
         int count = 0;
-        
+        rows = grid.length; 
+        cols = grid[0].length; 
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
                     count++;
                     bfs(grid, i, j);
-                } 
+                }
             }
         }
 
         return count;
     }
 
-    public void bfs(char[][] grid, int i, int j) {
+    private void bfs(char[][] grid, int i, int j) {
         Deque<int[]> que = new ArrayDeque<>();
         que.offer(new int[] {i, j});
         grid[i][j] = 0;
 
         while (!que.isEmpty()) {
-            int[] node = que.poll();
-            int row = node[0];
-            int col = node[1];
+            int[] island = que.poll();
+            int row = island[0];
+            int col = island[1];
 
             for (int[] step: steps) {
                 int newRow = row + step[0];
                 int newCol = col + step[1];
-                if ( 0 <= newRow && newRow < rows && 0 <= newCol && newCol < cols && grid[newRow][newCol] == '1') {
-                   que.offer(new int[]{newRow, newCol});
-                   grid[newRow][newCol] = '0';
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == '1') {
+                    que.offer(new int[] {newRow, newCol});
+                    grid[newRow][newCol] = 0;
                 }
             }
         }
