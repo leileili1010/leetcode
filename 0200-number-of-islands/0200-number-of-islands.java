cb083ce58a7c4536bc75b3c7456b9dda9a54c227
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-11 10:53:08 -0800

    Time: 4 ms (51.5%), Space: 50.1 MB (27.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,18 +1,18 @@
 class Solution {
-    int rows;
-    int cols;
-    int[][] steps = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int n;
+    int m;
 
     public int numIslands(char[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
         int count = 0;
-        rows = grid.length; 
-        cols = grid[0].length; 
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
                     count++;
-                    bfs(grid, i, j);
+                    dfs(grid, visited, i, j);
                 }
             }
         }
@@ -20,26 +20,14 @@ class Solution {
         return count;
     }
 
-    private void bfs(char[][] grid, int i, int j) {
-        Deque<int[]> que = new ArrayDeque<>();
-        que.offer(new int[] {i, j});
-        grid[i][j] = 0;
-
-        while (!que.isEmpty()) {
-            int[] island = que.poll();
-            int row = island[0];
-            int col = island[1];
-
-            for (int[] step: steps) {
-                int newRow = row + step[0];
-                int newCol = col + step[1];
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == '1') {
-                    que.offer(new int[] {newRow, newCol});
-                    grid[newRow][newCol] = '0';
-                }
-            }
+    private void dfs(char[][] grid, boolean[][] visited, int row, int col) {
+        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == '0'|| visited[row][col]) {
+            return;
         }
-
-
+        visited[row][col] = true;
+        dfs(grid, visited, row, col+1);
+        dfs(grid, visited, row, col-1);
+        dfs(grid, visited, row+1, col);
+        dfs(grid, visited, row-1, col);
     }
 }
