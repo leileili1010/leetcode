f51e4997cd7ad25940ffdae808ad9879722b2863
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-11 22:49:33 -0800

    Time: 27 ms (56.68%), Space: 55.7 MB (77.44%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,53 +1,47 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        // edge case
         if (n <= 2) {
             List<Integer> res = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++){
                 res.add(i);
             }
             return res;
         }
 
-        // construct graph and inDegree
-        int[] inDegree = new int[n];
         List<List<Integer>> graph = new ArrayList<>();
-        createGraph(graph, inDegree, n, edges);
+        int[] inDegree = new int[n];
+        createGraph(edges, graph, inDegree, n);
 
-        // outter level
         List<Integer> leaves = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (inDegree[i] == 1) leaves.add(i);
-        }
-        
-        // remove outter level
+        } 
+
         while (n > 2) {
             List<Integer> nextLeaves = new ArrayList<>();
-            for (int leave: leaves) {
-                for (int node: graph.get(leave)) {
+            for (int leaf: leaves) {
+                for (int node: graph.get(leaf)) {
                     inDegree[node]--;
-                    if (inDegree[node] == 1) {
-                        nextLeaves.add(node);
-                    }
+                    if (inDegree[node] == 1) nextLeaves.add(node);
                 }
             }
             n -= leaves.size();
             leaves = nextLeaves;
         }
-
+       
         return leaves;
     }
 
-    private void createGraph(List<List<Integer>> graph, int[] inDegree, int n, int[][] edges) {
+    private void createGraph(int[][] edges, List<List<Integer>> graph, int[] inDegree, int n) {
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-
+        
         for (int[] edge: edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
-            inDegree[edge[1]]++;
             inDegree[edge[0]]++;
+            inDegree[edge[1]]++;
         }
     }
 }
