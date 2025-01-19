6a61a3a85f62b7b57743440e5a480e9ab9df33ec
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-19 08:17:30 -0800

    Time: 6 ms (17.48%), Space: 41.9 MB (78.35%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,54 +1,76 @@
+
+
+import static java.util.Collections.indexOfSubList;
+
 class Solution {
     public String alienOrder(String[] words) {
+        Map<Character, Set<Character>> graph = constructGraph(words);
+        if (graph == null) return "";
+        return topologicalSorting(graph);
+    }
+
+    private Map<Character, Set<Character>> constructGraph (String[] words) {
         Map<Character, Set<Character>> graph = new HashMap<>();
-        Map<Character, Integer> indegree = new HashMap<>();
 
         for (String word: words) {
-            for (char c: word.toCharArray()){
-                graph.putIfAbsent(c, new HashSet<>());
-                indegree.putIfAbsent(c, 0);
+            for (char c: word.toCharArray()) {
+                if (!graph.containsKey(c)) {
+                    graph.put(c, new HashSet<>());
+                }
             }
         }
 
         for (int i = 0; i < words.length-1; i++) {
-            String word1 = words[i];
-            String word2 = words[i+1];
-            if (word1.length() > word2.length() && word1.startsWith(word2)) {
-                return "";
+            if (words[i].length() > words[i+1].length() && words[i].startsWith(words[i+1])) {
+                return null;
             }
-
-            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-                char ch1 = word1.charAt(j);
-                char ch2 = word2.charAt(j);
-                if (ch1 != ch2) {
-                    if (!graph.get(ch1).contains(ch2)) {
-                        graph.get(ch1).add(ch2);
-                        indegree.put(ch2, indegree.get(ch2) + 1);
-                    }
+            for (int j = 0; j < Math.min(words[i].length(), words[i+1].length()); j++) {
+                if (words[i].charAt(j) != words[i+1].charAt(j)) {
+                    graph.get(words[i].charAt(j)).add(words[i+1].charAt(j));
                     break;
                 }
             }
         }
 
+     return graph;   
+    }
+
+    private Map<Character, Integer> getIndegree(Map<Character, Set<Character>> graph) {
+        Map<Character, Integer> indegree = new HashMap<>();
+
+        for (char key: graph.keySet()) {
+           indegree.put(key, 0);
+        }
+
+        for (char key: graph.keySet()) {
+            for (char c: graph.get(key)) {
+                indegree.put(c, indegree.get(c)+1);
+            }
+        }
+
+        return indegree;
+    }
+
+    private String topologicalSorting(Map<Character, Set<Character>> graph) {
+        Map<Character, Integer> indegree = getIndegree(graph);
         Deque<Character> que = new ArrayDeque<>();
-        for (char c: indegree.keySet()) {
-            if (indegree.get(c) == 0) que.offer(c);
+
+        for (char key: indegree.keySet()) {
+            if (indegree.get(key) == 0) que.offer(key);
         }
 
         StringBuilder sb = new StringBuilder();
         while (!que.isEmpty()) {
             char cur = que.poll();
             sb.append(cur);
-            for (char c: graph.get(cur)) {
-                indegree.put(c, indegree.get(c)-1);
-                if (indegree.get(c) == 0) que.offer(c);
+            for(char neighbor: graph.get(cur)) {
+                indegree.put(neighbor, indegree.get(neighbor)-1);
+                if (indegree.get(neighbor) == 0) que.offer(neighbor);
             }
         }
 
-         if (sb.length() != indegree.size()) {
-            return "";
-        }
-        
-        return sb.toString();
+        return sb.length() == indegree.size()? sb.toString(): "";
     }
+
+    
 }
