bc956bcd42352c8709289b741f585018efb2b4c7
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-19 21:33:38 -0800

    Time: 27 ms (85.32%), Space: 51.3 MB (81.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-
+        List<List<Integer>> res = new ArrayList<>();
+        
         for (int i = 0; i < nums.length; i++) {
-            // 去重
             if (i > 0 && nums[i] == nums[i-1]) {
-                continue; //只能是continue，如果是i++，那么target也需要更新
+                continue;
             }
-            findTwoSum(nums, i, res);
+            find3Sum(nums, i, res);
         }
         return res;
     }
 
-    public void findTwoSum(int[] nums, int i, List<List<Integer>> res) {
-        int target = -nums[i];
+    public void find3Sum(int[] nums, int i, List<List<Integer>> res) {
         int left = i+1, right = nums.length-1;
+
         while (left < right) {
-            int sum = nums[left] + nums[right];
-            if (sum < target) {
-                left++;
-            } else if (sum > target) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum > 0) {
                 right--;
+            } else if (sum < 0) {
+                left++;
             } else {
-                res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left++], nums[right--])));
-                // 去重，只有找到target的时候才需要去重
-                while (left < right && nums[left] == nums[left-1]) {
-                    left++; // 可以移动左边也可以移动右边
-                }
+                res.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                while (right > left && nums[right] == nums[right + 1]) right--;
+                while (right > left && nums[left] == nums[left - 1]) left++;
             }
+
         }
     }
 }
