a57ccb7d9ba9bc10d4718c00c4ee7c5bae073dc4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-01-19 21:29:52 -0800

    Time: 29 ms (72.88%), Space: 51.4 MB (70.27%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for (int i = 0; i <= n-3; i++) {
-            int target = 0 - nums[i];
-            int left = i+1, right = n-1;
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
-            while (left < right) {
-                if (nums[left] + nums[right] < target) {
-                    left++;
-                } else if (nums[left] + nums[right] > target) {
-                    right--;
-                } else {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left++], nums[right--])));
-                    while (left < right && nums[right] == nums[right+1]) {
-                        right--;
-                    }
-                }
-                
-            }    
-
+        for (int i = 0; i < nums.length; i++) {
+            // 去重
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue; //只能是continue，如果是i++，那么target也需要更新
+            }
+            findTwoSum(nums, i, res);
         }
-
         return res;
+    }
+
+    public void findTwoSum(int[] nums, int i, List<List<Integer>> res) {
+        int target = -nums[i];
+        int left = i+1, right = nums.length-1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left++], nums[right--])));
+                // 去重，只有找到target的时候才需要去重
+                while (left < right && nums[left] == nums[left-1]) {
+                    left++; // 可以移动左边也可以移动右边
+                }
+            }
+        }
     }
 }
