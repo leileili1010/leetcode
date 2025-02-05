b9931fe4e41a05d962abdebb2a2e18aa413ca1c8
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-02-04 21:29:38 -0800

    Time: 1 ms (99.79%), Space: 45.2 MB (36.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums);// 排序才能去重
         dfs(nums, new boolean[nums.length], new ArrayList<Integer>(), res);
         return res;
     }
@@ -13,7 +13,8 @@ class Solution {
         }
         for (int i = 0; i < nums.length; i++) {
             if (visited[i]) continue;
-            if (i > 0 && nums[i] == nums[i-1] && visited[i-1]) continue;
+            if (i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            
             list.add(nums[i]);
             visited[i] = true;
             dfs(nums, visited, list, res);
