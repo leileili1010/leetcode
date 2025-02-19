d4699d65a6959b1883f863d44439e7a7e5bf51a1
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-02-19 12:18:54 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        char[] ch1 = s.toCharArray();
-        char[] ch2 = t.toCharArray();
-        
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
+        int[] chars = new int[26];
 
-        return Arrays.equals(ch1, ch2);
+        int i = 0, j = 0;
 
+        while (i < s.length() && j < t.length()) {
+           chars[s.charAt(i++) - 'a']++;
+           chars[t.charAt(j++)-'a']--;
+        }
+
+        for (int num: chars) {
+            if (num != 0) return false;
+        }
+
+        return true;
     }
 }
