f617c1af742394141627f60c948edcfbe108a399
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-02-19 08:34:55 -0800

    Time: 1 ms (96.61%), Space: 41.8 MB (62.34%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        int i = 0; // Pointer for word
-    int j = 0; // Pointer for abbr
-    int wordLen = word.length();
-    int abbrLen = abbr.length();
+        int i = 0, j = 0;
 
-    while (i < wordLen && j < abbrLen) {
-        if (Character.isDigit(abbr.charAt(j))) {
-            if (abbr.charAt(j) == '0') return false; // Leading zero is invalid
-            int num = 0;
-            while (j < abbrLen && Character.isDigit(abbr.charAt(j))) {
-                num = num * 10 + (abbr.charAt(j) - '0');
+        while (i < word.length() && j < abbr.length()) {
+            if (Character.isDigit(abbr.charAt(j))) {
+                if (abbr.charAt(j) == '0') return false;
+                int num = 0;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    num = num * 10 + (abbr.charAt(j) - '0');
+                    j++;
+                }
+                i += num;
+            } else {
+                if (word.charAt(i) != abbr.charAt(j)) return false;
+                i++;
                 j++;
             }
-            i += num; // Skip 'num' characters in word
-        } else {
-            if (word.charAt(i) != abbr.charAt(j)) return false;
-            i++;
-            j++;
         }
+        return i == word.length() && j == abbr.length();
     }
-    return i == wordLen && j == abbrLen;
-        }
-    
 }
