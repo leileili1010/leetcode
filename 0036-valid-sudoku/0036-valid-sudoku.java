8e588d291e4967941a7d615323abc4c61e63d16b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-02-20 09:46:11 -0800

    Time: 3 ms (54.7%), Space: 44.3 MB (77.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,71 +1,39 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        // rows
         for (int i = 0; i < 9; i++) {
-            if (!isValidRow(board, i)) return false;
-        }
-
-        for (int i = 0; i < 9; i++) {
-            if (!isValidCol(board,i)) return false;
-        }
-
-        int row = 0, col = 0;
-        while (row < 9) {
-            if (!isValidGrid(board, row, col)) return false;
-            col += 3;
-            if (col == 9) {
-                col = 0;
-                row += 3;
+            Set<Character> seen = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (Character.isDigit(board[i][j])) {
+                    if (!seen.add(board[i][j])) return false;
+                }             
             }
         }
 
-        return true;
-
-        
-    }
-
-    private boolean isValidRow(char[][] board, int row) {
-        Set<Character> set = new HashSet<>();
-        
-        for (int i = 0; i < 9; i++) {
-            char ch = board[row][i];
-            
-            if (Character.isDigit(ch)) {
-                if (ch-'0' > 9 || ch-'0' < 1) return false;
-                if (!set.add(ch)) return false;
+        // cols
+        for (int j = 0; j < 9; j++) {
+            Set<Character> seen = new HashSet<>();
+            for (int i = 0; i < 9; i++) {
+                if (Character.isDigit(board[i][j])) {
+                    if (!seen.add(board[i][j])) return false;
+                }             
             }
         }
-        return true;
-    }
 
-    private boolean isValidCol(char[][] board, int col) {
-        Set<Character> set = new HashSet<>();
-        
-        for (int i = 0; i < 9; i++) {
-            char ch = board[i][col];
-            
-            if (Character.isDigit(ch)) {
-                if (ch-'0' > 9 || ch-'0' < 1) return false;
-                if (!set.add(ch)) return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isValidGrid(char[][] board, int row, int col) {
-        Set<Character> set = new HashSet<>();
-        
-        for (int i = row; i < row + 3; i++) {
-            for (int j = col; j < col + 3; j++) {
-                char ch = board[i][j];
-                if (Character.isDigit(ch)) {
-                    if (ch-'0' > 9 || ch-'0' < 1) return false;
-                    if (!set.add(ch)) return false;
+        // square
+        for (int square = 0; square < 9; square++) {
+             Set<Character> seen = new HashSet<>();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square/3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (Character.isDigit(board[row][col])) {
+                    if (!seen.add(board[row][col])) return false;
+                }
                 }
             }
-              
         }
-        
+
         return true;
     }
-
 }
