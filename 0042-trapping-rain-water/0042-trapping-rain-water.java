048edc2bfec714526752bae1aceb8dce21caf20b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-02-23 14:26:36 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+        int res = 0;
+        int left = 0, right = height.length-1;
+        int maxLeft = height[left], maxRight = height[height.length-1];
+
+        while (left < right) {
+            if (maxLeft <= maxRight) {
+                left++;
+                res += maxLeft - height[left] > 0? maxLeft - height[left]: 0;
+                maxLeft = Math.max(maxLeft, height[left]);
+            } else {
+                right--;
+                res += maxRight - height[right] > 0? maxRight - height[right]: 0;
+                maxRight = Math.max(maxRight, height[right]);
+            }
+
+            return res;
+        
+        }
+
+
+    }
+}
