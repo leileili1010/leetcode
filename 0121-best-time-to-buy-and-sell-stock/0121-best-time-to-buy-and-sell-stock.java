2004ba9fe05154e2f9d2c7b0cc97871f7291136c
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-04 21:10:50 -0800

    Time: 23 ms (2.54%), Space: 58.3 MB (97.22%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int slow = 0, fast = 1, max = 0;
+        int n = prices.length;
+        int[][] dp = new int[n][2];
 
-        while (fast < prices.length) {
-            if (prices[fast] > prices[slow]) {
-                int profit = prices[fast] - prices[slow];
-                max = Math.max(max, profit);
-            } else {
-                slow = fast;
-            }
-            fast++;
+        dp[0][0] = 0;
+        dp[0][1] = - prices[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
         }
 
-        return max;
-
-    
+        return dp[n-1][0];
     }
 }
