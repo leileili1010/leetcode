deebed2ab8475f6aabb077a7c44c71562e297cb7
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-04 20:45:22 -0800

    Time: 6 ms (85.83%), Space: 57.3 MB (73.81%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,26 +1,41 @@
 class SparseVector {
-    private Map<Integer, Integer> map;
+    List<int[]> list;
     
     SparseVector(int[] nums) {
-        this.map = new HashMap<>();
+        this.list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                map.put(i, nums[i]);
+                list.add(new int[]{i, nums[i]});
             }
         }
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
-        if (this.map.size() > vec.map.size()) return vec.dotProduct(this);
+        if (this.list.size() > vec.list.size()) return vec.dotProduct(this);
         int res = 0;
-        
-        for (int key: this.map.keySet()) {
-            if (vec.map.containsKey(key)) {
-                res += this.map.get(key) * vec.map.get(key);
-            }
+
+        for (int[] pair: this.list) {
+            int idx = pair[0];
+            int value = pair[1];
+            res += value * binarySearch(vec.list, idx, 0, vec.list.size()-1);
         }
         return res;
+    }
+
+    private int binarySearch(List<int[]> list, int target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            int idx = list.get(mid)[0];
+            if (idx == target) {
+                return list.get(mid)[1];
+            } else if (idx < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
     }
 }
 
