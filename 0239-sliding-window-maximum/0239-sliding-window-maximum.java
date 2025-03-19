eb6d616c1ce92f7e552d18c3555c2d239922a313
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-18 20:56:02 -0700

    Time: 504 ms (5%), Space: 65.6 MB (5.18%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,23 +1,16 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] res = new int[n-k+1];
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> (b[0]-a[0]));
-        
-        for (int i = 0; i < k; i++) {
-            maxHeap.offer(new int[]{nums[i], i});
-        }
-        res[0] = maxHeap.peek()[0];
-
-        for (int i = k; i < n; i++) {
-            maxHeap.offer(new int[]{nums[i], i});
-            while (!maxHeap.isEmpty() && maxHeap.peek()[1] <= i-k) {
-                maxHeap.poll();
-            }
-            res[i-k+1] = maxHeap.peek()[0];
-        }
-
-        return res;
-
-    }
+public int[] maxSlidingWindow(int[] nums, int k) {
+TreeMap<Integer, Integer> map = new TreeMap<>();
+int n = nums.length;
+for(int i=0;i<k;i++) map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+int[] ans = new int[n-k+1];
+ans[0] = map.lastKey();
+for(int i=k;i<n;i++) {
+map.put(nums[i-k], map.get(nums[i-k]) -1);
+if(map.get(nums[i-k])==0) map.remove(nums[i-k]);
+map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+ans[i-k+1] = map.lastKey();
+}
+return ans;
+}
 }
