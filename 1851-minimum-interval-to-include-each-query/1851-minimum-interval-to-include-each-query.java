901fb1485554b922669ecb527b5b4676d5a5bc4b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-22 13:31:27 -0700

    Time: 101 ms (72.11%), Space: 101.5 MB (38.9%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -2,23 +2,23 @@ class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
         int N = queries.length;
         int[] res = new int[N];
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->(a[1] - a[0]) - (b[1] - b[0]));
-        
+
         int[][] queryPairs = new int[N][2];
         for (int i = 0; i < N; i++) {
             queryPairs[i] = new int[] {queries[i], i};
         }
 
-        Arrays.sort(queryPairs, (a,b) -> a[0]-b[0]);
-        Arrays.sort(intervals, (a,b)->a[0]==b[0]? a[1]-b[1]: a[0]-b[0]);
-        
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        Arrays.sort(queryPairs, (a,b) -> a[0] -b[0]);
+
         int i = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> (a[1]-a[0])-(b[1]-b[0]));
+
         for (int[] query: queryPairs) {
             int num = query[0];
             int idx = query[1];
 
             while (i < intervals.length && intervals[i][0] <= num) {
-                // push all intervals starting <= num, whether they end before or after num
                 minHeap.offer(intervals[i]);
                 i++;
             }
@@ -27,9 +27,9 @@ class Solution {
                 minHeap.poll();
             }
 
-            res[idx] = !minHeap.isEmpty()? minHeap.peek()[1] - minHeap.peek()[0] +1: -1;
-           
+            res[idx] = !minHeap.isEmpty()? minHeap.peek()[1]-minHeap.peek()[0]+1: -1;
         }
-        return res;        
+
+        return res;
     }
 }
