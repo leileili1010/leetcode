3f47fe744debb9596267ea1793c6a4357ac3ca3b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-22 13:43:27 -0700

    Time: 0 ms (100%), Space: 46.1 MB (22.77%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -5,9 +5,9 @@ class Solution {
         while (left <= right) {
             int mid = left + (right-left)/2;
             if (nums[mid] < target) {
-                mid = mid+1;
+                left = mid+1;
             } else if (nums[mid] > target) {
-                mid = mid-1;
+                right = mid-1;
             } else {
                 return mid;
             }
