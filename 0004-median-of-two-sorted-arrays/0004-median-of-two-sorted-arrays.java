d6036a16b262dfdfbbb04dd5c5273684e9f25d52
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-03-23 09:31:13 -0700

    Time: 1 ms (100%), Space: 46.2 MB (59.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,44 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Ensure nums1 is the smaller array for binary search
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        // quick check to ensure we search on the shorter array
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
 
+        // logic to find median: 
+            // if m+n is odd, median is the max left side number (most left)
+                // so we want left side to have one more element than the right side
+            // if m+n is even, median is average of (leftmost + rightmost)/2
+                // so both sides have same number of elements
         int m = nums1.length;
         int n = nums2.length;
-        int totalLeft = (m + n + 1) / 2;
-
+        int numberOfLeftElements = (m+n+1)/2;
+        
         int left = 0, right = m;
-
         while (left <= right) {
-            int partition1 = (left + right) / 2;
-            int partition2 = totalLeft - partition1;
+            int partition1 = left + (right-left)/2; //partition for nums1, number of elements we need for leftside from nums1
+            int partition2 = numberOfLeftElements - partition1; //partition for nums2, number of elements we need for leftside from nums2
 
-            int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
-            int minRight1 = (partition1 == m) ? Integer.MAX_VALUE : nums1[partition1];
+            int maxLeftFromNums1 = partition1 == 0? Integer.MIN_VALUE: nums1[partition1-1];
+            int maxLeftFromNums2 = partition2 == 0? Integer.MIN_VALUE: nums2[partition2-1];
 
-            int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
-            int minRight2 = (partition2 == n) ? Integer.MAX_VALUE : nums2[partition2];
+            int minRightFromNums1 = partition1 == m? Integer.MAX_VALUE: nums1[partition1];
+            int minRightFromNums2 = partition2 == n? Integer.MAX_VALUE: nums2[partition2];
 
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                // Correct partition
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+            // implement binary search
+            if (maxLeftFromNums1 <= minRightFromNums2 && maxLeftFromNums2 <= minRightFromNums1) { // valid
+                if ((m+n) % 2 == 0) {
+                    return (Math.max(maxLeftFromNums1,maxLeftFromNums2) + Math.min(minRightFromNums1, minRightFromNums2)) / 2.0;
                 } else {
-                    return Math.max(maxLeft1, maxLeft2);
+                    return Math.max(maxLeftFromNums1,maxLeftFromNums2);
                 }
-            } else if (maxLeft1 > minRight2) {
-                // Too far right, move left
+            } else if (maxLeftFromNums1 > minRightFromNums2) { // not valid
                 right = partition1 - 1;
-            } else {
-                // Too far left, move right
+            } else { // not valid
                 left = partition1 + 1;
             }
         }
-
         return 0.0;
-
-        
     }
 }
