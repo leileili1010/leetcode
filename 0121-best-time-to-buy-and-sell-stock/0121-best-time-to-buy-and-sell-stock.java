8f38b088d33a624b15a7cf2e08c671827c1c1201
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-04-05 12:41:48 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -7,10 +7,11 @@ class Solution {
         dp[0][1] = - prices[0];
 
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][0] + prices[i]);
             dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
         }
 
         return dp[n-1][0];
+        
     }
 }
