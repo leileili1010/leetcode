495e39ea1c27bcee80f3403dfd04c5905932edf6
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-04-05 10:53:02 -0700

    Time: 117 ms (5.03%), Space: 69.3 MB (5.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -3,18 +3,27 @@ class Solution {
         int N = nums.length;
         if (N <= 1) return N;
 
-        int len = 1, max = 1;
-        Arrays.sort(nums);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.isEmpty() || !set.contains(num)) { // skip duplicates
+                pq.offer(num);
+                set.add(num);
+            }
+        }
 
-        for (int i = 1; i < N; i++) {
-            if (nums[i] == nums[i-1]) continue;
-            
-            if (nums[i] == nums[i-1]+1) {
+        int len = 0, max = 0; 
+        int prev = pq.peek()-1; // we made up this prev so len starts from 0;
+        while (!pq.isEmpty()) {
+            int cur = pq.poll();
+
+            if (cur == prev+1) {
                 len += 1;
                 max = Math.max(len, max);
             } else {
                 len = 1;
             }
+            prev = cur;
         }
         return max;
     }
