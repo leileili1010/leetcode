f39fe5f83fe47012f005fad7bd746f8db99eb9ce
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-04-05 10:01:18 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,33 @@
+public class Codec {
+    List<Integer> list;
+
+    Codec () {
+        this.list = new ArrayList<>();
+    }
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        list.clear();
+        StringBuilder sb = new StringBuilder();
+        for (String str: strs) {
+            list.add(str.length());
+            sb.append(str);
+        }     
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int start = 0;
+        for (int len: list) {
+            res.add(s.substring(start, start+len));
+            start = len;
+        } 
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
