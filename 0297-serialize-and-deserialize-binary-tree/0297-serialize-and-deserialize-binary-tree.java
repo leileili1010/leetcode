6d8c052172b9ded8e5ebcfa008b8ecd32859ddc4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-05-22 14:42:12 -0700

    Time: 14 ms (65.41%), Space: 45.4 MB (94.07%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -10,33 +10,74 @@
 public class Codec {
 
     // Encodes a tree to a single string.
+    // public String serialize(TreeNode root) {
+    //     if (root == null) return "#";
+        
+    //     Deque<TreeNode> que = new ArrayDeque<>();
+    //     StringBuilder sb = new StringBuilder();
+    //     que.offer(root);
+
+    //     while (!que.isEmpty()) {
+    //         TreeNode curr = que.poll();
+    //         if (curr == null) {
+    //             sb.append("#,");
+    //         } else {
+    //             sb.append(curr.val).append(",");
+    //             que.offer(curr.left);
+    //             que.offer(curr.right);
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
+
     public String serialize(TreeNode root) {
         if (root == null) return "#";
+
         StringBuilder sb = new StringBuilder();
-        sb.append(root.val);
-        sb.append(",").append(serialize(root.left)); 
-        sb.append(",").append(serialize(root.right));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (curr == null) {
+                sb.append("#,");
+            } else {
+                sb.append(curr.val).append(",");
+                queue.offer(curr.left);
+                queue.offer(curr.right);
+            }
+        }
+
         return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] strs = data.split(",");
-        Deque<String> que = new ArrayDeque<>();
-        for (String str: strs) {
-            que.offer(str);
-        }
-        return helper(que);
-    }
+       if (data.equals("#")) return null;
 
-    private TreeNode helper(Deque<String> que) {
-        String s = que.poll();
-        if (s.equals("#")) return null;
-        TreeNode root = new TreeNode(Integer.valueOf(s));
-        root.left = helper(que);
-        root.right = helper(que);
-        return root;
-    } 
+       String[] nodes = data.split(",");
+       Deque<TreeNode> que = new ArrayDeque<>();
+       TreeNode root = new TreeNode(Integer.valueOf(nodes[0]));
+       que.offer(root);
+       
+       int i = 1;
+       while (!que.isEmpty()) {
+            TreeNode curr = que.poll();
+
+            if (!nodes[i].equals("#")) {
+                curr.left = new TreeNode(Integer.parseInt(nodes[i]));
+                que.offer(curr.left);
+            }
+            i++;
+
+            if (!nodes[i].equals("#")) {
+                curr.right = new TreeNode(Integer.parseInt(nodes[i]));
+                que.offer(curr.right);
+            }
+            i++;
+       }
+       return root;
+    }
 }
 
 // Your Codec object will be instantiated and called as such:
