bc3083de1bd00845e6bed2cb20bef0428aaab7d2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-05-21 19:19:05 -0700

    Time: 1 ms (97.79%), Space: 44.6 MB (30.14%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,25 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int postIdx;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (inorder.length == 0 || postorder.length == 0) return null;
-
-        TreeNode root = new TreeNode(postorder[postorder.length-1]);
-        int mid = findMid(inorder, root.val);
-
-        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid);
-        int[] leftPostorder = Arrays.copyOfRange(postorder, 0, mid);
-        root.left = buildTree(leftInorder, leftPostorder);
-        
-        int[] rightInorder = Arrays.copyOfRange(inorder, mid+1, inorder.length);
-        int[] rightPostorder = Arrays.copyOfRange(postorder, mid, postorder.length-1);
-        root.right = buildTree(rightInorder, rightPostorder);
-
-        return root;
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        postIdx = postorder.length - 1;
+        return builder(0, inorder.length, inorder, postorder);
     }
 
-    private int findMid(int[] inorder, int num) {
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == num) return i;
-        }
-        return -1;
+    private TreeNode builder(int start, int end, int[] inorder, int[] postorder) {
+        if (start >= end) return null;
+
+        TreeNode root = new TreeNode(postorder[postIdx--]);
+        int mid = map.get(root.val);
+
+        // Important: build right subtree first
+        root.right = builder(mid + 1, end, inorder, postorder);
+        root.left = builder(start, mid, inorder, postorder);
+
+        return root;
     }
 }
