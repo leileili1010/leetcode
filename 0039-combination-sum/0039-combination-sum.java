b5531844833edf9443cd2a63e6c16d487e9740a4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-05-24 11:35:11 -0700

    Time: 2 ms (83.41%), Space: 44.8 MB (46.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,32 @@
 class Solution {
+    // 不想继续传参数就写在外面；
+    int[] candidates;
+    int target;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
-        
-        dfs(candidates, new ArrayList<Integer>(), target, 0, res);
-        return res; 
+       this.candidates = candidates;
+       this.target = target;
+       List<List<Integer>> res = new ArrayList<>();
+       
+       Arrays.sort(candidates); // 1） 1）2）减少循环, 可加不可加
+       dfs(res, new ArrayList<>(), 0 , 0);
+
+       return res;
     }
 
-    private void dfs(int[] candidates, List<Integer> list, int sum, int start, List<List<Integer>> res) {
-        if (sum == 0) {
-            res.add(new ArrayList<>(list));
+    private void dfs(List<List<Integer>> res, List<Integer> list, int sum, int start) {
+        if (sum == target) {
+            res.add(new ArrayList<>(list)); // return copy of the list not the original list as list constantly changes. As list is a referece type, what you put in res will also changes.
             return;
         }
-
-        if (sum < 0) return;
+        if (sum > target) return;
 
         for (int i = start; i < candidates.length; i++) {
-            if (candidates[i] > sum) break;
-            if (i > start && candidates[i] == candidates[i-1]) continue;
-
+            if (candidates[i] > target - sum) break; // 2） 1）2）可加不可加
+            
             list.add(candidates[i]);
-            sum -= candidates[i];
-            dfs(candidates, list, sum, i, res);
+            dfs(res, list, sum + candidates[i], i);
             list.remove(list.size()-1);
-            sum += candidates[i];
         }
-    }
+    }  
 }
