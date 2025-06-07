d7972d3f41aa0f1c31ff5dd6c09c583f61547ac5
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-06-07 11:02:38 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,53 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        dfs(nums, res, target, new ArrayList<>(), 0, 4);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> quad = new ArrayList<>();
+        kSum(4, 0, target, nums, res, quad);
         return res;
     }
 
-    private void dfs(int[] nums, List<List<Integer>> res, long target, List<Integer> list, int start, int k) {
-        int N = nums.length;
-
-        if (k == 0) {
-           if (target == 0) res.add(new ArrayList<>(list));
+    // backtracking
+    private void kSum(int k, int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
+        // based case
+        if (k == 2) {
+            twoSum(start, target, nums, res, quad);
             return;
         }
-
-        for (int i = start; i < N; i++) {
+        
+        for (int i = start; i < nums.length-k+1; i++) {
+            // remove duplicate
             if (i > start && nums[i] == nums[i-1]) continue;
-            if (N-i < k) return;
-            if ((long)(nums[i]) * k > target) return;
-            if ((long)nums[i] + (long)nums[N-1] * (k-1) < target) continue;
+            
+            if (nums.length - i < k) break; // or return
 
+            if ((long)nums[i] * k > target) break; // 也可以用return
 
-            list.add(nums[i]);
-            dfs(nums, res, target-nums[i], list, i+1, k-1);
-            list.remove(list.size()-1);
+            // Prune 3: if maximum possible sum < target
+            if ((long)nums[i] + (long)nums[n - 1] * (k - 1) < target) continue; //只能用continue
+
+            quad.add(nums[i]);
+            kSum(k-1, i+1, target-nums[i], nums, res, quad);
+            quad.remove(quad.size()-1);
+        }
+    }
+
+    // binary search + two pointers
+    private void twoSum(int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
+        int left = start, right = nums.length-1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                List<Integer> temp = new ArrayList<>(quad);
+                temp.add(nums[left++]);
+                temp.add(nums[right--]);
+                res.add(temp);
+                while (left < right && nums[left] == nums[left-1]) left++;
+            }
         }
     }
 }
