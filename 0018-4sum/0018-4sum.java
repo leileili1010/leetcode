36ab3084393af2ab2084801eb047ef4357a8c32a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-07-04 13:15:26 -0700

    Time: 4 ms (97.41%), Space: 44.1 MB (37.93%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,15 +2,15 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        dfs(nums, (long)target, res, new ArrayList<>(), 0, 4);
+         kSum(4, 0, target, nums, res, new ArrayList<>());
         return res;
     }
 
-    private void dfs(int[] nums, long target, List<List<Integer>>res, List<Integer> list, int start, int k) {
+    private void kSum(int k, int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
         int N = nums.length;
 
-        if (k == 0) {
-            if (target == 0) res.add(new ArrayList<>(list));
+        if (k == 2) {
+            twoSum(start, target, nums, res, quad);
             return; 
         }
 
@@ -26,9 +26,31 @@ class Solution {
             // pruning 3
             if ((long)nums[i] + (long)nums[N-1] * (k-1) < target) continue; 
 
-            list.add(nums[i]);
-            dfs(nums, target-nums[i], res, list, i+1, k-1);
-            list.remove(list.size()-1);
+            quad.add(nums[i]);
+            kSum(k-1, i+1, target-nums[i], nums, res, quad);
+            quad.remove(quad.size()-1);
+        }
+    }
+
+
+    private void twoSum(int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
+        int left = start, right = nums.length-1;
+        while (left < right) {
+            long sum = nums[right] + nums[left];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                List<Integer> temp = new ArrayList<>(quad);
+                temp.add(nums[left++]);
+                temp.add(nums[right--]);
+                res.add(temp);
+                while (left < right && nums[left] == nums[left-1]) {
+                    left++;
+                }
+            }
+
         }
     }
 }
