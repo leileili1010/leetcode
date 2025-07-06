510408edadc2d362f44f67b5d3da68e541302d8b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-07-06 12:05:27 -0700

    Time: 33 ms (56.08%), Space: 54.9 MB (38.45%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,126 +1,73 @@
-
-
 class Twitter {
-    private static int timeStamp = 0;
-    private Map<Integer, User> userMap;
-    
-    // Tweet class to store tweet information
-    private class Tweet {
-        public int id;
-        public int time;
-        public Tweet next;
-        
-        public Tweet(int id) {
+    private static int timestamp = 0;
+
+    // Tweet class to store tweet id and time
+    private static class Tweet {
+        int id;
+        int time;
+
+        public Tweet(int id, int time) {
             this.id = id;
-            this.time = timeStamp++;
-            this.next = null;
+            this.time = time;
         }
     }
-    
-    // User class to manage user data
-    private class User {
-        public int id;
-        public Set<Integer> followed;
-        public Tweet tweetHead;
-        
-        public User(int id) {
-            this.id = id;
-            this.followed = new HashSet<>();
-            follow(id); // follow itself
-            this.tweetHead = null;
-        }
-        
-        public void follow(int id) {
-            followed.add(id);
-        }
-        
-        public void unfollow(int id) {
-            followed.remove(id);
-        }
-        
-        public void post(int id) {
-            Tweet t = new Tweet(id);
-            t.next = tweetHead;
-            tweetHead = t;
-        }
-    }
-    
+
+    // userId -> list of tweets (in reverse chronological order)
+    private Map<Integer, List<Tweet>> tweets;
+
+    // userId -> set of followees
+    private Map<Integer, Set<Integer>> followees;
+
     public Twitter() {
-        userMap = new HashMap<>();
+        tweets = new HashMap<>();
+        followees = new HashMap<>();
     }
-    
-    /** Compose a new tweet */
+
+    // Post a new tweet
     public void postTweet(int userId, int tweetId) {
-        if (!userMap.containsKey(userId)) {
-            User u = new User(userId);
-            userMap.put(userId, u);
-        }
-        userMap.get(userId).post(tweetId);
+        tweets.putIfAbsent(userId, new ArrayList<>());
+        tweets.get(userId).add(new Tweet(tweetId, timestamp++));
     }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed */
+
+    // Get the 10 most recent tweet ids in the user's news feed
     public List<Integer> getNewsFeed(int userId) {
-        List<Integer> res = new ArrayList<>();
-        
-        if (!userMap.containsKey(userId)) {
-            return res;
-        }
-        
-        Set<Integer> users = userMap.get(userId).followed;
-        PriorityQueue<Tweet> pq = new PriorityQueue<>(users.size(), (a, b) -> (b.time - a.time));
-        
-        // Add the head tweet of each followed user to the priority queue
-        for (int user : users) {
-            if (userMap.containsKey(user)) {
-                Tweet t = userMap.get(user).tweetHead;
-                if (t != null) {
-                    pq.add(t);
+        PriorityQueue<Tweet> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a.time, b.time) // min-heap by time
+        );
+
+        // Ensure user follows themselves
+        followees.putIfAbsent(userId, new HashSet<>());
+        followees.get(userId).add(userId);
+
+        for (int followeeId : followees.get(userId)) {
+            List<Tweet> tweetList = tweets.getOrDefault(followeeId, new ArrayList<>());
+            for (int i = tweetList.size() - 1; i >= 0 && i >= tweetList.size() - 10; i--) {
+                pq.offer(tweetList.get(i));
+                if (pq.size() > 10) {
+                    pq.poll(); // keep only top 10 recent
                 }
             }
         }
-        
-        int n = 0;
-        while (!pq.isEmpty() && n < 10) {
-            Tweet t = pq.poll();
-            res.add(t.id);
-            n++;
-            
-            // Add the next tweet from the same user
-            if (t.next != null) {
-                pq.add(t.next);
-            }
+
+        List<Integer> result = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            result.add(pq.poll().id);
         }
-        
-        return res;
+
+        Collections.reverse(result); // latest tweets first
+        return result;
     }
-    
-    /** Follower follows a followee */
+
+    // Follow a user
     public void follow(int followerId, int followeeId) {
-        if (!userMap.containsKey(followerId)) {
-            User u = new User(followerId);
-            userMap.put(followerId, u);
-        }
-        if (!userMap.containsKey(followeeId)) {
-            User u = new User(followeeId);
-            userMap.put(followeeId, u);
-        }
-        userMap.get(followerId).follow(followeeId);
+        followees.putIfAbsent(followerId, new HashSet<>());
+        followees.get(followerId).add(followeeId);
     }
-    
-    /** Follower unfollows a followee */
+
+    // Unfollow a user
     public void unfollow(int followerId, int followeeId) {
-        if (!userMap.containsKey(followerId) || followerId == followeeId) {
-            return;
+        if (followeeId != followerId && followees.containsKey(followerId)) {
+            followees.get(followerId).remove(followeeId);
         }
-        userMap.get(followerId).unfollow(followeeId);
     }
 }
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter obj = new Twitter();
- * obj.postTweet(userId,tweetId);
- * List<Integer> param_2 = obj.getNewsFeed(userId);
- * obj.follow(followerId,followeeId);
- * obj.unfollow(followerId,followeeId);
- */
