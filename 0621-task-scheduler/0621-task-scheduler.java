afab20a16688d8020d16b27ba283dd6d1bbebfda
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-07-05 20:56:50 -0700

    Time: 19 ms (61.76%), Space: 45.6 MB (74.24%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,37 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        // step 1: construct frequency map
         int[] freq = new int[26];
         for (char c: tasks) {
-            freq[c-'A']++;
+            freq[c - 'A']++;
         }
-        Arrays.sort(freq);
 
+        // step 2: contruct maxheap
+        PriorityQueue<Integer> maxHeap  = new PriorityQueue<>((a,b) -> (b - a));
+        for (int num: freq) {
+            if (num > 0) maxHeap.offer(num);
+        }
+
+        // step 3
         int time = 0;
-        while (freq[25] > 0) {//每一轮都会重新sort, 如果最大的freq都是0，说明tasks已经全部做完了
-            int i = 0;
-            while (i < n+1 && freq[25] > 0) {
-                int idx = 25-i;
-                if (idx >= 0 && freq[idx] > 0) freq[idx]--;
-                time++;
-                i++;
-            }   
-            Arrays.sort(freq);
+        while (!maxHeap.isEmpty()) {
+            int cycle = n + 1;
+            List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+            
+            while(cycle-- > 0 && !maxHeap.isEmpty()) {
+                int task = maxHeap.poll();
+                taskCount++;
+                task--;
+                if (task > 0) store.add(task); //如果task == 0,就没必要再放入heap, 已经做完该种task了
+            }
+
+            for (int num: store) {
+                maxHeap.offer(num);
+            }
+
+            // time += n+1 除了最后一轮，直接+该轮实际的task数
+            time += maxHeap.isEmpty()? taskCount: n+1; // 这里不能+cycle, cycle ！= n+1;
         }
         return time;
     }
