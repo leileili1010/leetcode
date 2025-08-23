209b3bc812ee9bc73f9f268ba37714a208e591c6
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-22 23:12:42 -0700

    Time: 1 ms (86.26%), Space: 42 MB (78.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
-        
-        // base case: 1st row is always 1
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
+        triangle.add(new ArrayList<>(Arrays.asList(1)));
 
         for (int i = 1; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
-            List<Integer> preRow = triangle.get(i-1); //[1]
-            
-            // first element always 1
+            List<Integer> preRow = triangle.get(i-1);
             row.add(1);
 
             for (int j = 1; j < i; j++) {
-                row.add(preRow.get(j - 1) + preRow.get(j));
+                row.add(preRow.get(j-1) + preRow.get(j));
             }
 
-            // last element always 1
             row.add(1);
-        
+
             triangle.add(row);
         }
         return triangle;
+
     }
 }
