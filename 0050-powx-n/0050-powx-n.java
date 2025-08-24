b0cc25d8656b3dede6daef61a7411937194db30a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-24 16:52:50 -0700

    Time: 0 ms (100%), Space: 42.2 MB (61.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n; // use long to avoid overflow for -2^31
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return fastPow(x, N);
+        if (x == 0 ) return 0;
+        if (n == 0) return 1;
+
+        double res = fastPow(x, Math.abs((long)n));
+        return n > 0? res: 1/res;
     }
 
     private double fastPow(double x, long n) {
         if (n == 0) return 1.0;
+
         double half = fastPow(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
-        }
+        return (n % 2 == 0)? half * half: half * half * x;
     }
 }
