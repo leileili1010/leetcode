7c4b6fe99358379d2b31b45692fd80e52f3602b3
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-30 07:43:31 -0700

    Time: 99 ms (53.83%), Space: 71.9 MB (43.42%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
@@ -3,11 +3,12 @@ class Solution {
         int[] prefixSum = getPrefixSum(nums);
         int maxLen = Integer.MIN_VALUE;
         HashMap<Integer, Integer> sum2Index = new HashMap<>();
-        sum2Index.put(0, 0);
+        sum2Index.put(0,0);
 
         for (int i = 0; i < nums.length; i++) {
             if (sum2Index.containsKey(prefixSum[i+1]-k)) {
-                maxLen = Math.max(maxLen, i+1-sum2Index.get(prefixSum[i+1]-k));
+                int len = i+1-sum2Index.get(prefixSum[i+1]-k);
+                maxLen = Math.max(maxLen, len);
             }
             if (!sum2Index.containsKey(prefixSum[i+1])) {
                 sum2Index.put(prefixSum[i+1], i+1);
@@ -19,13 +20,10 @@ class Solution {
     private int[] getPrefixSum(int[] nums) {
         int[] prefixSum = new int[nums.length+1];
         prefixSum[0] = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum[i+1] = prefixSum[i] + nums[i]; 
-        }
 
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
         return prefixSum;
     }
-
-
 }
