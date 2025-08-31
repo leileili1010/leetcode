2730c401a680ce396713021484495e7e445987ed
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-30 21:40:59 -0700

    Time: 2 ms (100%), Space: 50.4 MB (14.22%) - LeetHub

--- 0370-range-addition/0370-range-addition.java
+++ 0370-range-addition/0370-range-addition.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
+        int[] res = new int[length];
         int[] diff = new int[length]; // 差分数组，初始全 0
 
-        // 1) 处理所有区间更新：O(k)
-        for (int[] op : updates) {
-            int l = op[0], r = op[1], val = op[2];
-            diff[l] += val;
-            if (r + 1 < length) diff[r + 1] -= val;
+        // 1. 处理所有区间更新：O(k)
+        for (int[] update : updates) {
+            int value = update[2]; 
+            int start = update[0];
+            int end = update[1];
+            
+            res[start] += value;
+            if (end < length-1) res[end+1] -= value;
         }
 
-        // 2) 前缀和还原最终数组：O(n)
-        int[] res = new int[length];
-        int run = 0;
+        // 2. 前缀和还原最终数组：O(n)
+        int sum = 0;
         for (int i = 0; i < length; i++) {
-            run += diff[i];
-            res[i] = run;
+            sum += res[i];
+            res[i] = sum;
         }
         return res;
     }
