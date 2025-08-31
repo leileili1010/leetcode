11cf21b541c42dba4f576536d4642a462c8a9274
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-31 16:09:51 -0700

    Time: 1 ms (100%), Space: 44.5 MB (46.89%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -2,30 +2,25 @@ class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int maxLen = 0;
         for (int[] trip: trips) {
-            maxLen = Math.max(maxLen, trip[2]); // 7
+            maxLen = Math.max(maxLen, trip[2]); 
         }
 
-        int[] diff = new int[maxLen + 1]; // 8
+        int[] diff = new int[maxLen + 1]; 
 
         for (int[] trip: trips) {
-            int val = trip[0]; //3
-            int start = trip[1]; // 5
-            int end = trip[2]; // 7
-            diff[start] += val; // 
+            int val = trip[0]; 
+            int start = trip[1]; 
+            int end = trip[2]; 
+            diff[start] += val;  
             diff[end] += -val;
          }
         
         int sum = 0; 
-        for (int i = 0; i < diff.length; i++) { // i = 4
-            sum += diff[i]; // 0 + 2 = 2
-            if (sum > capacity) return false; // false
-            diff[i] = sum; // 
+        for (int i = 0; i < diff.length; i++) { 
+            sum += diff[i]; 
+            if (sum > capacity) return false; 
         }
         return true;
     }
 }
 
-// 0 1 2 3 4  5   6  7
-//   2       -2
-//            3     -3
-// 0 2 2 2 2  
