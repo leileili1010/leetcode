ad1a1f42c8d7bcc74ca514c87984008f6300a454
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-08-30 17:39:40 -0700

    Time: 24 ms (69.88%), Space: 58.1 MB (42.38%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -6,19 +6,15 @@ class Solution {
         map.put(0, 0); // remainder to index
         int sum = 0;
 
-        for (int i = 0; i < nums.length; i++) { // i = 2
-            sum += nums[i]; // 29
-            int remainder = ((sum % k) + k) % k; // 5
-            if (map.containsKey(remainder) && i+1-map.get(remainder) >= 2) // T && T 
-                return true;
-            
-            map.put(remainder, i+1);
+        for (int i = 0; i < nums.length; i++) { 
+            sum += nums[i]; 
+            int remainder = sum % k; 
+            if (map.containsKey(remainder)) {
+                if (i+1-map.get(remainder) > 1) return true;
+            } else {
+                map.put(remainder, i+1);
+            }
         }
         return false;
     }
 }
-
-// map
-// 0, 0 (i = 0)
-// 5, 1 (i = 1)
-// 1, 2 (i = 2)
