cd502a61cce565a09ba752e1543c153c0e208ac2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-07 12:27:07 -0700

    Time: 0 ms (100%), Space: 40.2 MB (98.64%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.java
@@ -0,0 +1,13 @@
+class Solution {
+   public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        if (sx == tx && sy == ty) return true;
+        if (sx > tx && sy > ty) return false;
+
+        while (tx > sx && ty > sy)
+            if (ty > tx) ty %= tx; 
+            else tx %= ty;
+        
+        return sx == tx && sy <= ty && (ty - sy) % sx == 0 ||
+                sy == ty && sx <= tx && (tx - sx) % sy == 0;
+    }
+}
