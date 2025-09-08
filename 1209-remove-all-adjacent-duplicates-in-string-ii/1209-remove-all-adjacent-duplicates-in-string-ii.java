8bbcf9f26b0c3696fcf1c16a92eb0bd68b37a982
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-07 17:10:49 -0700

    Time: 20 ms (80.95%), Space: 46 MB (30.33%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,20 +1,21 @@
 class Solution {
-    public String removeDuplicates(String s, int k) {
-        StringBuilder sb = new StringBuilder(s);
-        int[] count = new int[sb.length()];
-
-        for (int i = 0; i < sb.length(); i++) {
-            if (i == 0 || sb.charAt(i) != sb.charAt(i-1)) {
-                count[i] = 1;
+   public String removeDuplicates(String s, int k) {
+    Stack<Integer> counts = new Stack<>();
+    char[] sa = s.toCharArray();
+    int j = 0;
+    for (int i = 0; i < s.length(); ++i, ++j) {
+        sa[j] = sa[i];
+        if (j == 0 || sa[j] != sa[j - 1]) {
+            counts.push(1);
+        } else {
+            int incremented = counts.pop() + 1;
+            if (incremented == k) {
+                j = j - k;
             } else {
-                count[i] = count[i-1]+1;
-                if (count[i] == k) {
-                    sb.delete(i-k+1, i+1);
-                    i = i-k;
-                }
+                counts.push(incremented);
             }
         }
-        return sb.toString();
-        
     }
+    return new String(sa, 0, j);
+}
 }
