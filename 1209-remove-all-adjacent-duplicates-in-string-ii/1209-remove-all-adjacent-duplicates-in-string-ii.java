e68427f093811888b63026fafda84b013ddf26b2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-07 18:24:52 -0700

    Time: 27 ms (70.29%), Space: 45.5 MB (61.31%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,33 +1,19 @@
 class Solution {
-    private static class Node {
-        char ch;
-        int cnt;
-        Node(char ch, int cnt) { this.ch = ch; this.cnt = cnt; }
-    }
-
     public String removeDuplicates(String s, int k) {
-        Deque<Node> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder(s);
+        int[] count = new int[s.length()];
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (!stack.isEmpty() && stack.peek().ch == c) {
-                // 同组字符累加
-                Node last = stack.peek();
-                last.cnt++;
-                // 达到 k 就整组删除
-                if (last.cnt == k) stack.pop();
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i-1)) {
+                count[i] = 1;
             } else {
-                stack.push(new Node(c, 1));
+                count[i] = count[i-1] + 1;
+                if (count[i] == k) {
+                    sb.delete(i-k+1, i+1);
+                    i = i-k;
+                }
             }
         }
-
-        // 还原字符串（从左到右）
-        StringBuilder res = new StringBuilder();
-        while (!stack.isEmpty()) {
-    Node node = stack.pop();
-    for (int i = 0; i < node.cnt; i++) res.append(node.ch);
-}
-return res.reverse().toString();
+        return sb.toString();
     }
 }
