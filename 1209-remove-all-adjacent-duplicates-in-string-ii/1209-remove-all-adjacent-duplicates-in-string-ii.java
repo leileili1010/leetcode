7e5c47665c5642f46d2adcb2ec36e1a3bccd831a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-07 17:16:39 -0700

    Time: 16 ms (87.23%), Space: 45.3 MB (81.72%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,21 +1,33 @@
 class Solution {
-   public String removeDuplicates(String s, int k) {
-    Stack<Integer> counts = new Stack<>();
-    char[] sa = s.toCharArray();
-    int j = 0;
-    for (int i = 0; i < s.length(); ++i, ++j) {
-        sa[j] = sa[i];
-        if (j == 0 || sa[j] != sa[j - 1]) {
-            counts.push(1);
-        } else {
-            int incremented = counts.pop() + 1;
-            if (incremented == k) {
-                j = j - k;
+    private static class Node {
+        char ch;
+        int cnt;
+        Node(char ch, int cnt) { this.ch = ch; this.cnt = cnt; }
+    }
+
+    public String removeDuplicates(String s, int k) {
+        Deque<Node> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek().ch == c) {
+                // 同组字符累加
+                Node last = stack.peek();
+                last.cnt++;
+                // 达到 k 就整组删除
+                if (last.cnt == k) stack.pop();
             } else {
-                counts.push(incremented);
+                stack.push(new Node(c, 1));
             }
         }
-    }
-    return new String(sa, 0, j);
+
+        // 还原字符串（从左到右）
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+    Node node = stack.pop();
+    for (int i = 0; i < node.cnt; i++) res.append(node.ch);
 }
+return res.reverse().toString();
+    }
 }
