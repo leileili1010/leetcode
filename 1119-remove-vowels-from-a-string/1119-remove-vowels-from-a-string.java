7c7d6bbc64c3b1dc171bb369766075b448ed4645
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-12 10:45:32 -0700

    Time: 0 ms (100%), Space: 41.4 MB (86.78%) - LeetHub

---
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeVowels(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
