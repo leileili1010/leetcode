f0f0aff986a659e429d0dbd054a95dbedd78648f
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-13 19:19:22 -0700

    Time: 5 ms (76.84%), Space: 44.9 MB (56.58%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        Arrays.sort(items, (a,b) -> (b[1] - a[1]));
-        HashMap<Integer, int[]> map = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
 
         for (int[] item: items) {
             int id = item[0];
             int score = item[1];
 
-            int[] arr = map.computeIfAbsent(id, k->new int[2]);
-            if (arr[0] < 5) {
-                arr[0] += 1;
-                arr[1] += score;
-            }
+            map.putIfAbsent(id, new PriorityQueue<>());
+            PriorityQueue<Integer> pq = map.get(id);
+
+            pq.offer(score);
+            if (pq.size() > 5) pq.poll();
         }
 
         int[][] res = new int[map.size()][2];
+
         int idx = 0;
         for (int key: map.keySet()) {
+            PriorityQueue<Integer> pq = map.get(key);
+            int sum = 0;
+            for (int s: pq) sum += s;
             res[idx][0] = key;
-            res[idx][1] = map.get(key)[1] / 5;
+            res[idx][1] = sum / 5;
             idx++;
         }
-
         return res;
     }
 }
