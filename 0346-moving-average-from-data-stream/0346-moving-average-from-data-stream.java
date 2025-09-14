8ecc097244384ed171a778174060aa36bfe9edce
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-13 20:39:35 -0700

    Time: 44 ms (38.49%), Space: 50.4 MB (6.88%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
@@ -1,26 +1,25 @@
 class MovingAverage {
-    private Queue<Integer> window;
-    private int maxSize;
+    private Deque<Integer> window;
+    private int k;
     private double sum;
 
     public MovingAverage(int size) {
-        this.window = new LinkedList<>();
-        this.maxSize = size;
+        this.k = size;
         this.sum = 0;
+        this.window = new ArrayDeque<>();
     }
-    
+
     public double next(int val) {
         window.offer(val);
         sum += val;
 
-        if (window.size() > maxSize) {
-            sum -= window.poll();
+        if (window.size() > k) {
+            sum -= window.poll(); // 移除最早的数
         }
 
         return sum / window.size();
     }
 }
-
 
 /**
  * Your MovingAverage object will be instantiated and called as such:
