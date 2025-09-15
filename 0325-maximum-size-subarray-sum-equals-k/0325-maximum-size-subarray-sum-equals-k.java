1acb6e02b081c4e15fbf32bd20a6ff11313c9b40
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-14 17:26:18 -0700

    Time: 81 ms (93.71%), Space: 71.3 MB (89.33%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
@@ -1,22 +1,19 @@
 class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
-        int maxLen = Integer.MIN_VALUE;
         HashMap<Integer, Integer> map = new HashMap<>(); // <sum, index>
         map.put(0, 0);
         int sum = 0;
+        int maxLen = 0;
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-
             if (map.containsKey(sum-k)) {
-                int len = i + 1 - map.get(sum-k);
-                maxLen = Math.max(maxLen, len);
+                int len = i - map.get(sum-k)+1;
+                maxLen = Math.max(len, maxLen);
             } 
-            if (!map.containsKey(sum)) {
-                map.put(sum, i+1);
-            }
-        }
 
-        return maxLen == Integer.MIN_VALUE? 0: maxLen;
+            if (!map.containsKey(sum)) map.put(sum, i+1);
+        }
+        return maxLen;
     }
 }
