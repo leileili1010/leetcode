a287edcb7bcecb77eb4a9f242c2020a79a164f02
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-14 19:06:31 -0700

    Time: 3 ms (91.39%), Space: 47.3 MB (85.19%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int n = customers.length; // 4
+        int n = customers.length;
         int sum = 0;
 
-        // 初始化
-        for (int i = 0; i < n; i++) { 
-            if (i < minutes) { 
+        for (int i = 0; i < n; i++) {
+            if (i < minutes) {
                 sum += customers[i];
             } else {
-                sum += (1-grumpy[i]) * customers[i]; 
+                sum += (1-grumpy[i]) * customers[i];
             }
-        } // sum = 8
-
-        int left = 0, res = sum; // res = 8
-        for (int i = minutes; i < n; i++) { // i = 3
-            if (grumpy[i] == 1) sum += customers[i]; // sum = 23
-            if (grumpy[left] == 1) sum -= customers[left]; // left = 2
-            res = Math.max(res, sum); // res = 14
-            left++;
         }
 
+        int res = sum, left = 0;
+        for (int i = minutes; i < n; i++ ) {
+            if (grumpy[i] == 1) sum += customers[i];
+            if (grumpy[left] == 1) sum -= customers[left];
+            left++;
+            res = Math.max(res, sum);
+        }
+        
         return res;
     }
 }
