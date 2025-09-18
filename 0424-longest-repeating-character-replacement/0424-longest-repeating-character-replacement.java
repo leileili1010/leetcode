a32fdf3181f7c8211cd11a478bdf4c7db6623b44
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-18 10:56:54 -0700

    Time: 7 ms (88.61%), Space: 44.6 MB (92.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,21 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int left = 0, max = 0, res = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] count = new int[26];
+        int left = 0, res = 0, maxCount = 0;
 
-        for (int i = 0; i < s.length(); i++) { // i = 2
-            char cur = s.charAt(i); // cur = 'B'
-            map.put(cur, map.getOrDefault(cur, 0)+1); // map: A-2, B-1
-            max = Math.max(max, map.get(cur)); // max = 2
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            count[cur-'A']++;
+            maxCount = Math.max(maxCount, count[cur-'A']); //用于记录当下字母出现的最高频率
 
-            while (i-left+1-max > k) {
+            while ((i-left+1) - maxCount > k) {
                 char c = s.charAt(left);
-                map.put(c, map.get(c)-1);
-                max = Math.max(max, map.get(cur));
+                count[c-'A']--;
+                maxCount = Math.max(maxCount, count[c-'A']);
                 left++;
             }
-
-            res = Math.max(res, i-left+1); // res = 3
+            res = Math.max(res, i-left+1);
         }
         return res;
     }
