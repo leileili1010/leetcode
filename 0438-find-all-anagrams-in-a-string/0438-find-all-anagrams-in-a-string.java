70a2377cf82aaf648de06e0f40aac1686e8e757e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-18 20:56:00 -0700

    Time: 10 ms (80.11%), Space: 44.8 MB (82.61%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,25 +4,35 @@ class Solution {
         if (s.length() < p.length()) return res;
 
         int[] count = new int[26];
-        int[] temp = new int[26];
-        
-        for (char c: p.toCharArray()) {
-            count[c-'a']++;
-        }
-        
-        int left = 0; 
-        for (int i = 0; i < s.length(); i++) {
-            temp[s.charAt(i)-'a']++;
+        int[] window = new int[26];
+        int kinds = 0; // p中不同字母的种类数
 
-            while (i-left+1 > p.length()) {
-                temp[s.charAt(left)-'a']--;
+        for (char c : p.toCharArray()) {
+            if (count[c - 'a'] == 0) kinds++;
+            count[c - 'a']++;
+        }
+
+        int left = 0, right = 0, match = 0;
+        while (right < s.length()) {
+            int idx = s.charAt(right) - 'a';
+            window[idx]++;
+            if (window[idx] == count[idx]) match++;
+            if (window[idx] == count[idx] + 1) match--; // 多了
+
+            // 窗口收缩到长度等于p
+            while (right - left + 1 > p.length()) {
+                int li = s.charAt(left) - 'a';
+                if (window[li] == count[li]) match--;
+                if (window[li] == count[li] + 1) match++;
+                window[li]--;
                 left++;
             }
 
-            if (i-left+1 == p.length() && Arrays.equals(temp, count)) {
+            if (right - left + 1 == p.length() && match == kinds) {
                 res.add(left);
             }
 
+            right++;
         }
         return res;
     }
