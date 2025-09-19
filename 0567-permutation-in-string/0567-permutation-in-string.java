e1345f33af5b995bc19d298f574e8df1aa187289
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-19 08:43:48 -0700

    Time: 8 ms (57.4%), Space: 43.1 MB (43.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,24 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s2.length() < s1.length()) return false;
+        int n = s1.length(), m = s2.length();
+        if (m < n) return false;
+
         int[] count1 = new int[26];
         int[] count2 = new int[26];
-        int windowSize = s1.length();
-       
-        for (int i = 0; i < s1.length(); i++) {
-            count1[s1.charAt(i)-'a']++;
-            count2[s2.charAt(i)-'a']++;
+
+        for (int i = 0; i < n; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
         }
 
-        if (Arrays.equals(count1, count2)) return true;
-
-        for (int i = windowSize; i < s2.length(); i++) {
-            char cur = s2.charAt(i);
-            count2[cur-'a']++;
-            count2[s2.charAt(i-windowSize)-'a']--;
-            if (Arrays.equals(count1, count2)) return true;
-
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] == count2[i]) matches++;
         }
-        return false;
+
+        for (int i = n; i < m; i++) {
+            if (matches == 26) return true;
+
+            int addIdx = s2.charAt(i) - 'a';
+            int removeIdx = s2.charAt(i - n) - 'a';
+
+            // Add new char
+            count2[addIdx]++;
+            if (count2[addIdx] == count1[addIdx]) matches++;
+            else if (count2[addIdx] == count1[addIdx] + 1) matches--;
+
+            // Remove old char
+            count2[removeIdx]--;
+            if (count2[removeIdx] == count1[removeIdx]) matches++;
+            else if (count2[removeIdx] == count1[removeIdx] - 1) matches--;
+        }
+        return matches == 26;
     }
 }
