e702a76548004382a63137602a66e487422a7005
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-21 07:16:38 -0700

    Time: 0 ms (100%), Space: 45.7 MB (97.82%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -7,42 +7,39 @@ class Solution {
     }
 
     private int findFirst(int[] nums, int target) {
-        int start = 0, end = nums.length-1, res = -1;
+        int start = 0, end = nums.length-1;
 
-        while (start <= end) {
+        while (start+1 < end) {
             int mid = start + (end-start)/2;
             
-            if (nums[mid] < target) {
-                start = mid+1;
-            } else if (nums[mid] > target) {
-                end = mid-1;
+            if (nums[mid] >= target) {
+                end = mid; // 继续往左找
             } else {
-                res = mid;
-                end = mid-1;
-            }
+                start = mid;
+            } 
         }
 
-        return res;
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
     }
 
 
-    private int findLast(int[] nums, int target) {
-        int start = 0, end = nums.length-1, res = -1;
+       private int findLast(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
 
-        while (start <= end) {
+        while (start+1 < end) {
             int mid = start + (end-start)/2;
             
-            if (nums[mid] < target) {
-                start = mid+1;
-            } else if (nums[mid] > target) {
-                end = mid-1;
+            if (nums[mid] <= target) {
+                start = mid; // 继续往右找
             } else {
-                res = mid;
-                start = mid+1;
-            }
+                end = mid;
+            } 
         }
 
-        return res;
-
+        if (nums[end] == target) return end;
+        if (nums[start] == target) return start;
+        return -1;
     }
 }
