9dcc95a1ad740cd80e2a31374e374c59a8ee895d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-21 20:29:36 -0700

    Time: 0 ms (100%), Space: 43.4 MB (13.93%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,19 +1,27 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         int n = arr.length;
-        int left = 0, right = n - 1;
+        int start = 0, end = n - 1;
 
-        // 找到第一个 missing(i) >= k 的下标 left
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        // 如果到最后一个元素都没达到k，直接返回
+        if (arr[n-1] - n < k) return n + k;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
             int missing = arr[mid] - (mid + 1);
+
             if (missing >= k) {
-                right = mid - 1;
+                end = mid;
             } else {
-                left = mid + 1;
+                start = mid;
             }
         }
-        // 此时 left 为第一个 missing(i) >= k 的位置；若不存在则 left == n
-        return left + k;
+
+        int missingAtStart = arr[start] - (start + 1);
+        if (missingAtStart >= k) {
+            return start + k;
+        } else {
+            return end + k;
+        }
     }
 }
