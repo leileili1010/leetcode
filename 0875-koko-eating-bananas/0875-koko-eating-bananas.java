482980da7c6c5bc554dcfe5bfd6b54d2f2d95b3e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-24 09:02:12 -0700

    Time: 14 ms (50.16%), Space: 45.4 MB (34.29%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,23 +1,23 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int left = 1;
-        int right = Arrays.stream(piles).max().getAsInt(); // Find the max value in piles
-        int res = right; // we know for sure this must work;
+        int  right = 0, left = 1, res = 0;
+        for (int num: piles) {
+            right = Math.max(right, num);
+        }
 
         while (left <= right) {
-            int k = left + (right-left)/2;
-            long hours = 0; // Use long to prevent overflow
-            for(int p: piles) {
-                int curh = p % k == 0? p/k: p/k+1;
-                hours += curh; 
+            int mid = left + (right-left)/2;
+            long hours = 0;
+
+            for (int pile: piles) {
+                hours += pile % mid == 0? pile/mid: pile/mid+1;
             }
 
-           if (hours <= h) {
-                res = k;
-                right = k-1;
+            if (hours <= h) {
+                res = mid;
+                right = mid - 1;
             } else {
-                left = k+1;
+                left = mid + 1;
             }
         }
         return res;
