234b26ca9c5d3c46a412a8dec4c396691464dfb9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-24 07:18:49 -0700

    Time: 0 ms (100%), Space: 50.7 MB (14.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int left = 0, right = nums.length-1;
-
-        while (left  < right) { // 保证at least 有两个数才开始while loop
-            int mid = left + (right - left)/2;
-            if (mid % 2 == 1) mid--; // 保证mid是偶数
-
-            if (nums[mid] == nums[mid+1]) {
-                left = mid + 2;
-            } else {
-                right = mid;
-            }
-        }
-        return nums[left];
+       int left = 0, right = nums.length - 1;
+while (left + 1 < right) {
+    int mid = left + (right - left) / 2;
+    if (mid % 2 == 1) mid--;
+    if (nums[mid] == nums[mid + 1]) {
+        left = mid + 2;
+    } else {
+        right = mid;
+    }
+}
+// 退出时 left 和 right 只差 0 或 1，需要手动判断
+if (left == right) return nums[left];
+return nums[left] == nums[left+1] ? nums[right] : nums[left];
     }
 }
