100d66bb07cc6a7908b22ee03128c02427cdcae3
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-25 19:28:55 -0700

    Time: 0 ms (100%), Space: 41.8 MB (93.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,50 +1,49 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int minIdx = findMinIdx(nums);
-        if (target < nums[minIdx] || (minIdx > 0 && target > nums[minIdx-1])) {
-            return -1;
-        }
+        // 1. find index of the min element
+        int minIdx = findMinIndex(nums);
+
+        // 2. edge cases
+        if (target < nums[minIdx] || minIdx > 0 && target > nums[minIdx-1]) return -1;
+
+        // 3. confirm search range
         int n = nums.length;
-        int start, end;
-        if (minIdx == 0) {
-            start = 0;
-            end = n-1;
-        } else {
-            if (target <= nums[n-1]) {
-                start = minIdx;
-                end = n-1;
-            } else {
-                start = 0;
-                end = minIdx-1;
-            }
+        int left = 0, right = n-1; // normal when minIdx == 0
+
+        if (target > nums[right]) {
+            right = minIdx-1;
+        } else  {
+            left = minIdx;
         }
 
-        while (start <= end) {
-            int mid = start + (end - start)/2;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+
             if (nums[mid] > target) {
-                end = mid-1;
+                right = mid - 1;
             } else if (nums[mid] < target) {
-                start = mid + 1;
+                left = mid + 1;
             } else {
                 return mid;
             }
         }
         return -1;
-
     }
 
-    private int findMinIdx(int[] nums) {
-        int start = 0, end = nums.length-1;
+    private int findMinIndex(int[] nums) {
+        int left = 0, right = nums.length-1;
 
-        while (start + 1 < end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] > nums[end]) {
-                start = mid;
+        while (left + 1 < right) {
+            int mid = left + (right-left) /2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid;
             } else {
-                end = mid;
+                right = mid;
             }
         }
 
-        return nums[start] < nums[end]? start: end;
+        return nums[left] > nums[right]? right: left;
     }
+
 }
