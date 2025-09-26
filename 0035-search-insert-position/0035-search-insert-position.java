1463c89e3cf6a47f1728c7d2247d4fd2038265a4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-09-25 19:49:30 -0700

    Time: 0 ms (100%), Space: 43 MB (66.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,17 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         // find the 1st number >= target, return index
-        int left = 0, right = nums.length-1;
+        int n = nums.length;
+        if (target > nums[n-1]) return n;
+        int left = 0, right = n-1;
 
-        while (left <= right) {
+        while (left + 1 < right) {
             int mid = left + (right-left)/2;
 
             if (nums[mid] >= target) {
-                right = mid-1;
+                right = mid;
             } else {
-                left = mid+1;
+                left = mid;
             }
         }
-        return left;
+        return nums[left] >= target? left: right;
     }
 }
