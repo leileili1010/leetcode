c8892e481b695f0c6126ad2cfc83374f971cea7b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-10-10 22:51:33 -0700

    Time: 41 ms (5.02%), Space: 56.7 MB (5.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,56 +1,50 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-       // 0. 判断grid
-       if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) return 0;
-       
-        // 1. 定义数据
-        HashSet<Integer> visited = new HashSet<>();
+    int rows;
+    int cols;
+    int[][] steps = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        int n = grid.length;
-        int m = grid[0].length;
+    public int numIslands(char[][] grid) {
+        rows = grid.length; 
+        cols = grid[0].length;
+        HashSet<String> visited = new HashSet<>();
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited.contains(i * m + j) && grid[i][j] == '1') {
-                    count += 1;
-                    bfs(grid, i, j, visited);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                char cur = grid[i][j];
+                if (cur == '1' && !visited.contains(i+"_"+j)) {
+                    count++;
+                    bfs(i, j, visited, grid);
                 }
             }
-        } 
-
-        return count;       
+        }
+    
+        return count;
     }
 
-    private void bfs(char[][] grid, int i, int j, HashSet<Integer> visited) {
-        Deque<Integer> que = new ArrayDeque<>();
-        int m = grid[0].length;
-        que.offer(i * m + j);
-        visited.add(i * m + j);
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+    private void bfs(int row, int col, HashSet<String> visited, char[][] grid) {
+        Deque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{row, col});
+        visited.add(row+"_"+col);
 
         while (!que.isEmpty()) {
-            int cur = que.poll();
-            int x = cur / m;
-            int y = cur % m;
+            int[] cur = que.poll();
+            int curRow = cur[0];
+            int curCol = cur[1];
 
-            for (int k = 0; k < 4; k++) {
-                int newX = x + dx[k];
-                int newY = y + dy[k];
+            for (int[] step: steps) {
+                int newRow = curRow + step[0];
+                int newCol = curCol + step[1];
 
-                if (isValid(grid, newX, newY, visited)) {
-                    que.offer(newX * m + newY);
-                    visited.add(newX * m + newY);
+                if (isValidCell(newRow, newCol) && grid[newRow][newCol] == '1' && !visited.contains(newRow+"_"+newCol)) {
+                    que.offer(new int[]{newRow, newCol});
+                    visited.add(newRow+"_"+newCol);
                 }
             }
         }
     }
-    
-    private boolean isValid(char[][] grid, int x, int y, HashSet<Integer> visited) {
-        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) return false;
-        if (visited.contains(x * grid[0].length + y)) return false;
-        if (grid[x][y] != '1') return false;
-        return true; 
+
+    private boolean isValidCell(int row, int col) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
     }
 }
