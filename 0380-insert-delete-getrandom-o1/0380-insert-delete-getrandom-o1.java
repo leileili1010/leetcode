982720b277842f4d6435a2217eb418a6b46ec559
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-10-11 05:41:40 -0700

    Time: 27 ms (70.92%), Space: 91.9 MB (71.36%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,54 @@
+class RandomizedSet {
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random rand;
+
+    public RandomizedSet() {
+        map = new HashMap<>(); // <val, index>
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        list.add(val);
+        map.put(val, list.size()-1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+
+        int idx = map.get(val);
+        int lastVal = list.get(list.size() - 1);
+
+        // 1️⃣ swap 最后一个元素到 idx 位置
+        list.set(idx, lastVal);
+
+        // 2️⃣ 更新最后一个元素的索引
+        map.put(lastVal, idx);
+
+        // 3️⃣ 删除最后一个元素
+        list.remove(list.size() - 1);
+
+        // 4️⃣ 移除 val 的映射
+        map.remove(val);
+
+        return true;
+    }
+    
+    public int getRandom() {
+        int end = list.size();
+        int idx = rand.nextInt(end);
+        
+        return list.get(idx);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
