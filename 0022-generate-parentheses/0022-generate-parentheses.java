8c1f68dd785832c7931bd77aab1254d485a926d9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-02 07:58:49 -0800

    Time: 2 ms (43.54%), Space: 44.6 MB (7.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backtrack(res, "", 0, 0, n);
+        dfs(res, "", 0, 0, n);
         return res;
     }
 
-    private void backtrack(List<String> res, String cur, int open, int close, int n) {
-        if (cur.length() == n * 2) {
-            res.add(cur);
+    private void dfs(List<String> res, String s, int open, int close, int n) {
+        if (s.length() == 2 * n) {
+            res.add(s);
             return;
         }
 
         if (open < n) {
-            backtrack(res, cur+"(", open+1, close, n);
+            dfs(res, s+"(", open+1, close, n);
         }
 
         if (close < open) {
-            backtrack(res, cur+")", open, close+1, n);
+            dfs(res, s+")", open, close+1, n);
         }
     }
 }
