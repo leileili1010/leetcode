f0f7ae1f2ade27419749199f1374bfb857259d26
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-09 20:07:51 -0800

    Time: 8 ms (18.27%), Space: 47.1 MB (6.08%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,45 +1,27 @@
 class Solution {
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    int rows, cols;
-
     public int islandPerimeter(int[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
-        boolean[][] visited = new boolean[rows][cols];
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int res = 0;
+        int[][] steps = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    // Start BFS once — there’s only one island
-                    return bfs(i, j, visited, grid);
+                    res += 4;
+                    
+                    for (int[] step: steps) {
+                        int x = i + step[0];
+                        int y = j + step[1];
+                        if (isValid(x, y, grid)) res--;
+                    }
                 }
             }
         }
-        return 0;
-    }
-
-    private int bfs(int row, int col, boolean[][] visited, int[][] grid) {
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{row, col});
-        visited[row][col] = true;
-        int perimeter = 0;
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
-
-            for (int[] d : dirs) {
-                int x = r + d[0], y = c + d[1];
-                // Out of bounds or water adds to perimeter
-                if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0) {
-                    perimeter++;
-                } else if (!visited[x][y]) {
-                    visited[x][y] = true;
-                    q.offer(new int[]{x, y});
-                }
-            }
-        }
-
-        return perimeter;
+        return res;
+    }    
+                    
+    private boolean isValid(int x, int y, int[][] grid) {
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1;
     }
 }
