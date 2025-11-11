e8925d981ba472c36b1c4c184415650e610f0e7b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-10 19:39:53 -0800

    Time: 0 ms (100%), Space: 43.1 MB (16.64%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        // edge case
+        int[] orderMap = new int[26];
+        for (int i = 0; i < order.length(); i++) {
+            orderMap[order.charAt(i) - 'a'] = i;
+        }
+
         for (int i = 1; i < words.length; i++) {
             String prev = words[i-1];
             String cur = words[i];
 
-            if (prev.contains(cur) && prev.length() > cur.length()) return false;
+            if (prev.startsWith(cur) && prev.length() > cur.length()) return false;
 
             int len = Math.min(cur.length(), prev.length());
             for (int j = 0; j < len; j++) {
                 char c1 = prev.charAt(j);
                 char c2 = cur.charAt(j);
                 if ( c1 != c2 ) {
-                    if( order.indexOf(c1) > order.indexOf(c2)) return false;
+                    if(orderMap[c1-'a'] > orderMap[c2-'a'] ) return false;
                     else break;
                 } 
             }
