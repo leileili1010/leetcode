ff53c3d9a99274acb5faf3060abf3bc633af4a52
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-12 11:31:55 -0800

    Time: 75 ms (86.64%), Space: 49.2 MB (45.48%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,51 +1,52 @@
 class Solution {
-    public int openLock(String[] deadends, String target) {
+    public int openLock(String[] deadends, String target) {        
+        // edge case
         Set<String> dead = new HashSet<>(Arrays.asList(deadends));
-        if (dead.contains("0000")) return -1; // 初始状态死锁
+        if (dead.contains("0000")) return -1;
 
-        Queue<String> queue = new LinkedList<>();
-        queue.offer("0000");
+        // bfs
+        Deque<String> que = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
+        que.offer("0000");
         visited.add("0000");
 
         int steps = 0;
+        while (!que.isEmpty()) {
+            int n = que.size();
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();  // 当前层的节点数
-            for (int i = 0; i < size; i++) {
-                String cur = queue.poll();
-
+            for (int i = 0; i < n; i++) {
+                String cur = que.poll();
                 if (cur.equals(target)) return steps;
-                if (dead.contains(cur)) continue;  // 遇到死锁跳过
 
-                for (String next : getNextStates(cur)) {
-                    if (!visited.contains(next)) {
+                for (String next: getNextWord(cur)) {
+                    if (!visited.contains(next) && !dead.contains(next)) {
+                        if (next.equals(target)) return steps + 1;
+                        que.offer(next);
                         visited.add(next);
-                        queue.offer(next);
                     }
                 }
             }
             steps++;
         }
 
-        return -1; // 找不到路径
+        return -1;
     }
 
-    // 生成当前状态的所有下一步状态
-    private List<String> getNextStates(String cur) {
+    private List<String> getNextWord(String cur) {
         List<String> res = new ArrayList<>();
-        char[] arr = cur.toCharArray();
+        char[] chars = cur.toCharArray();
 
         for (int i = 0; i < 4; i++) {
-            char ch = arr[i];
-            // +1
-            arr[i] = ch == '9' ? '0' : (char)(ch + 1);
-            res.add(new String(arr));
-            // -1
-            arr[i] = ch == '0' ? '9' : (char)(ch - 1);
-            res.add(new String(arr));
-            // 恢复
-            arr[i] = ch;
+            char original = chars[i];
+            // + 1
+            chars[i] = original == '9' ? '0' : (char)(original + 1);
+            res.add(new String(chars));
+            
+            // -1    
+            chars[i] = original == '0' ? '9' : (char)(original - 1);
+            res.add(new String(chars));
+
+            chars[i] = original;
         }
         return res;
     }
