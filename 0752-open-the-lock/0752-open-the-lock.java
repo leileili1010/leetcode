86afd3e2066c39702923ad3dc12018bf59965516
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-13 07:42:53 -0800

    Time: 79 ms (82.61%), Space: 49.1 MB (46.91%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int openLock(String[] deadends, String target) {        
-        // edge case
-        Set<String> dead = new HashSet<>(Arrays.asList(deadends));
-        if (dead.contains("0000")) return -1;
-
-        // bfs
+    public int openLock(String[] deadends, String target) {
+        Set<String> deadSet= new HashSet<>(Arrays.asList(deadends));
+        if (deadSet.contains("0000")) return -1;
+        
         Deque<String> que = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
+
         que.offer("0000");
         visited.add("0000");
 
@@ -18,9 +17,10 @@ class Solution {
                 String cur = que.poll();
                 if (cur.equals(target)) return steps;
 
-                for (String next: getNextWord(cur)) {
-                    if (!visited.contains(next) && !dead.contains(next)) {
-                        if (next.equals(target)) return steps + 1;
+                for (String next: getNextWords(cur)) {
+                    if (next.equals(target)) return steps + 1;
+
+                    if (!visited.contains(next) && !deadSet.contains(next)) {
                         que.offer(next);
                         visited.add(next);
                     }
@@ -32,18 +32,18 @@ class Solution {
         return -1;
     }
 
-    private List<String> getNextWord(String cur) {
+    private List<String> getNextWords(String cur) {
         List<String> res = new ArrayList<>();
         char[] chars = cur.toCharArray();
 
         for (int i = 0; i < 4; i++) {
             char original = chars[i];
-            // + 1
-            chars[i] = original == '9' ? '0' : (char)(original + 1);
+            // +1;
+            chars[i] = original == '9'? '0': (char)(original + 1);
             res.add(new String(chars));
-            
-            // -1    
-            chars[i] = original == '0' ? '9' : (char)(original - 1);
+
+            // -`
+            chars[i] = original == '0'? '9': (char)(original -1);
             res.add(new String(chars));
 
             chars[i] = original;
