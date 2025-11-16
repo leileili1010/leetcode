55b807975bb584592bbd9a77773720db26de0922
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-16 15:01:01 -0800

    Time: 8 ms (74.91%), Space: 49.4 MB (32.31%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -0,0 +1,42 @@
+class Solution {
+    int rows;
+    int cols;
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public void wallsAndGates(int[][] rooms) {
+        rows = rooms.length;
+        cols = rooms[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rooms[i][j] == 0) {
+                    bfs(i, j, rooms);
+                }
+            }
+        }
+    }
+
+    private void bfs(int row, int col, int[][] rooms) {
+        Deque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{row, col});
+
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int x = cur[0], y = cur[1];
+
+            for (int[] dir: dirs) {
+                int x1 = x + dir[0];
+                int y1 = y + dir[1];
+
+                if (isValid(x1, y1, rooms) && rooms[x1][y1] > rooms[x][y] + 1) {
+                    que.offer(new int[]{x1, y1});
+                    rooms[x1][y1] = rooms[x][y] + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isValid(int x, int y, int[][] rooms) {
+        return x >= 0 && x < rows && y >= 0 && y < cols && rooms[x][y] != 0 && rooms[x][y] != -1;
+    }
+}
