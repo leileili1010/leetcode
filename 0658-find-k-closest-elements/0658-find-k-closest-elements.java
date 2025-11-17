20f22a870a0c02a0e3bc9e22e5c6858716197df2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-17 05:42:45 -0800

    Time: 12 ms (40.33%), Space: 47.7 MB (14.95%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = binarySearch(arr, x);
+        int right = left + 1;
+        int n = arr.length;
+        List<Integer> res = new ArrayList<>();
+
+        while (k > 0) {
+
+            if (left < 0) {
+                res.add(arr[right]);
+                right++;
+            } else if (right >= n) {
+                res.add(arr[left]);
+                left--;
+            } else if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+                res.add(arr[left]);
+                left--;
+            } else {
+                res.add(arr[right]);
+                right++;
+            }
+
+            k--;
+        }
+
+        Collections.sort(res); // MUST SORT !!!
+        return res;
+    }
+
+    private int binarySearch(int[] arr, int x) {
+        int left = 0, right = arr.length-1;
+        int res = 0;
+
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+
+            if (arr[mid] <= x) {
+                left = mid + 1;
+                res = mid;
+            } else {
+                right = mid-1;
+            }
+        }
+        return res;
+    } 
+}
