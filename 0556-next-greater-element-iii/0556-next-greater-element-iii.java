3a1bed98ee65bda59286cf628288215d514d4132
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-17 09:10:41 -0800

    Time: 0 ms (100%), Space: 42 MB (17.25%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        char[] arr = Integer.toString(n).toCharArray();
+        int len = arr.length;
+        
+        // 1. find the first decreasing digit from right
+        int i = len - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+        if (i < 0) return -1; // fully non-increasing
+        
+        // 2. find the smallest digit > arr[i] from the right
+        int j = len - 1;
+        while (arr[j] <= arr[i]) {
+            j--;
+        }
+        
+        // 3. swap digits[i] and digits[j]
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        
+        // 4. reverse the suffix
+        reverse(arr, i + 1, len - 1);
+        
+        // 5. check overflow
+        long num = Long.parseLong(new String(arr));
+        return num > Integer.MAX_VALUE ? -1 : (int) num;
+    }
+    
+    private void reverse(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
