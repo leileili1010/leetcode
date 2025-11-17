8957a78882d38fd26cea91575370f85b2f896941
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-17 09:08:15 -0800

    Time: 0 ms (100%), Space: 44.5 MB (17.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,34 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        // find the pivot
         int n = nums.length;
         int i = n-2;
-        
-        // 1. find the pivot, from right to left, the first decreasing number
-        while (i >= 0 && nums[i] >= nums[i+1]) {
-            i--;
-        }
-        
-        // 2. find the smallest number > pivot, from n-1 to pivot+1 index
+
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
+
+        // find the smallest to swap
         if (i >= 0) {
-            int j = n-1;
+            int j = n - 1;
+            while (j > i && nums[j] <= nums[i]) j--;
+            swap(nums, i, j); 
 
-            while (j > i && nums[j] <= nums[i]) {
-                j--;
-            }
-
-            // 3. swap pivot and the number we found
-            swap(nums, i, j);
         }
 
-        // 4. reverse the partial digits order right to the pivot index
+        // reverse
         reverse(nums, i+1, n-1);
     }
 
-    private void reverse (int[] nums, int left, int right) {
+    private void reverse(int[] nums, int left, int right) {
         while (left < right) {
-            swap(nums, left, right);
-            left++;
-            right--;
+            swap(nums, left++, right--);
         }
     }
 
