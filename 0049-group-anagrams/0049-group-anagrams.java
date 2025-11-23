c85558cf908688e685985cb155e00182e67043bf
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-23 08:29:45 -0800

    Time: 6 ms (98.57%), Space: 49.5 MB (30.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,14 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // map: key is the sorted str and value is a list of strs
-        // loop through strs, put str in map with the corresponding key
-        // loop through map to get the value (list of each key), add to res
-        
-        List<List<String>> res = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
 
         for (String str: strs) {
-            char[] strArr = str.toCharArray();
-            Arrays.sort(strArr);
-            String key = new String(strArr);
-
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+            char[] letters = str.toCharArray();
+            Arrays.sort(letters);
+            String newStr = new String(letters);
+            map.computeIfAbsent(newStr, k -> new ArrayList<>()).add(str);
         }
 
-        for (String key: map.keySet()) {
-            res.add(map.get(key));
-        }
-        
-        return res;
+        return new ArrayList(map.values());
     }
 }
