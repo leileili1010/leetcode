a64b110b60ea5d20a7b01f55a7c017af95d506f9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-26 22:27:12 -0800

    Time: 4 ms (60.24%), Space: 52.7 MB (12.39%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Integer c1 = null, c2 = null;
+        int count1 = 0, count2 = 0;
+        List<Integer> res = new ArrayList<>();
+
+        for (int num: nums) {
+            if (c1 != null && num == c1) {
+                count1++;
+            } else if (c2 != null && num == c2) {
+                count2++;
+            } else if (count1 == 0) {
+                c1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                c2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num: nums) {
+            if (c1 != null && num == c1) count1++;
+            else if (c2 != null && num == c2) count2++;
+        }
+
+        int threshold = nums.length / 3;
+        if (count1 > threshold) res.add(c1);
+        if (count2 > threshold) res.add(c2);
+        return res;
+    }
+}
