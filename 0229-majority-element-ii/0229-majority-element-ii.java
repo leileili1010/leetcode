7d769525d1083d3a79745a3b47b3421d0583f276
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-26 22:01:17 -0800

    Time: 4 ms (60.24%), Space: 52.3 MB (26.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length; // n = 2
-        Arrays.sort(nums); // [1,2]
-        int slow = 0, fast = 0;
         List<Integer> res = new ArrayList<>();
+        Integer c1 = null, c2 = null;
+        int count1 = 0, count2 = 0;
 
-        while (fast < n) { // 0 < 2
-            int count = 0; 
-
-            while (fast < n && nums[fast] == nums[slow]) { // fast 1 > slow 0, 
-                fast++;
+        for (int num: nums) {
+            if (c1 != null && c1 == num) {
+                count1++;
+            } else if (c2 != null && c2 == num) {
+                count2++;
+            } else if (count1 == 0) {
+                c1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                c2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
-            count = fast - slow; // count  = 1
-            if (count > n/3) res.add(nums[slow]);
-            slow = fast; // slow = 1
         }
 
+        count1 = 0;
+        count2 = 0;
+        for (int num: nums) {
+            if (c1 != null && num == c1) count1++;
+            else if (c2 != null & num == c2) count2++;
+        }
+
+        int threshold = nums.length / 3;
+        if (count1 > threshold) res.add(c1);
+        if (count2 > threshold) res.add(c2);
+
         return res;
+        
     }
 }
