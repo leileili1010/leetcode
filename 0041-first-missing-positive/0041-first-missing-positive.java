87c30960b4dfc883c9a5b492935828682826d5f9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-27 11:00:46 -0800

    Time: 1 ms (99.99%), Space: 71.3 MB (17.1%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,25 +2,22 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
 
-        // Step 1: Put each number in its correct index
         for (int i = 0; i < n; i++) {
-            // keep swapping until nums[i] is in the right position, or it can't be placed
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                int correctIndex = nums[i] - 1;
-                int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1]) {
+                int j = nums[i] - 1;
+                swap(i, j, nums);
             }
         }
 
-        // Step 2: find the first index where the number is wrong
         for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
+            if (nums[i] != i+1) return i+1;
         }
+        return n+1;
+    }
 
-        // if all positions are correct, answer is n+1
-        return n + 1;
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
