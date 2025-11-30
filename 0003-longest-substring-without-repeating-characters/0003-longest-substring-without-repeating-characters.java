0fd9023c08cd2f8ea0d0d2766fa8ea2e74db5744
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-11-29 22:05:17 -0800

    Time: 1 ms (100%), Space: 44.5 MB (87.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0, n = s.length();
+    public int lengthOfLongestSubstring(String S) {
+        char[] s = S.toCharArray(); // 转换成 char[] 加快效率（忽略带来的空间消耗）
+        int n = s.length;
+        int ans = 0;
         int left = 0;
-        Set<Character> set = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-
-            while (!set.add(c)) {
-                char letter = s.charAt(left);
-                set.remove(letter);
-                left++;
+        boolean[] has = new boolean[128]; // 也可以用 HashSet<Character>，这里为了效率用的数组
+        for (int right = 0; right < n; right++) {
+            char c = s[right];
+            // 如果窗口内已经包含 c，那么再加入一个 c 会导致窗口内有重复元素
+            // 所以要在加入 c 之前，先移出窗口内的 c
+            while (has[c]) { // 窗口内有 c
+                has[s[left]] = false;
+                left++; // 缩小窗口
             }
-
-            max = Math.max(max, i-left+1);
+            has[c] = true; // 加入 c
+            ans = Math.max(ans, right - left + 1); // 更新窗口长度最大值
         }
-
-        return max;
+        return ans;
     }
 }
