0866943b07a64a703b3641d3a714154a07482c4e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-02 15:36:57 -0800

    Time: 9 ms (98.36%), Space: 148.5 MB (21.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,28 +2,31 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[n - k + 1]; // 窗口个数
-        Deque<Integer> q = new ArrayDeque<>(); // 更快的写法见【Java 数组】
+        int[] q = new int[n];
+        int head = 0, tail = -1;
 
         for (int i = 0; i < n; i++) {
             // 1. 右边入
-            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
-                q.removeLast(); // 维护 q 的单调性
+            while (head <= tail && nums[q[tail]] <= nums[i]) {
+                tail--; // 右边出队
             }
-            q.addLast(i); // 注意保存的是下标，这样下面可以判断队首是否离开窗口
+            q[++tail] = i; // 右边入队
 
             // 2. 左边出
             int left = i - k + 1; // 窗口左端点
-            if (q.getFirst() < left) { // 队首离开窗口
-                q.removeFirst();
+            if (q[head] < left) { // 队首离开窗口
+                head++;
             }
 
             // 3. 在窗口左端点处记录答案
             if (left >= 0) {
                 // 由于队首到队尾单调递减，所以窗口最大值就在队首
-                ans[left] = nums[q.getFirst()];
+                ans[left] = nums[q[head]];
             }
         }
 
         return ans;
     }
 }
+
+
