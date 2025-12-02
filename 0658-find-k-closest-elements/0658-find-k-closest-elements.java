28fbfd8595c768bf23db41224261f7f229fb1309
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-02 10:26:44 -0800

    Time: 14 ms (34.85%), Space: 47.6 MB (43.47%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        // in a sorted (ascending order) array
-        // find k closest numbers to x, in ascending order
+        // use binary search to find left side next to which we can inserch x
+        // use two pointers to go against each other to find the closer one and add to res List
 
-        List<Integer> res = new ArrayList<>(); // must be in ascending order
+        List<Integer> res = new ArrayList<>();
         int left = binarySearch(arr, x);
         int right = left + 1;
 
@@ -17,21 +17,19 @@ class Solution {
         return res;
     }
 
-    private int binarySearch(int[] arr, int x) {
+    private int binarySearch(int[] arr, int target) {
         int left = 0, right = arr.length-1;
-        int res = 0;
-
+        
         while (left <= right) {
             int mid = left + (right-left)/2;
 
-            if (arr[mid] <= x) {
-                res = mid;
-                left = mid+1;
+            if (arr[mid] <= target) {
+                left = mid + 1;
             } else {
-                right = mid-1;
-            }  
+                right = mid -1;
+            }
         }
-        return res;
+        return right;
     }
 
     private boolean isLeftCloser(int left, int right, int[] arr, int x) {
