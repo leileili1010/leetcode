e93ad059bb607e4c282873e5a68bc7eab03d3586
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-01 18:56:53 -0800

    Time: 4 ms (99.66%), Space: 44 MB (46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,37 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        if (m < n) return false;
-
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-
-        for (int i = 0; i < n; i++) {
-            count1[s1.charAt(i) - 'a']++;
-            count2[s2.charAt(i) - 'a']++;
+        // corner case
+        int m = s1.length();
+        if (s2.length() < m) return false;
+        
+        // count freq for each letter in s1
+        int[] cntS1 = new int[26];
+        for (char c: s1.toCharArray()) {
+            cntS1[c-'a']++;
         }
 
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (count1[i] == count2[i]) matches++;
+        char[] s = s2.toCharArray();
+        int[] cntS2 = new int[26];
+        for (char i = 0; i < s.length; i++) {
+            // 1. enter the window
+            char c = s[i];
+            cntS2[c-'a']++;
+            if (i + 1 < m) continue; // window is too small
+
+            // 2. compare two count array
+            if (Arrays.equals(cntS1, cntS2)) return true;
+
+            // 3. leave the window, start prep for next window
+            cntS2[s[i-m+1] - 'a']--;
         }
-
-        for (int i = n; i < m; i++) {
-            if (matches == 26) return true;
-
-            int addIdx = s2.charAt(i) - 'a';
-            int removeIdx = s2.charAt(i - n) - 'a';
-
-            // Add new char
-            count2[addIdx]++;
-            if (count2[addIdx] == count1[addIdx]) matches++;
-            else if (count2[addIdx] == count1[addIdx] + 1) matches--;
-
-            // Remove old char
-            count2[removeIdx]--;
-            if (count2[removeIdx] == count1[removeIdx]) matches++;
-            else if (count2[removeIdx] == count1[removeIdx] - 1) matches--;
-        }
-        return matches == 26;
+        return false;
     }
 }
