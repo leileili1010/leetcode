b404bd61f573b378d40f940d996513294fdb2d9b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 15:46:23 -0800

    Time: 8 ms (87.67%), Space: 48.5 MB (50.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> res = new ArrayList<>();
-        int[] current = intervals[0];
-
-        for (int[] interval: intervals) {
-            int start = interval[0], end = interval[1];
-            if (start > current[1]) {
-                res.add(current);
-                current = interval;
-            } else {
-                current[1] = Math.max(current[1], end);
+        Arrays.sort(intervals, (p, q) -> p[0] - q[0]); // 按照左端点从小到大排序
+        List<int[]> ans = new ArrayList<>();
+        for (int[] p : intervals) {
+            int m = ans.size();
+            if (m > 0 && p[0] <= ans.get(m - 1)[1]) { // 可以合并
+                ans.get(m - 1)[1] = Math.max(ans.get(m - 1)[1], p[1]); // 更新右端点最大值
+            } else { // 不相交，无法合并
+                ans.add(p); // 新的合并区间
             }
         }
-
-        res.add(current);
-
-        return res.toArray(new int[0][]);
-        
-        
+        return ans.toArray(new int[ans.size()][]);
     }
 }
+
