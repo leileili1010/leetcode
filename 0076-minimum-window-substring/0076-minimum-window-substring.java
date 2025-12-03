0c63bbea86ee698740233a546124109d1912df97
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 10:09:31 -0800

    Time: 2 ms (99.89%), Space: 46.5 MB (34.62%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
+        int[] cntS = new int[128];
+        int[] cntT = new int[128];
+        int need = 0;
 
         for (char c: t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0)+1);
+            if (cntT[c] == 0) need++;
+            cntT[c]++;
         }
 
-        int left = 0, count = 0, minLen = Integer.MAX_VALUE, minStart = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
+        int left = 0, hava = 0, minStart = 0, minLen = Integer.MAX_VALUE;
+        
+        char[] arr = s.toCharArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            char curr = arr[i];
+            if (cntT[curr] > 0) {
+                cntS[curr]++;
+                if (cntS[curr] == cntT[curr]) hava++;
+            }    
 
-            if (map.containsKey(cur)) {
-                window.put(cur, window.getOrDefault(cur, 0)+1);
-                if (window.get(cur).equals(map.get(cur))) count++;
-            }
-
-            while (count == map.size()) {
-                int len = i-left+1;
-                if (len < minLen ) {
+            while (hava == need) {
+                if (i-left+1 < minLen) {
                     minStart = left;
                     minLen = i-left+1;
                 }
-                
-                char c = s.charAt(left);
-                if (window.containsKey(c)) {
-                    window.put(c, window.get(c)-1);
-                    if (window.get(c) < map.get(c)) count--;
-                }
+
+                char lc = arr[left];
+               
+                if (cntT[lc] > 0) {
+                    cntS[lc]--;
+                    if (cntS[lc] < cntT[lc]) hava--;
+                }  
+
                 left++;
             }
+
         }
+
         return minLen == Integer.MAX_VALUE? "": s.substring(minStart, minStart+minLen);
     }
 }
