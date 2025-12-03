d8d4af4a9fe134d9ecc95a2489dca9f59cc45df7
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 13:18:11 -0800

    Time: 1 ms (98.31%), Space: 47.3 MB (5.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -4,19 +4,19 @@ class Solution {
 
         for (int[] interval: intervals) {
             int start = interval[0], end = interval[1];
-            
-            if (newInterval[1] < start) {
-                res.add(new int[]{newInterval[0], newInterval[1]});
-                newInterval = interval; 
-            } else if (newInterval[0] > end) {
+
+            if (newInterval[0] > end) {
                 res.add(interval);
+            } else if (newInterval[1] < start) {
+                res.add(newInterval);
+                newInterval = interval;
             } else {
                 newInterval[0] = Math.min(start, newInterval[0]);
                 newInterval[1] = Math.max(end, newInterval[1]);
             }
         }
-        res.add(newInterval);
 
+        res.add(newInterval);
         return res.toArray(new int[0][]);
     }
 }
