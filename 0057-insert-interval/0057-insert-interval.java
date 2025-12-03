561064620fb1ba158c5bed297bbd696977726cb0
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 13:53:11 -0800

    Time: 1 ms (98.31%), Space: 46.7 MB (46.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        // 3 cases 
+            // 1. intevals on the left side of new interval, no overlapping, add it to res
+            // 2. overlapping, update new interval
+            // 3. intervals on the right side of new interval, add it to res
         List<int[]> res = new ArrayList<>();
-
-        for (int[] interval: intervals) {
-            int start = interval[0], end = interval[1];
-
-            if (newInterval[0] > end) {
-                res.add(interval);
-            } else if (newInterval[1] < start) {
-                res.add(newInterval);
-                newInterval = interval;
-            } else {
-                newInterval[0] = Math.min(start, newInterval[0]);
-                newInterval[1] = Math.max(end, newInterval[1]);
-            }
+        int i = 0, n = intervals.length;
+        
+        // case 1: intervals on the left side 
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i++]);
         }
 
+        // case 2: overlapping
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
         res.add(newInterval);
+
+        // case 3: intervals on the right side 
+        while (i < n) {
+            res.add(intervals[i++]);
+        }
+       
         return res.toArray(new int[0][]);
     }
 }
