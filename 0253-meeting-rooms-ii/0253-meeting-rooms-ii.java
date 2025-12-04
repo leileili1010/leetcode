83e065e27fa1b91f167a18a22c2e746e0bd18ae1
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 20:59:58 -0800

    Time: 8 ms (81.36%), Space: 48.2 MB (49.89%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,29 +1,22 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // This ensures that meetings are processed in the order they begin. Sorting is necessary so we can process meetings one by one in chronological order.
-        Arrays.sort(intervals, (a, b) -> a[0] == b[0]? a[1]-b[1]: a[0]-b[0]);
-        
-        // The min-heap keeps track of the earliest ending meetings.
-        // It always has the meeting that ends soonest at the top (peek()).
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        Arrays.sort(intervals, (a,b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
 
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
 
-        // For each new meeting interval, you check if it can reuse an existing room:
-            //If its start time interval[0] is after or equal to the earliest ending meeting (meeting[1]), then a room is free.
-            // You poll() that meeting — i.e., free the room.
-            // Then, you add the current meeting to the heap:
-            // This reserves a room (either a reused one or a new one).
         for (int[] interval: intervals) {
-            if (!minHeap.isEmpty()) {
-                int[] meeting = minHeap.peek();
-                if (interval[0] >= meeting[1]) {
-                    minHeap.poll();
-                }
-            }
-            minHeap.add(interval);
+            if (!minHeap.isEmpty() && interval[0] >= minHeap.peek()[1]) {
+                minHeap.poll();
+            } 
+
+            minHeap.offer(interval);
         }
-        
-        // the size of the heap reaches is the minimum number of rooms required.
         return minHeap.size();
+
+
+
     }
 }
