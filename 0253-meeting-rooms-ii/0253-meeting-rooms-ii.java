4242ab92ef417447d340d402eda190f0e613cb6e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 20:20:10 -0800

    Time: 11 ms (13.94%), Space: 48.5 MB (27.6%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        List<int[]> times = new ArrayList<>();
+        int res = 0, count = 0;
+
+        for (int[] interval: intervals) {
+            times.add(new int[] {interval[0], 1});
+            times.add(new int[]{interval[1], -1});
+        }
+
+        Collections.sort(times, (a,b) -> a[0] == b[0]? a[1]-b[1]: a[0]-b[0]);
+
+        for (int[] time: times) {
+            count += time[1];
+            res = Math.max(count, res);
+        }
+
+        return res;
+    }
+}
