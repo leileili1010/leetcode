b1c796d7e306de0c436d3c69ab4845b80985c5bd
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-03 20:32:36 -0800

    Time: 12 ms (10.51%), Space: 48.4 MB (27.6%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        List<int[]> times = new ArrayList<>();
-        int res = 0, count = 0;
+        // This ensures that meetings are processed in the order they begin. Sorting is necessary so we can process meetings one by one in chronological order.
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0]? a[1]-b[1]: a[0]-b[0]);
+        
+        // The min-heap keeps track of the earliest ending meetings.
+        // It always has the meeting that ends soonest at the top (peek()).
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1]-b[1]);
 
+
+        // For each new meeting interval, you check if it can reuse an existing room:
+            //If its start time interval[0] is after or equal to the earliest ending meeting (meeting[1]), then a room is free.
+            // You poll() that meeting — i.e., free the room.
+            // Then, you add the current meeting to the heap:
+            // This reserves a room (either a reused one or a new one).
         for (int[] interval: intervals) {
-            times.add(new int[] {interval[0], 1});
-            times.add(new int[]{interval[1], -1});
+            if (!minHeap.isEmpty()) {
+                int[] meeting = minHeap.peek();
+                if (interval[0] >= meeting[1]) {
+                    minHeap.poll();
+                }
+            }
+            minHeap.add(interval);
         }
-
-        Collections.sort(times, (a,b) -> (a[0] - b[0]));
-
-        for (int[] time: times) {
-            count += time[1];
-            res = Math.max(count, res);
-        }
-
-        return res;
-
+        
+        // the size of the heap reaches is the minimum number of rooms required.
+        return minHeap.size();
     }
 }
