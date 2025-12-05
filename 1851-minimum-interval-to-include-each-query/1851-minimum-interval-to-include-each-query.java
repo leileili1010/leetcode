6e9e7971a9f01e35f9bf0f64e79844ababa4cd49
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-05 14:32:43 -0800

    Time: 101 ms (81.4%), Space: 136.9 MB (53.67%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,42 +1,41 @@
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
-        // pair each query with its index, sort
-        // sort intervals by left end
-        // loop through queries
-            // while start < query, push interval to minHeap
-            // check expired interval, removed them from minHeap
-            // top of minHeap is the response for that query
-        
         int n = queries.length;
         int[][] queryPairs = new int[n][2];
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> ((a[1]-a[0])-(b[1]-b[0])));
-        int[] res = new int[n];
 
-    
+        // 1. build queryPair from queries
         for (int i = 0; i < n; i++) {
             queryPairs[i][0] = queries[i];
             queryPairs[i][1] = i;
         }
 
-        Arrays.sort(queryPairs, (a,b) -> a[0] - b[0]);
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        // 2. sort intervals and queryPairs
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+        Arrays.sort(queryPairs, (a,b) -> (a[0] - b[0]));
 
+        // 3. use minHeap to find out the shortest distance
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> ((a[1]-a[0])-(b[1]-b[0])));
         int i = 0;
+        int[] res = new int[n];
+
         for (int[] queryPair: queryPairs) {
             int query = queryPair[0];
             int idx = queryPair[1];
 
+            // 1) put intervals that has a left end <= query in minHeap
             while (i < intervals.length && intervals[i][0] <= query) {
                 minHeap.offer(intervals[i++]);
             }
 
+            // 2) remove expired interval when its right end <= query
             while (!minHeap.isEmpty() && minHeap.peek()[1] < query) {
                 minHeap.poll();
             }
 
-            int[] interval = minHeap.peek();
-            res[idx] = !minHeap.isEmpty()? minHeap.peek()[1] - minHeap.peek()[0] +1: -1;
+            // 3) update
+            res[idx] = minHeap.isEmpty()? -1: minHeap.peek()[1]-minHeap.peek()[0]+1;
         }
+
         return res;
     }
 }
