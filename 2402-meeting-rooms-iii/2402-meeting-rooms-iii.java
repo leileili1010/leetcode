56817150b50d49fa279a59ab4e319be5b9c243e1
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-05 20:17:32 -0800

    Time: 76 ms (37.24%), Space: 126.7 MB (52.03%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -9,27 +9,29 @@ class Solution {
                 // update new end time = used room end + original duration
                 // udate count
 
-        
-        int[] count = new int[n];
-        PriorityQueue<Integer> freeRooms = new PriorityQueue<>(); // room nubmer;
-       PriorityQueue<long[]> usedRooms = new PriorityQueue<>(
-            (a, b) -> a[0] == b[0] ? Long.compare(a[1], b[1]) : Long.compare(a[0], b[0])
-        ); // <endTime, room numnber>
-        
+        // 1. sort meeting numbers
         Arrays.sort(meetings, (a,b) -> (a[0] - b[0]));
 
+        // 2. put rooms in minHeaps
+        PriorityQueue<Integer> freeRooms = new PriorityQueue<>(); // <roomNumber>
         for (int i = 0; i < n; i++) freeRooms.offer(i);
+
+        // 3. loop through meetings and assign rooms
+        PriorityQueue<long[]> usedRooms = new PriorityQueue<>((a,b) -> 
+        a[0] == b[0]? Long.compare(a[1], b[1]): Long.compare(a[0], b[0])); //<endTime, roomNumber>
+        int[] count = new int[n];
 
         for (int[] meeting: meetings) {
             long start = meeting[0];
             long end = meeting[1];
             long duration = end - start;
 
-            // release used rooms with finished meetings
+            // 1) released usedRoom if meeting finishes
             while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
                 freeRooms.offer((int)usedRooms.poll()[1]);
             }
 
+            // 2) 
             int room;
             if (freeRooms.isEmpty()) {
                 long[] earliest = usedRooms.poll();
@@ -39,14 +41,15 @@ class Solution {
                 room = freeRooms.poll();
             }
 
-            usedRooms.offer(new long[]{end, room});
+            usedRooms.offer(new long[] {end, room});
             count[room]++;
-        } 
+        }
 
         int res = 0;
-        for (int i = 1; i < count.length; i++) {
+        for (int i = 1; i < n; i++) {
             if (count[i] > count[res]) res = i;
         }
+
         return res;
 
     }
