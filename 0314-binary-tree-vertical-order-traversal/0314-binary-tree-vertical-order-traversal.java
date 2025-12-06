74c4225cd06c463f3791f8a158e793d967a5a63d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-06 11:43:43 -0800

    Time: 3 ms (54.14%), Space: 44.2 MB (40.22%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Pair {
     int col;
     TreeNode node;
@@ -21,39 +22,33 @@ class Pair {
         this.col = col;
         this.node = node;
     }
-}
+} 
 
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        // corner case root == null, return res;
-        // TreeMap or hashMap <col, List<Integer>>
-        // bfs to do a level order traverse to update hashMap
-            // use queue to implement bfs, queue<col, node>
-        // loop through map to update res
-        
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
+        Map<Integer, List<Integer>> map = new HashMap<>(); // <col, list of node.val>;
         Deque<Pair> queue = new ArrayDeque<>();
-        Map<Integer, List<Integer>> map = new TreeMap<>(); // <>
         queue.offer(new Pair(0, root));
+        int minCol = 0;
 
         while (!queue.isEmpty()) {
             Pair curr = queue.poll();
             TreeNode node = curr.node;
             int col = curr.col;
 
+            minCol = Math.min(minCol, col);
             map.computeIfAbsent(col, k -> new ArrayList<>()).add(node.val);
 
             if (node.left != null) queue.offer(new Pair(col-1, node.left));
             if (node.right != null) queue.offer(new Pair(col+1, node.right));
         }
 
-        for (List<Integer> list: map.values()) {
-            res.add(list);
+        for (int col = minCol; col < minCol + map.size(); col++) {
+            res.add(map.get(col));
         }
         return res;
-
-
     }
 }
