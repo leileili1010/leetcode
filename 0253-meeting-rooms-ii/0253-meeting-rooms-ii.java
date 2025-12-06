c4214f625f54e64d5dbc567b82c722f2e11c4d74
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-05 16:18:29 -0800

    Time: 8 ms (82.42%), Space: 48.3 MB (40.67%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,20 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        List<int[]> meetings = new ArrayList<>();
-        int max = 0, time = 0;
+        Arrays.sort(intervals, (a,b) -> (a[0] == b[0]? a[1]-b[1]: a[0]-b[0]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
 
-        for (int[] interval: intervals) {
-            meetings.add(new int[]{interval[0], 1});
-            meetings.add(new int[]{interval[1], -1});
+        for (int i = 0; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (!minHeap.isEmpty() && minHeap.peek()[1] <= interval[0]) {
+                minHeap.poll();
+            } 
+            minHeap.offer(interval);
         }
 
-         Collections.sort(meetings, (a,b) -> a[0] == b[0]? a[1]-b[1]: a[0]-b[0]);
+        return minHeap.size();
 
-        for (int[] meeting: meetings) {
-            time += meeting[1];
-            max = Math.max(max, time);
-        }
-
-        return max;
     }
 }
