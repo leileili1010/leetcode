46192a9431581e42617e1e8ba673c0d0e41694d4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-07 11:34:46 -0800

    Time: 15 ms (93.45%), Space: 97.7 MB (46.05%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
         int n = nums.length;
-        Deque<Integer> candidateK = new LinkedList<Integer>();
-        candidateK.push(nums[n - 1]);
-        int maxK = Integer.MIN_VALUE;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int k = Integer.MIN_VALUE;
 
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] < maxK) {
-                return true;
-            }
-            while (!candidateK.isEmpty() && nums[i] > candidateK.peek()) {
-                maxK = candidateK.pop();
-            }
-            if (nums[i] > maxK) {
-                candidateK.push(nums[i]);
-            }
+        for (int i = n-1; i >= 0; i--) {
+            if (nums[i] < k) return true;
+
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                k = stack.pop(); 
+            } 
+            stack.push(nums[i]);
         }
 
         return false;
