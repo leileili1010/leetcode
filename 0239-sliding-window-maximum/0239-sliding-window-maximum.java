4a68653440872abd40bd2c8655141b14d1c06741
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-07 17:15:15 -0800

    Time: 30 ms (74.55%), Space: 143.5 MB (51.37%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,26 +1,32 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         // 1. initialization
-            // maxHeap to save int[num, index], the top is the max for certain maxSlidingWindow
+            // queue to save index
         // 2. slide window, 
-            // add new number, check if index of the top should not be included in the window, 
-            // keep moving expired element (using while loop)
-            // update res array
+            // enter the window: 
+                // if the prior elements in queue are less than the current number, keeping polling (while loop)
+                // add current index to queue
+                // remove expired index from queue if it is expired
+        // 3. update res array, the max for res[i] is the first elemnt (most left) in the queue
         int n = nums.length;
         int[] res = new int[n-k+1];
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[0] - a[0]); // int[value, index]
+        Deque<Integer> queue = new ArrayDeque<>(); // index        
 
         for (int i = 0; i < n; i++) {
             int num = nums[i];
-            maxHeap.offer(new int[]{num, i});
-
-            int start = i-k+1;
-            while (!maxHeap.isEmpty() && maxHeap.peek()[1] < start) {
-                maxHeap.poll();
+            // add num and remove invalid from the window
+            while (!queue.isEmpty() && num >= nums[queue.getLast()]) {
+                queue.removeLast();
             }
+            queue.addLast(i);
 
-            if (i+1 < k) continue;
-            res[start] = maxHeap.peek()[0];
+            // remove expired index 
+            int left = i-k+1;
+            if (queue.getFirst() < left) queue.removeFirst();
+
+            if (i + 1 < k) continue;
+
+            res[i-k+1] = nums[queue.getFirst()];
         }  
 
         return res;
