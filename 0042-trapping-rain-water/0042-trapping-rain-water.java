e748c3dabd538f79863b5cf20fe320d0a9dbb835
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-11 22:01:39 -0800

    Time: 0 ms (100%), Space: 47.6 MB (40.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,15 +1,11 @@
 class Solution {
     public int trap(int[] height) {
-       // two pointers: left & right to track maxLeft and maxRight
-       // compare maxLeft and maxRight values, move the pointer with the smaller maxValue
-       // calculate water = maxLeft/right - height[i]
-       // update maxleft or maxRight
-       int N = height.length;
-       int left = 0, right = N-1;
-       int maxLeft = height[left], maxRight = height[right];
-       int res = 0, water = 0;
+        int n = height.length;
+        int left = 0, right = n-1;
+        int maxLeft = height[left], maxRight = height[right];
+        int res = 0, water = 0;
 
-       while (left < right) {
+        while (left < right) {
             if (maxLeft < maxRight) {
                 left++;
                 water = maxLeft - height[left];
@@ -21,7 +17,7 @@ class Solution {
                 res += water > 0? water: 0;
                 maxRight = Math.max(maxRight, height[right]);
             }
-       }
-       return res;
+        }
+        return res;
     }
 }
