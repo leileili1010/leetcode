b3b0d70c25ca12515335f67f419227fecefd3896
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-12 21:41:40 -0800

    Time: 0 ms (100%), Space: 43.7 MB (27.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,22 +2,23 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // 1. 转置
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+            // 转置：对角线上方
+            for (int j = i+1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-        }
 
-        // 2. 翻转
-        for (int[] row: matrix) {
+            // 翻转
+            int[] row = matrix[i];
             for (int j = 0; j < n/2; j++) {
                 int temp = row[j];
                 row[j] = row[n-1-j];
                 row[n-1-j] = temp;
             }
+            
         }
+
     }
 }
