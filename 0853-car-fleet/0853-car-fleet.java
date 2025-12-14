fcf3b25b976b209a5cc38a1ca3b5c600b0ccca7b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-14 08:07:46 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,7 +1,7 @@
 class Car {
     int speed;
     int position;
-    int timeToDestination;
+    double timeToDestination;
 
     Car (int position, int speed) {
         this.position = position;
@@ -17,7 +17,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             cars[i] = new Car(position[i], speed[i]);
             int distance = target-position[i];
-            cars[i].timeToDestination = distance % speed[i] == 0? distance/speed[i]: distance/speed[i] + 1;
+            cars[i].timeToDestination = distance/speed[i] * 1.0;
         }
 
         // cars: (0,1,12), (3,3,3), (5,1,7), (8,4,1), (10,2,1)
@@ -29,7 +29,6 @@ class Solution {
         for (Car car: cars) {
             while (!stack.isEmpty() && stack.peek().timeToDestination <= car.timeToDestination) {
                 Car prev = stack.pop(); 
-                car = prev.speed < car.speed? prev: car;
             }
             
             stack.push(car); // (0,1,12), (5,1,7), (10,2,1)
