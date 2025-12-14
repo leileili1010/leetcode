fa0a5f77560e108550b2e60bb8cc6a86da868c5a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-14 11:38:12 -0800

    Time: 64 ms (91.55%), Space: 95.4 MB (63.8%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,28 +1,32 @@
-class Solution {
-    public int carFleet(int target, int[] position, int[] speed) {
-        int N = position.length;
-        Car[] cars = new Car[N];
-        for (int i = 0; i < N; ++i)
-            cars[i] = new Car(position[i], (double) (target - position[i]) / speed[i]);
-        Arrays.sort(cars, (a, b) -> Integer.compare(a.position, b.position));
-
-        int ans = 0, t = N;
-        while (--t > 0) {
-            if (cars[t].time < cars[t-1].time) ans++; //if cars[t] arrives sooner, it can't be caught
-            else cars[t-1] = cars[t]; //else, cars[t-1] arrives at same time as cars[t]
-        }
-
-        return ans + (t == 0 ? 1 : 0); //lone car is fleet (if it exists)
-    }
-}
-
 class Car {
     int position;
-    double time;
-    Car(int p, double t) {
-        position = p;
-        time = t;
+    double timeToTarget;
+    int speed;
+
+    Car(int position, int speed) {
+        this.position = position;
+        this.speed = speed;
     }
 }
 
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        Car[] cars = new Car[n];
 
+        for (int i = 0; i < n; i++) {
+            cars[i] = new Car(position[i], speed[i]);
+            cars[i].timeToTarget = (double) (target-position[i])/speed[i];
+        }  
+
+        Arrays.sort(cars, (a,b) -> (a.position-b.position));
+
+        int res = 1;
+        for (int i = n-1; i > 0; i--) {
+            if (cars[i].timeToTarget < cars[i-1].timeToTarget) res++;
+            else cars[i-1] = cars[i]; //  后面的车追上之后变成一个车队了
+        }
+
+        return res;
+    }
+}
