e00e751e9932bfdcd32b0f8b8b49a5b89edaaf4a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-16 13:36:34 -0800

    Time: 14 ms (90.63%), Space: 43.5 MB (62.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,15 +3,17 @@ class Solution {
         int[] startAndLength = new int[2];
 
         for (int i = 0; i < s.length(); i++) {
-            expand(s, i, 0, startAndLength);
-            expand(s, i, 1, startAndLength);
-
+            expand(s, i, 0, startAndLength); // for odd
+            expand(s, i, 1, startAndLength); // for even
         }
-        return s.substring(startAndLength[0], startAndLength[0]+startAndLength[1]);
+
+        int start = startAndLength[0];
+        int len = startAndLength[1];
+        return s.substring(start, start + len);
     }
 
-    private void expand(String s, int idx, int offset, int[] startAndLength) {
-        int left = idx, right = idx + offset;
+    private void expand(String s, int start, int offset, int[] startAndLength) {
+        int left = start, right = start + offset;
 
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
