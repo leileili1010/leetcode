8408faedb4a38edb99c71810adfcb5c164f62ae1
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-16 22:04:45 -0800

    done

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public int countPartitions(int[] nums) {
-        int totalSum = Arrays.stream(nums).sum();
-        return totalSum % 2 == 0? nums.length - 1: 0;     
-    }
-}
