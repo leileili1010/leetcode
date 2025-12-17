2e9ef28f01a411bbcfad64a01c7c79a2cf794046
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-16 22:44:37 -0800

    Time: 1 ms (99.93%), Space: 43.9 MB (71.4%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,39 @@
+class Solution {
+    private static final int[][] dirs = {{0,1}, {0, -1}, {1,0}, {-1, 0}};
+    
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int fresh = 0, res = 0;
+        Deque<int[]> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = grid[i][j];
+                if (num == 1) fresh++;
+                else if (num == 2) queue.offer(new int[]{i, j});
+            }
+        }
+
+        int time = 0;
+        while (!queue.isEmpty() && fresh > 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+
+                for (int[] dir: dirs) {
+                    int x = dir[0] + cur[0];
+                    int y = dir[1] + cur[1];
+
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        queue.offer(new int[]{x, y});
+                        fresh--;
+                    }
+                }
+            }
+              time++;
+        }
+        return fresh == 0? time: -1;
+        
+    }
+}
