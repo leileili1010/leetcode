3e5837fe587751060a8916b8fc87d45de006f0a2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-16 18:51:34 -0800

    Time: 0 ms (100%), Space: 43.9 MB (5.79%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m-j-1];
+                matrix[i][m-j-1] = temp;
+            }
+        }
+    }
+}
