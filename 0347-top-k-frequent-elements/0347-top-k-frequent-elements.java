132235319af730ca7941016130a30a20a103b08b
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-18 11:52:16 -0800

    Time: 11 ms (94.09%), Space: 47.5 MB (65.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // 1.count each letter 
-        int max = 0;
+        // contruct frequency map
+        // bucket sort
+            // An arrya of length of N+1
+            // loop through map to get frequncy, 
+            // arr[freq] = new ArrayList<>(), and then add nums
+        // loop through arr from backwards, select k numbers
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0)+1);
-            max = Math.max(max, map.get(num));
-        }
+        for (int num: nums) map.put(num, map.getOrDefault(num, 0)+1);
 
-        // 2.build buckets array
-        List<Integer>[] buckets = new List[max+1]; 
+        int n = nums.length;
+        List<Integer>[] freq = new List[n+1];
         for (int key: map.keySet()) {
             int idx = map.get(key);
-            if (buckets[idx] == null) buckets[idx] = new ArrayList<>();
-            buckets[idx].add(key);
+            if (freq[idx] == null) freq[idx] = new ArrayList<>();
+            freq[idx].add(key);
         }
 
-        // 3. update res
         int[] res = new int[k];
         int idx = 0;
-        for (int i = buckets.length-1; i >= 0 && idx < k; i--) {
-            List<Integer> bucket = buckets[i];
-            if (bucket == null) continue;
-            for (int num: bucket) {
+        for (int i = n; i >= 0 && idx < k; i--) {
+            if (freq[i] == null) continue;
+            for (int num: freq[i]) {
                 res[idx++] = num;
                 if (idx == k) return res;
             }
         }
+
         return res;
     }
 }
