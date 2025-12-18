04e7736c6633bb7d46357f3f08ddfce515827c19
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-17 16:12:17 -0800

    Time: 30 ms (73.5%), Space: 143.4 MB (70.42%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,22 +1,37 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] res = new int[n-k+1];
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> (b[0]-a[0])); // int[val, index]
-        
-        for (int i = 0; i < n; i++) { // i = 5
-            maxHeap.offer(new int[]{nums[i], i}); //maxHeap: [1,0] [3,1] [-1,2] [-3,3] [5,4] [3,5]
-            
-            if (i < k-1) continue;
+        // Double-Ended Queue，monotonically decreasing，
+        // loop through nums
+            // int cur = nums[i], if nums[i] >= queue.getLast(), que.removeLast()
+            // queue.addLast(cur);
+            // if (getFirst() < i-k+1) removeFirst
+            // if left < 0 continue
+            // update ans
 
-            // left bound: 
-            while (maxHeap.peek()[1] < i-k+1) {
-                maxHeap.poll();
+        int n = nums.length; // n = 8
+        Deque<Integer> queue = new ArrayDeque<>(); // index
+        int[] res = new int[n-k+1];
+
+        // nums = [1,3,-1,-3,5,3,6,7]
+        for (int i = 0; i < n; i++) { // i = 7
+            // enter queue
+            while (!queue.isEmpty() && nums[i] >= nums[queue.getLast()]) {
+                queue.removeLast();
             }
-            res[i-k+1] = maxHeap.peek()[0]; 
+            queue.addLast(i); // queue: 7
+
+            // leave queue
+            int left = i-k+1; // left = 5
+            if (queue.getFirst() < left) {
+                queue.removeFirst();
+            }
+
+            // update res;
+            if (left >= 0) {
+                res[left] = nums[queue.getFirst()];
+            }
         }
-        return res; // res ; {3, 3, 5, 5}
+        return res; // res: 3, 3, 5, 5, 6, 7
+    
     }
 }
-
-// [1,3,-1,-3,5,3,6,7]
