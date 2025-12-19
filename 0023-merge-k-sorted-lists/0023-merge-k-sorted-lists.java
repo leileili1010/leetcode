fef7b7d8a0f0caf8dfe7ae10f4894e1be76c2939
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-19 11:08:53 -0800

    Time: 1 ms (99.71%), Space: 47.2 MB (7.26%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,24 +10,33 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> (a.val-b.val));
-
-        for (ListNode node: lists) {
-            while (node != null) {
-                ListNode temp = node.next;
-                node.next = null;
-                minHeap.offer(node);
-                node = temp;
+        int m = lists.length;
+        if (m == 0) {
+            return null;
+        }
+        for (int step = 1; step < m; step *= 2) {
+            for (int i = 0; i < m - step; i += step * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + step]);
             }
         }
+        return lists[0];
+    }
 
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
-        while (!minHeap.isEmpty()) {
-            cur.next = minHeap.poll();
+    // 21. 合并两个有序链表
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(); // 用哨兵节点简化代码逻辑
+        ListNode cur = dummy; // cur 指向新链表的末尾
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1; // 把 list1 加到新链表中
+                list1 = list1.next;
+            } else { // 注：相等的情况加哪个节点都是可以的
+                cur.next = list2; // 把 list2 加到新链表中
+                list2 = list2.next;
+            }
             cur = cur.next;
         }
-
+        cur.next = list1 != null ? list1 : list2; // 拼接剩余链表
         return dummy.next;
     }
 }
