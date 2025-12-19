3a8e100c657e0fb92f8f5fa9e8e7d663f9665979
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-18 20:06:42 -0800

    Time: 2 ms (89.5%), Space: 49.4 MB (15.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,34 +10,34 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) return;
-        ListNode mid = findMiddle(head); 
+        // 1. find mid
+        // 2. reverse the second half
+        // 3. merge
+        ListNode mid = findMid(head);
         ListNode head2 = reverse(mid.next);
         ListNode head1 = head;
         mid.next = null;
 
-        while (head1 != null && head2 != null) {
+        while (head2 != null) {
             ListNode temp1 = head1.next;
             ListNode temp2 = head2.next;
 
             head1.next = head2;
+            head2.next = temp1;
             head1 = temp1;
-
-            head2.next = head1;
-            head2 = temp2; 
+            head2 = temp2;
         }
-    
     }
 
-    private ListNode findMiddle(ListNode head) {
+    private ListNode findMid(ListNode head) {
         ListNode slow = head, fast = head;
-
+        
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
-    } 
+    }
 
     private ListNode reverse(ListNode head) {
         ListNode prev = null, cur = head;
