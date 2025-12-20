86350a1e108a9793dc6eea39b1ce95629fbc2538
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-20 11:49:17 -0800

    Time: 6 ms (21.14%), Space: 175.7 MB (18.31%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,15 +2,22 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        int i = 0, j = 1;
+        int idx = 0;
 
         for (int num: nums) {
             if (num > 0) {
-                res[i] = num;
-                i += 2;
-            } else {
-                res[j] = num;
-                j += 2;
+                res[idx] = num;
+                idx += 2;
+                if (idx >= n) break;
+            }
+        }
+
+        idx = 1;
+        for (int num: nums) {
+            if (num < 0) {
+                res[idx] = num;
+                idx += 2;
+                if (idx >= n) break;
             }
         }
         return res;
