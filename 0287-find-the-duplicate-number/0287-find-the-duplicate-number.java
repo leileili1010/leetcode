5728dad10d1ce3fb156473ee9c006fd0b850657c
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-20 10:39:07 -0800

    Time: 4 ms (90.24%), Space: 83 MB (23.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,19 +2,18 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = 0, fast = 0;
 
-        while (true) {
+        // 1. find the intersection
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if (slow == fast) break;
-        }
+        } while (slow != fast);
 
+        // 2. find the duplicate
         slow = 0;
         while (slow != fast) {
             slow = nums[slow];
-            fast = nums[fast];
+            fast = nums[fast]; 
         }
         return slow;
-
-
     }
 }
