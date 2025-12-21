080d63ad0a876fb287c0c6e3dc1c6b01d2e1225a
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-20 17:57:33 -0800

    Time: 6 ms (96.64%), Space: 46.4 MB (14.07%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -10,8 +10,8 @@ class Solution {
         int n = nums.length;
         
         // base case
-        if (k == 0) {
-            if (target == 0) res.add(new ArrayList<>(list)); 
+        if (k == 2) {
+            twoSum(nums, res, list, start, target); 
             return;
         }
 
@@ -26,6 +26,26 @@ class Solution {
             list.add(nums[i]);
             backtracking(nums, res, target-nums[i], list, i+1, k-1); // k means we still need k numbers
             list.remove(list.size()-1);
+        }
+    }
+
+    private void twoSum(int[] nums, List<List<Integer>> res, List<Integer> list, int start, int target) {
+        int left = start, right = nums.length-1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                List<Integer> temp = new ArrayList<>(list);
+                temp.add(nums[left++]);
+                temp.add(nums[right--]);
+                res.add(temp);
+                while (left < right && nums[left] == nums[left-1]) left++;
+                while (left < right && nums[right] == nums[right+1]) right--;
+            }
         }
     }
 }
