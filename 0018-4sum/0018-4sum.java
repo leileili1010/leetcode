e409cb2edd131c9162e236e0502ee8fa64fe2070
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-20 17:50:13 -0800

    Time: 10 ms (94.13%), Space: 47.5 MB (6.76%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,55 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums); // 用于去重. e.g [1,1,3,4,6], 跳过第二个1
-        dfs(nums, target, res, new ArrayList<>(), 0, 4);
+        Arrays.sort(nums); // to remove duplicates
+        backtracking(nums, res, target, new ArrayList<>(), 0, 4);
         return res;
     }
 
-    private void dfs(int[] nums, long target, List<List<Integer>> res, List<Integer> list, int start, int k) {
+    private void backtracking(int[] nums, List<List<Integer>> res, int target, List<Integer> list, int start, int k) {
         int n = nums.length;
-
-        if (k == 2) {
-            twoSum(start, target, nums, res, list);
+        
+        // base case
+        if (k == 0) {
+            if (target == 0) res.add(new ArrayList<>(list)); 
             return;
         }
 
-        for (int i = start; i < nums.length; i++) {
-            if (i > start && nums[i] == nums[i-1]) continue; // 去重
-            
+        for (int i = start; i < n; i++) {
+            if (i > start && nums[i] == nums[i-1]) continue; // remove duplicates
+           
+            // pruning
             if (n - i < k) return;
-            if ((long)nums[i] * k > target) break;
+            if ((long)nums[i] * k > target) return;
             if ((long)nums[i] + (long)nums[n-1] * (k-1) < target) continue;
 
-
             list.add(nums[i]);
-            dfs(nums, target-nums[i], res, list, i+1, k-1);
+            backtracking(nums, res, target-nums[i], list, i+1, k-1); // k means we still need k numbers
             list.remove(list.size()-1);
         }
-
-
     }
-
-    private void twoSum(int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
-        int left = start, right = nums.length-1;
-        while (left < right) {
-            long sum = (long)nums[left] + nums[right];
-            if (sum < target) {
-                left++;
-            } else if (sum > target) {
-                right--;
-            } else {
-                List<Integer> temp = new ArrayList<>(quad);
-                temp.add(nums[left++]);
-                temp.add(nums[right--]);
-                res.add(temp);
-                while (left < right && nums[left] == nums[left-1]) left++;
-                while (left < right && nums[right] == nums[right+1]) {
-                    right--;
-                }
-            }
-        }
-    }
-
-
 }
