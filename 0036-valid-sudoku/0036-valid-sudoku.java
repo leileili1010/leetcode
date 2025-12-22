0012284a92ea693660756b9084a470bd429e3aec
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-22 09:33:43 -0800

    Time: 8 ms (23.83%), Space: 46.8 MB (19.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rowHas = new boolean[9][9]; // rowHas[i][x] 表示 i 行是否有数字 x
-        boolean[][] colHas = new boolean[9][9]; // colHas[j][x] 表示 j 列是否有数字 x
-        boolean[][][] subBoxHas = new boolean[3][3][9]; // subBoxHas[i'][j'][x] 表示 (i',j') 宫是否有数字 x
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<String, Set<Character>> square = new HashMap<>();
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char b = board[i][j];
-                if (b == '.') {
-                    continue;
-                }
-                int x = b - '1'; // 字符 '1'~'9' 转成数字 0~8
-                if (rowHas[i][x] || colHas[j][x] || subBoxHas[i / 3][j / 3][x]) { // 重复遇到数字 x
-                    return false;
-                }
-                // 标记行、列、宫包含数字 x
-                rowHas[i][x] = colHas[j][x] = subBoxHas[i / 3][j / 3][x] = true;
+                if (board[i][j] == '.') continue;
+                String squareKey = i/3 + "," + j/3;
+                
+                if (!rows.computeIfAbsent(i, k -> new HashSet<>()).add(board[i][j])) return false;
+                if (!cols.computeIfAbsent(j, k -> new HashSet<>()).add(board[i][j])) return false;
+                if (!square.computeIfAbsent(squareKey, k -> new HashSet<>()).add(board[i][j])) return false;
             }
         }
-
         return true;
     }
 }
-
-
