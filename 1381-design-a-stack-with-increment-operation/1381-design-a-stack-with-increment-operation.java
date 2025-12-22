a3e5cd78c9fe2bc4597ef28fbfa8a5c60844a269
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-22 15:16:12 -0800

    Time: 5 ms (96.45%), Space: 47.3 MB (23.93%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,34 +1,36 @@
 class CustomStack {
-    List<Integer> list;
-    int maxSize;
+    int[] stack;
+    int top;
 
     public CustomStack(int maxSize) {
         // Initializes the object with maxSize which is the maximum number of elements in the stack
-        list = new ArrayList<>();
-        this.maxSize = maxSize; // maxSize = 3
+        stack = new int[maxSize];
+        top = -1;
     }
     
     public void push(int x) {
         // Adds x to the top of the stack if the stack has not reached the maxSize
-        if (list.size() < maxSize) list.add(x); // stack: {1， 2， 3}
+        if (top + 1 < stack.length) {
+            top++;
+            stack[top] = x;
+        }
     }
     
     public int pop() {
         // Pops and returns the top of the stack or -1 if the stack is empty
-        if (list.isEmpty()) return -1;
-        int last = list.get(list.size()-1);
-        list.remove(list.size()-1);
-        return last;
+        if (top == -1) return -1;
+        top--;
+        return stack[top+1];
         
     }
     
     public void increment(int k, int val) {
         // Increments the bottom k elements of the stack by val. 
         // If there are less than k elements in the stack, increment all the elements in the stack.
-        if (list.isEmpty()) return;
-        for (int i = 0; i < k && i < list.size(); i++) {
-            list.set(i, list.get(i) + val);
-        } 
+        int limit = Math.min(k, top + 1);
+        for (int i = 0; i < limit; i++) {
+            stack[i] += val;
+        }
     }
 }
 
