c1578995406d814765cb9ee711ee06908896909d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-21 22:53:58 -0800

    Time: 8 ms (23.83%), Space: 46.9 MB (19.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Use 3 maps
-            // map<row, char>
-            // map<col, char>
-            // map<string, char>
-        // loop through each cell, update maps
         Map<Integer, Set<Character>> rows = new HashMap<>();
         Map<Integer, Set<Character>> cols = new HashMap<>();
         Map<String, Set<Character>> squares = new HashMap<>();
 
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
-                char ch = board[r][c];
-                if (ch == '.') continue;
+                if (board[r][c] == '.') continue;
 
                 String squareKey = r/3 + "," + c/3;
-                if (!rows.computeIfAbsent(r, k -> new HashSet<>()).add(ch)) return false;
-                if (!cols.computeIfAbsent(c, k -> new HashSet<>()).add(ch)) return false;
-                if (!squares.computeIfAbsent(squareKey, k -> new HashSet<>()).add(ch)) return false;
+                if (!rows.computeIfAbsent(r, k -> new HashSet<>()).add(board[r][c])) return false;
+                if (!cols.computeIfAbsent(c, k-> new HashSet<>()).add(board[r][c])) return false;
+                if (!squares.computeIfAbsent(squareKey, k-> new HashSet<>()).add(board[r][c])) return false;
             }
         }
-        return true;
+        
+        return true; 
     }
 }
