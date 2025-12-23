0403341aff0a92fb319fe792c06d38206fa5b937
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-23 11:10:21 -0800

    Time: 3 ms (84.04%), Space: 43.2 MB (46.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
-    public boolean isValid(String S) {
-        if (S.length() % 2 != 0) { // s 长度必须是偶数
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) { // s 长度必须是偶数
             return false;
         }
-        char[] s = S.toCharArray();
-        int top = 0; // 直接把 s 当作栈
-        for (char c : s) {
+        Deque<Character> st = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
             if (c == '(') {
-                s[top++] = ')'; // 入栈对应的右括号
+                st.push(')'); // 入栈对应的右括号
             } else if (c == '[') {
-                s[top++] = ']';
+                st.push(']');
             } else if (c == '{') {
-                s[top++] = '}';
-            } else if (top == 0 || s[--top] != c) { // c 是右括号
+                st.push('}');
+            } else if (st.isEmpty() || st.pop() != c) { // c 是右括号
                 return false; // 没有左括号，或者左括号类型不对
             }
         }
-        return top == 0; // 所有左括号必须匹配完毕
+        return st.isEmpty(); // 所有左括号必须匹配完毕
     }
 }
