cf476362d81e653ee7a79d56d24c75fc9db8fe72
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-23 11:04:29 -0800

    Time: 3 ms (84.04%), Space: 43.6 MB (13.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) { // s 长度必须是偶数
-            return false;
-        }
-        Map<Character, Character> mp = new HashMap<>() {{
-            put('(', ')');
-            put('[', ']');
-            put('{', '}');
-        }};
-        Deque<Character> st = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            if (mp.containsKey(c)) { // c 是左括号
-                st.push(mp.get(c)); // 入栈
-            } else if (st.isEmpty() || st.pop() != c) { // c 是右括号
-                return false; // 没有左括号，或者左括号类型不对
+        // closed must be matched with open
+        // stack should be empty at the end
+        if (s.length() % 2 != 0) return false;
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                
+                if (c == ']' && top != '[' ||
+                    c == '}' && top != '{' || 
+                    c == ')' && top != '('   
+                ) return false;
             }
         }
-        return st.isEmpty(); // 所有左括号必须匹配完毕
+        return stack.isEmpty();
     }
 }
-
