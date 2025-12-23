a35e796c500334908c5c9d069283c3ff373d915f
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-23 14:33:47 -0800

    Time: 22 ms (94.9%), Space: 104 MB (70.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> stack = new ArrayDeque<>(); // index
+        Deque<Integer> stack = new ArrayDeque<>();
         int n = temperatures.length;
         int[] res = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < temperatures.length; i++) {
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int idx = stack.pop();
                 res[idx] = i - idx;
@@ -12,5 +12,6 @@ class Solution {
             stack.push(i);
         }
         return res;
+
     }
 }
