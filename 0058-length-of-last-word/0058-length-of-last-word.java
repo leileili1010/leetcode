78d069c0a8fcd1b38c1762fc82a57d1d416126cd
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-24 10:55:14 -0800

    Time: 0 ms (100%), Space: 43.5 MB (11.74%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
-   public int lengthOfLastWord(String s) {
-    s = s.trim(); // 去掉首尾空格
-    return s.length() - s.lastIndexOf(' ') - 1;
-}
+    public int lengthOfLastWord(String s) {
+        s = s.trim(); // 去掉首尾空格
+        return s.length() - s.lastIndexOf(' ') - 1;
+    }
 }
