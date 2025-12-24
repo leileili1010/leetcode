b6417d4a00f38108f87f33fb928d4512c22a7fa8
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-23 17:29:02 -0800

    Time: 23 ms (90.26%), Space: 81.6 MB (15.86%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,14 +1,11 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // 单调栈 - 单调递增，存入的是index
-        // 枚举高度， 需要知道宽度
-        int n = heights.length;
+        // 单调栈，单调递增，<index>
+
         Deque<Integer> stack = new ArrayDeque<>(); // <index>, ascending order
         stack.push(-1);
-        int max = 0;
-
-        // heights = [2,1,5,6,2,3]
-                   // 0 1 2 3 4 5
+        int n = heights.length;
+        int res = 0;
 
         for (int i = 0; i <= n; i++) {
             int curHeight = i != n? heights[i]: -1;
@@ -16,12 +13,12 @@ class Solution {
             while (stack.size() > 1 && curHeight < heights[stack.peek()]) {
                 int idx = stack.pop();
                 int left = stack.peek();
-                int width = i - left - 1;
                 int height = heights[idx];
-                max = Math.max(max, width * height);
+                int width = i - left - 1;
+                res = Math.max(res, height * width);
             }
             stack.push(i);
         }
-        return max;
+        return res;
     }
 }
