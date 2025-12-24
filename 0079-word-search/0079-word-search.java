96053c44096dd5df1c6fe8f11c9a8e65ef3f2c37
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-24 14:43:24 -0800

    Time: 0 ms (100%), Space: 43.3 MB (20.51%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,37 +1,56 @@
 class Solution {
-    private static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, word, i, j, 1, visited)) return true;
+        // 为了方便，直接用数组代替哈希表
+        int[] cnt = new int[128];
+        for (char[] row : board) {
+            for (char c : row) {
+                cnt[c]++;
+            }
+        }
+
+        // 优化一
+        char[] w = word.toCharArray();
+        int[] wordCnt = new int[128];
+        for (char c : w) {
+            if (++wordCnt[c] > cnt[c]) {
+                return false;
+            }
+        }
+
+        // 优化二
+        if (cnt[w[w.length - 1]] < cnt[w[0]]) {
+            w = new StringBuilder(word).reverse().toString().toCharArray();
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (dfs(i, j, 0, board, w)) {
+                    return true; // 搜到了！
                 }
             }
         }
-        return false;
+        return false; // 没搜到
     }
 
-    private boolean dfs(char[][] board, String word, int row, int col, int idx, boolean[][] visited) {
-        if (idx == word.length()) {
+    private boolean dfs(int i, int j, int k, char[][] board, char[] word) {
+        if (board[i][j] != word[k]) { // 匹配失败
+            return false;
+        }
+        if (k == word.length - 1) { // 匹配成功！
             return true;
         }
-        visited[row][col] = true;
-
-        for (int[] dir: DIRS) {
-            int x = row + dir[0];
-            int y = col + dir[1];
-
-            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] == word.charAt(idx) && !visited[x][y]) {
-               if (dfs(board, word, x, y, idx + 1, visited)) return true;
+        board[i][j] = 0; // 标记访问过
+        for (int[] d : DIRS) {
+            int x = i + d[0];
+            int y = j + d[1]; // 相邻格子
+            if (0 <= x && x < board.length && 0 <= y && y < board[x].length && dfs(x, y, k + 1, board, word)) {
+                return true; // 搜到了！
             }
         }
-
-        visited[row][col] = false;
-        return false;
+        board[i][j] = word[k]; // 恢复现场
+        return false; // 没搜到
     }
 }
+
