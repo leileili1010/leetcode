e8a1087a86b13f87147bdbfa9d8ae553ca828980
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-25 14:23:22 -0800

    Time: 82 ms (51.11%), Space: 49.6 MB (26.5%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,5 +1,10 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // level order bfs
+            // loop each word in a level
+                // loop each letter in a word
+                    // change the letter from a to z, put it in queue if in wordlist
+        
         // corner case 
         Set<String> wordSet = new HashSet<>(wordList);
         if (!wordSet.contains(endWord)) return 0;
@@ -8,16 +13,17 @@ class Solution {
         Deque<String> queue = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
         queue.offer(beginWord);
-        visited.add(beginWord);
+        visited.add(endWord);
 
-        // dfs
-        int count = 1;
+        // bfs
+        int count = 1; // per requirement
         while (!queue.isEmpty()) {
             int n = queue.size();
-           
+
             for (int i = 0; i < n; i++) {
                 String cur = queue.poll();
                 char[] chars = cur.toCharArray();
+
                 for (String next: getNextWords(chars, wordSet)) {
                     if (next.equals(endWord)) return count+1;
                     if (visited.add(next)) {
@@ -30,16 +36,16 @@ class Solution {
         return 0;
     }
 
-    private List<String> getNextWords(char[] chars, Set<String> set) {
+    private List<String> getNextWords(char[] chars, Set<String> wordSet) {
         List<String> res = new ArrayList<>();
-        
+
         for (int i = 0; i < chars.length; i++) {
             char old = chars[i];
             for (char c = 'a'; c <= 'z'; c++) {
                 if (c == old) continue;
                 chars[i] = c;
-                String newStr = new String(chars);
-                if (set.contains(newStr))res.add(newStr);
+                String newWord = new String(chars);
+                if (wordSet.contains(newWord)) res.add(newWord);
             }
             chars[i] = old;
         }
