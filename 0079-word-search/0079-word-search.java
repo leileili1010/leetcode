39e7d6bd17898821ed6bb3ea06dfeaaea516b105
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-25 07:03:02 -0800

    Time: 183 ms (26.84%), Space: 43 MB (54.13%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,56 +1,41 @@
 class Solution {
-    private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-
+    int n;
+    int m;
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
-        char[] w = word.toCharArray();
+        n = board.length;
+        m = board[0].length;
+        if (m * n < word.length()) return false;
+
+        boolean[][] visited = new boolean[n][m];
         
-        // 1. 统计每一个字母在board中出现的频率
-        int[] counts = new int[128];
-        for (char[] row : board) {
-            for (char c : row) counts[c]++;
-        }
-
-        // 2. 如果某一个字母在word中出现的频率大于board中的总个数，return false
-        for (char c : w) {
-            if (--counts[c] < 0) return false;
-        }
-
-        // 3. 搜索方向优化 (可选，但建议口述)
-        // 如果尾部字符在矩阵中更少，反转单词从尾部开始搜
-        // ... (此处可根据时间决定是否实现)
-        if (counts[w[w.length-1]] < counts[w[0]]) {
-            w = new StringBuilder(word).reverse().toString().toCharArray();
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(board, w, i, j, 0)) return true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (findWord(board, word, i, j, 0, visited)) {
+                    return true;
+                }
             }
         }
+
         return false;
     }
 
-    private boolean dfs(char[][] board, char[] word, int i, int j, int k) {
-        if (board[i][j] != word[k]) return false;
-        if (k == word.length - 1) return true;
+    private boolean findWord(char[][] board, String word, int row, int col, int k, boolean[][] visited) {
+        if (board[row][col] != word.charAt(k)) return false; 
+        if (k == word.length()-1) return true;
+        
+        visited[row][col] = true; //只把和word匹配的标记true, 起点要走的四个方向不用标记true，因为有可能这次用不着，找下一个字符能用着
 
-        board[i][j] = 0; // 标记访问
-
-        for (int[] d : DIRS) {
-            int x = i + d[0];
-            int y = j + d[1];
-
-            if (0 <= x && x < board.length && 
-                0 <= y && y < board[0].length &&
-                dfs(board, word, x, y, k + 1))   // <-- 正确递归
-            {
-                return true;
+        for (int[] dir: dirs) { 
+            int x = row + dir[0];
+            int y = col + dir[1];
+             if (0 <= x && x < board.length && 0 <= y && y < board[x].length && !visited[x][y] && findWord(board, word, x, y, k+1, visited)) {
+                return true; // 搜到了！
             }
         }
-
-        board[i][j] = word[k]; // 回溯恢复
-        return false;
+        visited[row][col] = false; // very important step：表面起点不对，这条路走不通，只能回到主函数找下一个起点
+        return false; // very important step
     }
 }
 
