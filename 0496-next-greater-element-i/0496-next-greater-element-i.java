52311ae6381fd0fbaf087d47cf969ba442fe69b9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-24 16:17:38 -0800

    Time: 4 ms (67.84%), Space: 45.8 MB (8.3%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // 单调栈， 单调递减，<num>
+
+        // 1. save num1 index and num in HashMap
+        Map<Integer, Integer> map = new HashMap<>(); // <num, index>
         int n = nums1.length;
+        for (int i = 0; i < n; i++) {
+            map.put(nums1[i], i);
+        } 
+
+        // 2. 
         int[] res = new int[n];
         Arrays.fill(res, -1);
 
-        // nums1 = [2,4], nums2 = [1,2,3,4]
-        // map: (2: 0) (4: 1)
-        // stack: [4 ] 
-        // res [3, -1] 
-        Map<Integer, Integer> map = new HashMap<>(n, 1);
-        for (int i = 0; i < n; i++) {
-            map.put(nums1[i], i);
-        }
-
-        Deque<Integer> stack = new ArrayDeque<>(); // descending order
-        for (int num: nums2) { // num = 4
+        // 3. 
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int num: nums2) {
             while (!stack.isEmpty() && num > stack.peek()) {
-                int top = stack.pop(); // 3
+                int top = stack.pop();
                 res[map.get(top)] = num;
             }
             if (map.containsKey(num)) stack.push(num);
