05f1c12f3fdb5e81ae69f826bfa02d91faccbdb9
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-26 15:00:12 -0800

    Time: 600 ms (22.49%), Space: 47 MB (66.26%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,85 +1,62 @@
-class TrieNode {
-
-    HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
-    String word = null;
-
-    public TrieNode() {}
-}
-
 class Solution {
-
-    char[][] _board = null;
-    ArrayList<String> _result = new ArrayList<String>();
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public List<String> findWords(char[][] board, String[] words) {
-        // Step 1). Construct the Trie
-        TrieNode root = new TrieNode();
+        Trie trie = new Trie();
         for (String word : words) {
-            TrieNode node = root;
-
-            for (Character letter : word.toCharArray()) {
-                if (node.children.containsKey(letter)) {
-                    node = node.children.get(letter);
-                } else {
-                    TrieNode newNode = new TrieNode();
-                    node.children.put(letter, newNode);
-                    node = newNode;
-                }
-            }
-            node.word = word; // store words in Trie
+            trie.insert(word);
         }
 
-        this._board = board;
-        // Step 2). Backtracking starting for each cell in the board
-        for (int row = 0; row < board.length; ++row) {
-            for (int col = 0; col < board[row].length; ++col) {
-                if (root.children.containsKey(board[row][col])) {
-                    backtracking(row, col, root);
-                }
+        Set<String> ans = new HashSet<String>();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                dfs(board, trie, i, j, ans);
             }
         }
 
-        return this._result;
+        return new ArrayList<String>(ans);
     }
 
-    private void backtracking(int row, int col, TrieNode parent) {
-        Character letter = this._board[row][col];
-        TrieNode currNode = parent.children.get(letter);
-
-        // check if there is any match
-        if (currNode.word != null) {
-            this._result.add(currNode.word);
-            currNode.word = null;
+    public void dfs(char[][] board, Trie now, int i1, int j1, Set<String> ans) {
+        if (!now.children.containsKey(board[i1][j1])) {
+            return;
+        }
+        char ch = board[i1][j1];
+        now = now.children.get(ch);
+        if (!"".equals(now.word)) {
+            ans.add(now.word);
         }
 
-        // mark the current letter before the EXPLORATION
-        this._board[row][col] = '#';
-
-        // explore neighbor cells in around-clock directions: up, right, down, left
-        int[] rowOffset = { -1, 0, 1, 0 };
-        int[] colOffset = { 0, 1, 0, -1 };
-        for (int i = 0; i < 4; ++i) {
-            int newRow = row + rowOffset[i];
-            int newCol = col + colOffset[i];
-            if (
-                newRow < 0 ||
-                newRow >= this._board.length ||
-                newCol < 0 ||
-                newCol >= this._board[0].length
-            ) {
-                continue;
-            }
-            if (currNode.children.containsKey(this._board[newRow][newCol])) {
-                backtracking(newRow, newCol, currNode);
+        board[i1][j1] = '#';
+        for (int[] dir : dirs) {
+            int i2 = i1 + dir[0], j2 = j1 + dir[1];
+            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) {
+                dfs(board, now, i2, j2, ans);
             }
         }
+        board[i1][j1] = ch;
+    }
+}
 
-        // End of EXPLORATION, restore the original letter in the board.
-        this._board[row][col] = letter;
+class Trie {
+    String word;
+    Map<Character, Trie> children;
+    boolean isWord;
 
-        // Optimization: incrementally remove the leaf nodes
-        if (currNode.children.isEmpty()) {
-            parent.children.remove(letter);
+    public Trie() {
+        this.word = "";
+        this.children = new HashMap<Character, Trie>();
+    }
+
+    public void insert(String word) {
+        Trie cur = this;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word.charAt(i);
+            if (!cur.children.containsKey(c)) {
+                cur.children.put(c, new Trie());
+            }
+            cur = cur.children.get(c);
         }
+        cur.word = word;
     }
 }
