1bc3e38f3cd60a12c78b46796fe08e38f344ec65
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-26 07:21:48 -0800

    Time: 1 ms (91.87%), Space: 42.6 MB (80.37%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -7,21 +7,29 @@ class Solution {
         if (!dict.contains(endGene)) return -1;
 
         // initialization
-        Deque<String> queue = new ArrayDeque<>();
+        Set<String> startSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
         Set<String> visited = new HashSet<>();
-        queue.offer(startGene);
+        startSet.add(startGene);
+        endSet.add(endGene);
         visited.add(startGene);
+        visited.add(endGene);
 
         // level order BFS
         int steps = 0;
-        while (!queue.isEmpty()) {
-            int n = queue.size();
+        while (!startSet.isEmpty() && !endSet.isEmpty()) {
+            // optimization
+            if (startSet.size() > endSet.size()) {
+                Set<String> temp = startSet;
+                startSet = endSet;
+                endSet = temp;
+            }
+
+            Set<String> nextSet = new HashSet<>();
 
             // enter level
-            for (int i = 0; i < n; i++) {
-                String cur = queue.poll();
-                if (cur.equals(endGene)) return steps;
-                char[] chars = cur.toCharArray();
+            for (String word: startSet) {
+                char[] chars = word.toCharArray();
 
                 for (int j = 0; j < chars.length; j++) {
                     char old = chars[j];
@@ -29,15 +37,16 @@ class Solution {
                         if (c == old) continue;
                         chars[j] = c;
                         String newWord = new String(chars);
-                        // if (newWord.equals(endGene)) return steps + 1;
+                        if (endSet.contains(newWord)) return steps + 1;
                         if (dict.contains(newWord) && visited.add(newWord)) {
-                            queue.offer(newWord);
+                           nextSet.add(newWord);
                         } 
                     }
                     chars[j] = old;
                 }
             }
             // exit level
+            startSet = nextSet;
             steps++;
         }
         return -1;
