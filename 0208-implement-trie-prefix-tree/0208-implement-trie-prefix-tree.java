d3fc36763832cf9c1eed21801a6ebd849fef0031
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-26 19:15:19 -0800

    Time: 30 ms (95.58%), Space: 62 MB (48.74%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,54 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isWord;
+
+    TrieNode() {
+        children = new TrieNode[26];
+        isWord = false;
+    }
+}
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();    
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c: word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) {
+                node.children[c] = new TrieNode();
+            }
+            node = node.children[c];
+        }
+        node.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        return find(word) == 2;
+    }
+    
+    public boolean startsWith(String prefix) {
+        return find(prefix) != 0;
+    }
+
+    private int find(String word) {
+        TrieNode node = root;
+        for (char c: word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) return 0;
+            node = node.children[c]; 
+        }
+        return node.isWord? 2: 1; // 2-match word, 1-match prefix
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
