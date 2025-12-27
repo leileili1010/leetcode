afb6b52e6dddbd4c0362c09f9ab0b435ea18c2e4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-27 07:40:03 -0800

    Time: 0 ms (100%), Space: 43.1 MB (62.74%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -16,14 +16,21 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        dfs(root, res);
-        return res;
-    }
+        if (root == null) return res; 
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offer(root);
 
-    private void dfs(TreeNode root, List<Integer> res) {
-        if (root == null) return;
-        res.add(root.val);
-        dfs(root.left, res);
-        dfs(root.right, res);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
     }
 }
