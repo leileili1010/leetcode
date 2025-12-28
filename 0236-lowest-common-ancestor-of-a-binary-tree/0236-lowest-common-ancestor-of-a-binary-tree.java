4e224a65bbe36118c2ebd16b2151d18a33c492a2
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-28 12:09:10 -0800

    Time: 7 ms (97.31%), Space: 45.9 MB (95.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,18 +8,26 @@
  * }
  */
 class Solution {
+    private TreeNode res;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root; // 找到 p 或 q 就不往下递归了，原因见上面答疑
+        dfs(root, p, q);
+        return res;    
+    }
+
+    private boolean dfs(TreeNode node, TreeNode p, TreeNode q) {
+        // base case 
+        if (node == null) return false;
+        if (res != null) return false;
+
+        boolean left = dfs(node.left, p, q);
+        boolean right = dfs(node.right, p, q);
+        boolean cur = node == p || node == q;
+
+        if (left && cur || right && cur || left && right) {
+            res = node;
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) { // 左右都找到
-            return root; // 当前节点是最近公共祖先
-        }
-        // 如果只有左子树找到，就返回左子树的返回值
-        // 如果只有右子树找到，就返回右子树的返回值
-        // 如果左右子树都没有找到，就返回 null（注意此时 right = null）
-        return left != null ? left : right;
+
+        return left || right || cur;
     }
 }
