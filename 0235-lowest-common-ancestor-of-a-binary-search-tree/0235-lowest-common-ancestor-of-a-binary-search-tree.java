c731f628ce43adfb2cd80a1bb11e4a5a8a36d17f
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-28 10:28:37 -0800

    Time: 6 ms (93.69%), Space: 47.7 MB (28.3%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,19 +9,12 @@
  */
 
 class Solution {
-     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-
-        if (p.val > root.val && q.val > root.val) {
-            // Both nodes are in the left subtree
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (p.val < root.val && q.val < root.val) {
-            // Both nodes are in the right subtree
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int x = root.val;
+        if (p.val > x && q.val > x) {
+            return lowestCommonAncestor(root.right, p, q); 
+        } else if (p.val < x && q.val < x) {
             return lowestCommonAncestor(root.left, p, q);
-        } else {
-            // One node is on the left, the other is on the right
-            // OR one of them is equal to root → this is the LC
-            return root;
-        }
+        } else return root;
     }
 }
