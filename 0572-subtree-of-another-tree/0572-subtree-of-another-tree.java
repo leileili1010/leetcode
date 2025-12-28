cf45dd4e06b5378ac5a646de4b47278087912a1c
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-27 20:56:27 -0800

    Time: 2 ms (99.05%), Space: 46.4 MB (43.95%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,17 +15,41 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null) return true;
-        if (root == null) return false;
-
-        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        int hs = getHeight(subRoot);
+        return dfs(root, subRoot, hs).getValue();
     }
 
-    private boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
+    // 代码逻辑同 104. 二叉树的最大深度
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftH = getHeight(root.left);
+        int rightH = getHeight(root.right);
+        return Math.max(leftH, rightH) + 1;
+    }
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    // 100. 相同的树
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q; // 必须都是 null
+        }
+        return p.val == q.val &&
+               isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
+    // 返回 node 的高度，以及是否找到了 subRoot
+    private Pair<Integer, Boolean> dfs(TreeNode node, TreeNode subRoot, int hs) {
+        if (node == null) {
+            return new Pair<>(0, false);
+        }
+        Pair<Integer, Boolean> left = dfs(node.left, subRoot, hs);
+        Pair<Integer, Boolean> right = dfs(node.right, subRoot, hs);
+        if (left.getValue() || right.getValue()) {
+            return new Pair<>(0, true);
+        }
+        int nodeH = Math.max(left.getKey(), right.getKey()) + 1;
+        return new Pair<>(nodeH, nodeH == hs && isSameTree(node, subRoot));
     }
 }
