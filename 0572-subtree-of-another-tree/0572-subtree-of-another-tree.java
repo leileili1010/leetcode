7278151037a5d542846ee24f2d5ddef5466258bf
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-27 16:23:18 -0800

    Time: 2 ms (99.05%), Space: 46.4 MB (43.95%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,21 +15,17 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
-        }
-        return isSameTree(root, subRoot) ||
-               isSubtree(root.left, subRoot) ||
-               isSubtree(root.right, subRoot);
+        if (subRoot == null) return true;
+        if (root == null) return false;
+
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    // 100. 相同的树
     private boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == q; // 必须都是 null
-        }
-        return p.val == q.val &&
-               isSameTree(p.left, q.left) &&
-               isSameTree(p.right, q.right);
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
