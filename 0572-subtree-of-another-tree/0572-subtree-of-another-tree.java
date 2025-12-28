e335281e02801cf87616a1a68d94bab1f4acd3ca
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-27 16:18:54 -0800

    Time: 2 ms (99.05%), Space: 46.3 MB (79.92%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,18 +15,12 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        int hs = getHeight(subRoot);
-        return dfs(root, subRoot, hs).getValue();
-    }
-
-    // 代码逻辑同 104. 二叉树的最大深度
-    private int getHeight(TreeNode root) {
         if (root == null) {
-            return 0;
+            return false;
         }
-        int leftH = getHeight(root.left);
-        int rightH = getHeight(root.right);
-        return Math.max(leftH, rightH) + 1;
+        return isSameTree(root, subRoot) ||
+               isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
     }
 
     // 100. 相同的树
@@ -37,19 +31,5 @@ class Solution {
         return p.val == q.val &&
                isSameTree(p.left, q.left) &&
                isSameTree(p.right, q.right);
-    }
-
-    // 返回 node 的高度，以及是否找到了 subRoot
-    private Pair<Integer, Boolean> dfs(TreeNode node, TreeNode subRoot, int hs) {
-        if (node == null) {
-            return new Pair<>(0, false);
-        }
-        Pair<Integer, Boolean> left = dfs(node.left, subRoot, hs);
-        Pair<Integer, Boolean> right = dfs(node.right, subRoot, hs);
-        if (left.getValue() || right.getValue()) {
-            return new Pair<>(0, true);
-        }
-        int nodeH = Math.max(left.getKey(), right.getKey()) + 1;
-        return new Pair<>(nodeH, nodeH == hs && isSameTree(node, subRoot));
     }
 }
