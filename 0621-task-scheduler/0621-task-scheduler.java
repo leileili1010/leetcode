ae61ef8f8b687e278d8bffbfd579eb3c57e34f9d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 06:44:26 -0800

    Time: 20 ms (60.2%), Space: 48.7 MB (6.17%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,27 +1,31 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        // 1. calculate freq for each tasks
         int[] freq = new int[26];
-        for (char c: tasks) freq[c-'A']++;
+        for (char task: tasks) freq[task-'A']++; 
 
+        // 2. maxHeap<idx of freq> by freq
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> (b - a));
-        for (int num: freq) {
-            if (num > 0) maxHeap.offer(num);
-        }   
+        for (int f: freq) {
+            if (f > 0) maxHeap.offer(f);
+        }
 
+        // 3. simulation
         int time = 0;
         while (!maxHeap.isEmpty()) {
+            // for the current cycle
             int cycle = n + 1;
             int taskCount = 0;
             List<Integer> store = new ArrayList<>();
 
             while (cycle > 0 && !maxHeap.isEmpty()) {
-                int task = maxHeap.poll();
-                taskCount++;
+                int taskFreq = maxHeap.poll();
+                if (--taskFreq  > 0) store.add(taskFreq);
                 cycle--;
-                if (--task > 0) store.add(task);
+                taskCount++;
             }
 
-            for (int num: store) maxHeap.offer(num);
+            maxHeap.addAll(store);
 
             time += maxHeap.isEmpty()? taskCount: n+1;
         }
