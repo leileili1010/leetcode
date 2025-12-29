87c0963587bb074a4085f1087165ea3dc894ed34
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 13:12:19 -0800

    Time: 1 ms (99.64%), Space: 47 MB (25.35%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,28 +10,40 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        // dummy and cur
-        // put head of each linkedlist into minHeap
-        // pull top
-            // if head.next is not null, put head.next to minHeap
-            // cur.next = top
-            // cur = cur.next;
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> (a.val - b.val));
+        int m = lists.length;
+        if (m == 0) return null;
 
-        for (ListNode node: lists) {
-           if (node != null) minHeap.offer(node);
-        } 
+        // i: 每一组合并中左边的index
+        // i+step: 每一组合并中右边的下标
+        // i += step*2： 下一组合并中左边的下标
+        // [[.0..] [.1..] [.2..] [.3..] [.4..]]
+        // [[.0.1.] [...] [.2.3.] [...] [.4..]]
+        // [[.0.2.] [...] [...] [...] [.4..]]
 
+      for (int step = 1; step < m; step *= 2) {
+        for (int i = 0; i < m - step; i += step * 2) {
+            lists[i] = mergeTwoLists(lists[i], lists[i+step]);
+        }
+      }
+      return lists[0];
+    }
+
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
 
-        while (!minHeap.isEmpty()) {
-            ListNode top = minHeap.poll();
-            if (top.next != null) minHeap.offer(top.next);
-            cur.next = top;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
             cur = cur.next;
         }
 
-        return dummy.next;
+        cur.next = list1 != null? list1: list2;
+        return dummy.next; 
     }
 }
