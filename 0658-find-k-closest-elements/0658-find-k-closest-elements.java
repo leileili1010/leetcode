8cb66240b27aa2c8248d8b86cdbcd0857e1b64ff
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 14:58:58 -0800

    Time: 4 ms (98.16%), Space: 47.9 MB (44.71%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,36 +1,17 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int right = binarySearch(arr, x);
-        int left = right - 1;
-        while (k-- > 0) {
-            if (left < 0) {
-                right++;
-            } else if (right >= arr.length) {
-                left--;
-            } else if (x - arr[left] <= arr[right] - x) {
-                left--;
-            } else {
-                right++;
-            }
-        }
-        List<Integer> ans = new ArrayList<Integer>();
-        for (int i = left + 1; i < right; i++) {
-            ans.add(arr[i]);
-        }
-        return ans;
-    }
-
-    public int binarySearch(int[] arr, int x) {
-        int low = 0, high = arr.length - 1;
+        int low = 0, high = arr.length - k;
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] >= x) {
-                high = mid;
-            } else {
+            // 比较窗口左边界外的一个元素和右边界内的一个元素
+            if (x - arr[mid] > arr[mid + k] - x) {
                 low = mid + 1;
+            } else {
+                high = mid;
             }
         }
-        return low;
+        List<Integer> res = new ArrayList<>();
+        for (int i = low; i < low + k; i++) res.add(arr[i]);
+        return res;
     }
 }
-
