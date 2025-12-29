d8ded476161e9ab3bebeef88e50354cbbefa74a6
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 15:01:26 -0800

    Time: 14 ms (35.73%), Space: 48 MB (31%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,25 +1,32 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        // Initialize binary search bounds
-        int left = 0;
-        int right = arr.length - k;
-        
-        // Binary search against the criteria described
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (x - arr[mid] > arr[mid + k] - x) {
-                left = mid + 1;
+        List<Integer> res = new ArrayList<>();
+        int n = arr.length;
+        int left = findInsert(arr, x); // the biggest number <= x
+        int right = left + 1;
+
+        for (int i = 0; i < k && i < n; i++) {
+            if (left >= 0 && (right >= n || x-arr[left] <= arr[right]-x)) {
+                res.add(0, arr[left--]);
+            } else {
+                res.add(arr[right++]);
+            }
+        }
+        return res;
+    }
+
+    private int findInsert(int[] arr, int x) {
+        int left = 0, right = arr.length-1;
+
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] <= x) {
+                left = mid;
             } else {
                 right = mid;
             }
         }
-        
-        // Create output in correct format
-        List<Integer> result = new ArrayList<Integer>();
-        for (int i = left; i < left + k; i++) {
-            result.add(arr[i]);
-        }
-        
-        return result;
+        return left;
     }
 }
