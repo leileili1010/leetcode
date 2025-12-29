1fd5ae1043ce3012af7aba4f85191d118c2714bb
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 15:34:06 -0800

    Time: 1 ms (99.64%), Space: 46.9 MB (25.35%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,22 +10,32 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) return null;
+
+        for (int step = 1; step < n; step *= 2) {
+            for (int i = 0; i < n-step; i += step * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i+step]);
+            }
+        }
+        return lists[0];
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
 
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> (a.val - b.val));
-
-        for (ListNode node: lists) {
-            if (node != null) minHeap.offer(node);
-        }
-
-        while (!minHeap.isEmpty()) {
-            ListNode top = minHeap.poll();
-            cur.next = top;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
             cur = cur.next;
-            if (top.next != null) minHeap.offer(top.next);
         }
-
+        cur.next = l1 != null? l1: l2;
         return dummy.next;
     }
 }
