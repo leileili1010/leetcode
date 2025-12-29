959d9476f97848be0ed07b28bef6be303876a530
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 15:26:28 -0800

    Time: 4 ms (78.74%), Space: 46.7 MB (74.99%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,40 +10,22 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int m = lists.length;
-        if (m == 0) return null;
-
-        // i: 每一组合并中左边的index
-        // i+step: 每一组合并中右边的下标
-        // i += step*2： 下一组合并中左边的下标
-        // [[.0..] [.1..] [.2..] [.3..] [.4..]]
-        // [[.0.1.] [...] [.2.3.] [...] [.4..]]
-        // [[.0.2.] [...] [...] [...] [.4..]]
-
-      for (int step = 1; step < m; step *= 2) {
-        for (int i = 0; i < m - step; i += step * 2) {
-            lists[i] = mergeTwoLists(lists[i], lists[i+step]);
-        }
-      }
-      return lists[0];
-    }
-
-    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
 
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                cur.next = list1;
-                list1 = list1.next;
-            } else {
-                cur.next = list2;
-                list2 = list2.next;
-            }
-            cur = cur.next;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> (a.val - b.val));
+
+        for (ListNode node: lists) {
+            if (node != null) minHeap.offer(node);
         }
 
-        cur.next = list1 != null? list1: list2;
-        return dummy.next; 
+        while (!minHeap.isEmpty()) {
+            ListNode top = minHeap.poll();
+            cur.next = top;
+            cur = cur.next;
+            if (top.next != null) minHeap.offer(top.next);
+        }
+
+        return dummy.next;
     }
 }
