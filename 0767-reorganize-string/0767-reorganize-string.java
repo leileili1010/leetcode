ba524c2ae09109f4775f1737ed0473a69bae3992
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-28 23:43:26 -0800

    Time: 5 ms (39.37%), Space: 43.8 MB (7.26%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,37 +1,32 @@
 class Solution {
     public String reorganizeString(String s) {
         int[] freq = new int[26];
-        for (char c : s.toCharArray()) freq[c - 'a']++;
+        for (char c: s.toCharArray()) freq[c-'a']++;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> b[1] - a[1]
-        );
-
-        for (int i = 0; i < 26; i++) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> (b[1] - a[1]));
+        for (int i= 0; i < 26; i++) {
             if (freq[i] > 0) {
-                pq.offer(new int[]{i, freq[i]});
+                maxHeap.offer(new int[] {i, freq[i]});
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        int[] prev = null; // 上一个字符（暂存，避免相邻）
+        while (!maxHeap.isEmpty()) {
+            int cycle = 2;
+            List<int[]> store = new ArrayList<>();
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-
-            sb.append((char)(cur[0] + 'a'));
-            cur[1]--;
-
-            // 上一个字符可以重新入堆
-            if (prev != null && prev[1] > 0) {
-                pq.offer(prev);
+            while (cycle > 0 && !maxHeap.isEmpty()) {
+                int[] cur = maxHeap.poll();
+                char c = (char)(cur[0] + 'a');
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) return "";
+                sb.append(c);
+                if (--cur[1] > 0) store.add(cur);
+                cycle--;
             }
 
-            // 当前字符成为新的 prev
-            prev = cur;
+            for (int[] node: store) maxHeap.offer(node);
         }
 
-        // 如果最后还有剩余，说明无解
-        return sb.length() == s.length() ? sb.toString() : "";
+        return sb.toString();
     }
 }
