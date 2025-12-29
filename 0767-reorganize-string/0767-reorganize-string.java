aaef7478fbc208d6ac2488dfabd9ccad58383cc4
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 07:16:43 -0800

    Time: 3 ms (73.55%), Space: 43.1 MB (51.47%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,33 +1,33 @@
 class Solution {
     public String reorganizeString(String s) {
         int[] freq = new int[26];
-        for (char c: s.toCharArray()) freq[c-'a']++;
+        for (char c : s.toCharArray()) freq[c - 'a']++;
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> (b[1] - a[1]));
-        for (int i= 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                maxHeap.offer(new int[] {i, freq[i]});
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> b[1] - a[1]
+        );
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) pq.offer(new int[]{i, freq[i]});
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!maxHeap.isEmpty()) {
-            int cycle = 2;
-            List<int[]> store = new ArrayList<>();
+        int[] prev = null;
 
-            while (cycle > 0 && !maxHeap.isEmpty()) {
-                int[] cur = maxHeap.poll();
-                char c = (char)(cur[0] + 'a');
-                if (sb.length() > 0 && sb.charAt(sb.length()-1) == c) return "";
-                sb.append(c);
-                if (--cur[1] > 0) store.add(cur);
-                cycle--;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            sb.append((char)(cur[0] + 'a'));
+            cur[1]--;
+
+            if (prev != null && prev[1] > 0) {
+                pq.offer(prev);
             }
 
-            if (store.size() > 0) maxHeap.addAll(store);
+            prev = cur;
         }
 
-        return sb.toString();
+        return sb.length() == s.length() ? sb.toString() : "";
     }
 }
+
 
