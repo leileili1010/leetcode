d8d84732611a9ef62cf8e5344e04dc8d805de452
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 10:43:56 -0800

    Time: 20 ms (60.2%), Space: 48.6 MB (6.17%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,33 +1,38 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        // 1. calculate freq for each tasks
-        int[] freq = new int[26];
-        for (char task: tasks) freq[task-'A']++; 
+        // 1. calc freq for each letter
+        // 2. put all freq in maxheap
+        // 3. simulation
+            // while maxheap is not empty
+                // cycle = n + 1
+                // taskCount
+                // list to store processed tasks, put in maxHeap if > 0
+                // time += maxHeap is empty? taskCount: n+1
 
-        // 2. maxHeap<idx of freq> by freq
+        int[] freq = new int[26];
+        for (char task: tasks) freq[task - 'A']++;
+
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> (b - a));
         for (int f: freq) {
-            if (f > 0) maxHeap.offer(f);
+            if (f > 0) maxHeap.add(f);
         }
 
-        // 3. simulation
         int time = 0;
         while (!maxHeap.isEmpty()) {
-            // for the current cycle
             int cycle = n + 1;
             int taskCount = 0;
             List<Integer> store = new ArrayList<>();
 
             while (cycle > 0 && !maxHeap.isEmpty()) {
-                int taskFreq = maxHeap.poll();
-                if (--taskFreq  > 0) store.add(taskFreq);
+                int task = maxHeap.poll();
+                if (--task > 0) store.add(task);
                 cycle--;
                 taskCount++;
             }
 
-            maxHeap.addAll(store);
-
-            time += maxHeap.isEmpty()? taskCount: n+1;
+            if (store.size() > 0) maxHeap.addAll(store);
+            
+            time += maxHeap.isEmpty()? taskCount: n + 1; 
         }
         return time;
     }
