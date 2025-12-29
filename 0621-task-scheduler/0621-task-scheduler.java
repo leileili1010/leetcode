20ab8c18a9d0d5b8ddbc0b5d449badf5c5f68e95
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-28 22:48:39 -0800

    Time: 7 ms (65.03%), Space: 47.7 MB (90.45%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,15 +1,22 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
-        
-        for (int c: tasks) freq[c-'A']++;
-        int maxFreq = Arrays.stream(freq).max().getAsInt();
-        
-        int maxCount = 0;
-        for (int f: freq) {
-            if (f == maxFreq) maxCount++;
+        for (char c: tasks) {
+            freq[c-'A']++;
         }
+        Arrays.sort(freq);
 
-        return Math.max(tasks.length, (maxFreq-1) * (n+1) + maxCount);
+        int time = 0;
+        while (freq[25] > 0) {//每一轮都会重新sort, 如果最大的freq都是0，说明tasks已经全部做完了
+            int i = 0;
+            while (i < n+1 && freq[25] > 0) {
+                int idx = 25-i;
+                if (idx >= 0 && freq[idx] > 0) freq[idx]--;
+                time++;
+                i++;
+            }   
+            Arrays.sort(freq);
+        }
+        return time;
     }
 }
