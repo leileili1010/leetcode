21f6a583c4e075c05c4d459ee6446f39a7e14663
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 23:45:11 -0800

    Time: 1 ms (87.82%), Space: 45.5 MB (42.77%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> path = Arrays.asList(new Integer[nums.length]); // 所有排列的长度都是一样的 n
-        boolean[] onPath = new boolean[nums.length];
-        dfs(0, nums, ans, path, onPath);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, res, new ArrayList<>(), new boolean[nums.length]);
+        return res;
     }
 
-    // 枚举 path[i] 填 nums 的哪个数
-    private void dfs(int i, int[] nums, List<List<Integer>> ans, List<Integer> path, boolean[] onPath) {
-        if (i == nums.length) {
-            ans.add(new ArrayList<>(path));
+    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] visited) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
             return;
         }
-        for (int j = 0; j < nums.length; j++) {
-            if (!onPath[j]) {
-                path.set(i, nums[j]); // 从没有选的数字中选一个
-                onPath[j] = true; // 已选上
-                dfs(i + 1, nums, ans, path, onPath);
-                onPath[j] = false; // 恢复现场
-                // 注意 path 无需恢复现场，因为排列长度固定，直接覆盖就行
-            }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            list.add(nums[i]);
+            dfs(nums, res, list, visited);
+            visited[i] = false;
+            list.remove(list.size()-1);
         }
     }
 }
