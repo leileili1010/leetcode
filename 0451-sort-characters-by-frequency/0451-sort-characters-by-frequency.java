6729fdd1a07385f5d18f97d1550a94e77e18b643
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 22:06:18 -0800

    Time: 4 ms (98.46%), Space: 46.7 MB (50.6%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,24 +1,29 @@
 class Solution {
     public String frequencySort(String s) {
-        int[] freq = new int[128];
-        for (char c: s.toCharArray()) {
-            freq[c]++;
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) {
+            count[c]++;
         }
-        
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b) -> freq[b] - freq[a]); 
 
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] > 0) {
-                maxHeap.offer((char)i);
+        List<Character>[] buckets = new List[s.length() + 1];
+        for (int i = 0; i < 128; i++) {
+            int freq = count[i];
+            if (freq > 0) {
+                if (buckets[freq] == null) {
+                    buckets[freq] = new ArrayList<>();
+                }
+                buckets[freq].add((char) i);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!maxHeap.isEmpty()) {
-            char c = maxHeap.poll();
-
-            for (int i = 0; i < freq[c]; i++) {
-                sb.append(c);
+        for (int f = buckets.length - 1; f > 0; f--) {
+            if (buckets[f] != null) {
+                for (char c : buckets[f]) {
+                    for (int i = 0; i < f; i++) {
+                        sb.append(c);
+                    }
+                }
             }
         }
         return sb.toString();
