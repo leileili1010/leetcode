c4a787bace06c5425286773cf0f0d984f9122868
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-30 07:41:25 -0800

    Time: 1 ms (58.18%), Space: 43.6 MB (39.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,20 +1,24 @@
 class Solution {
+    private static final String[] MAPPING = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
     public List<String> letterCombinations(String digits) {
-        String[] cells = {null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits == null || digits.length() == 0) return List.of();
+
         List<String> res = new ArrayList<>();
-        dfs(digits, cells, res, new StringBuilder(), 0);
+        dfs(digits, res, new StringBuilder(), 0);
         return res;
     }
     
-    private void dfs(String digits, String[] cells, List<String> res, StringBuilder sb, int k) {
+    private void dfs(String digits, List<String> res, StringBuilder sb, int k) {
         if (sb.length() == digits.length()) {
             res.add(sb.toString());
             return;
         }
         
-        for (char c: cells[digits.charAt(k)-'0'].toCharArray()) {
+        String letters = MAPPING[digits.charAt(k)-'0'];
+        for (char c: letters.toCharArray()) {
             sb.append(c);
-            dfs(digits, cells, res, sb, k+1);
+            dfs(digits, res, sb, k+1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
