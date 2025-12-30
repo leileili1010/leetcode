385cb7632517f5033556bd09eb61ccee9ec77792
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 22:06:26 -0800

    Time: 19 ms (20.46%), Space: 49.3 MB (5.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,31 +1,35 @@
 class Solution {
     public String frequencySort(String s) {
-        int[] count = new int[128];
-        for (char c : s.toCharArray()) {
-            count[c]++;
-        }
-
-        List<Character>[] buckets = new List[s.length() + 1];
-        for (int i = 0; i < 128; i++) {
-            int freq = count[i];
-            if (freq > 0) {
-                if (buckets[freq] == null) {
-                    buckets[freq] = new ArrayList<>();
-                }
-                buckets[freq].add((char) i);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int f = buckets.length - 1; f > 0; f--) {
-            if (buckets[f] != null) {
-                for (char c : buckets[f]) {
-                    for (int i = 0; i < f; i++) {
-                        sb.append(c);
-                    }
-                }
-            }
-        }
-        return sb.toString();
+        
+    if (s == null || s.isEmpty()) return s;
+    
+    // Count up the occurances.
+    Map<Character, Integer> counts = new HashMap<>();
+    for (char c : s.toCharArray()) {
+        counts.put(c, counts.getOrDefault(c, 0) + 1);
     }
+    
+    int maximumFrequency = Collections.max(counts.values());
+    
+    // Make the list of buckets and apply bucket sort.
+    List<List<Character>> buckets = new ArrayList<>();
+    for (int i = 0; i <= maximumFrequency; i++) {
+        buckets.add(new ArrayList<Character>());
+    }
+    for (Character key : counts.keySet()) {
+        int freq = counts.get(key);
+        buckets.get(freq).add(key);
+    }
+
+    // Build up the string. 
+    StringBuilder sb = new StringBuilder();
+    for (int i = buckets.size() - 1; i >= 1; i--) {
+        for (Character c : buckets.get(i)) {
+            for (int j = 0; j < i; j++) {
+                sb.append(c);
+            }
+        }
+    }
+    return sb.toString();
+}
 }
