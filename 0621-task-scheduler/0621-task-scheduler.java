c8be93453761deef4a8c6aff83da91891b9f99c3
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 19:14:46 -0800

    Time: 4 ms (81.99%), Space: 48.5 MB (11.26%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,18 +1,18 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         // 1. calculate freq for each tasks
-        // 2. maxFreq and maxChar
-        // 3. formular
+        // 2. maxFreq and maxCount
+        // 3. return Math.max((maxFreq - 1)*(n+1) + 3, tasks.length);
 
         int[] freq = new int[26];
-        for (char task: tasks) freq[task-'A']++;
+        for (char task: tasks) freq[task - 'A']++;
 
         int maxFreq = Arrays.stream(freq).max().getAsInt();
-        int maxCount = 0; 
-        for (int i = 0; i < 26; i++) {
-            if (maxFreq == freq[i]) maxCount++;
+        int maxCount = 0;
+        for (int f: freq) {
+            if (f == maxFreq) maxCount++;
         }
 
-        return Math.max(tasks.length, (maxFreq-1) * (n+1) + maxCount);
+        return Math.max(tasks.length, (maxFreq-1)*(n+1) + maxCount);
     }
 }
