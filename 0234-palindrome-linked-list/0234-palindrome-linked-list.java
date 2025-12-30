3e949341e903c7805ddf9a9a2bfcacca650f70f8
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-29 21:34:20 -0800

    Time: 6 ms (44.1%), Space: 94.3 MB (67.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,39 +10,38 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode mid = middleNode(head);
+        ListNode mid = findMiddleNode(head);
         ListNode head2 = reverseList(mid);
+
         while (head2 != null) {
-            if (head.val != head2.val) { // 不是回文链表
-                return false;
-            }
+            if (head.val != head2.val) return false;
             head = head.next;
             head2 = head2.next;
         }
         return true;
     }
 
-    // 876. 链表的中间结点
-    private ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow;
+
+        return slow; // 如果是偶数个节点，停留在后半部分第一个点
     }
 
-    // 206. 反转链表
     private ListNode reverseList(ListNode head) {
-        ListNode pre = null;
+        ListNode prev = null;
         ListNode cur = head;
+
         while (cur != null) {
-            ListNode nxt = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = nxt;
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
-        return pre;
+        return prev;
     }
 }
