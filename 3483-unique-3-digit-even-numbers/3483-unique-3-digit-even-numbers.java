d39b594d807124df268bbec9541f50bcb70f4de5
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-31 15:43:41 -0800

    Time: 6 ms (50.46%), Space: 46.4 MB (53.86%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,25 +1,30 @@
 class Solution {
+    private boolean[] used;
+    private Set<Integer> set;
+
     public int totalNumbers(int[] digits) {
-        Set<Integer> set = new HashSet<>();
-        int n = digits.length;
-        for (int i = 0; i < n; i++) { // 个位数
-            int a = digits[i];
-            if (a % 2 > 0) {
-                continue;
-            }
-            for (int j = 0; j < n; j++) { // 十位数
-                if (j == i) {
-                    continue;
-                }
-                for (int k = 0; k < n; k++) { // 百位数
-                    int c = digits[k];
-                    if (c == 0 || k == i || k == j) {
-                        continue;
-                    }
-                    set.add(c * 100 + digits[j] * 10 + a);
-                }
-            }
-        }
+        used = new boolean[digits.length];
+        set = new HashSet<>();
+        dfs(digits, 0, 0);
         return set.size();
+    }
+
+    private void dfs(int[] digits, int pos, int num) {
+        if (pos == 3) {
+            set.add(num);
+            return;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            if (used[i]) continue;
+            int digit = digits[i];
+            
+            if (pos == 0 && digit == 0) continue;
+            if (pos == 2 && digit % 2 != 0) continue;
+
+            used[i] = true;
+            dfs(digits, pos+1, num * 10 + digit);
+            used[i] = false;
+        }
     }
 }
