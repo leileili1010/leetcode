83eb01a92633d2d5f10a5e89ae500a195d9f4e6c
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-31 07:13:14 -0800

    Time: 7 ms (90.98%), Space: 47.1 MB (47.78%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,13 +1,13 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int n = customers.length();
-        int[] prefixSum = new int[n+1];
+        int penality = 0;
 
-        int max = 0, res = 0;
+        int minPenality = 0, res = 0;
         for (int i = 0; i < n; i++) {
-            prefixSum[i+1] = prefixSum[i] + (customers.charAt(i) == 'Y'? 1: -1);
-            if (prefixSum[i+1] > max) {
-                max = prefixSum[i+1];
+            penality = penality + (customers.charAt(i) == 'N'? 1: -1);
+            if (penality < minPenality) {
+                minPenality = penality;
                 res = i+1;
             }
         }
