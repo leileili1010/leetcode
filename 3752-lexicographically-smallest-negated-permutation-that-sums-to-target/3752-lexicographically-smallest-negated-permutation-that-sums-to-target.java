ddd640f879e5c3cc9658f302a5560f1c3ad1277e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-31 15:59:43 -0800

    Time: 3 ms (99.74%), Space: 163.5 MB (37.44%) - LeetHub

--- 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.java
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] lexSmallestNegatedPerm(int n, long target) {
-        long mx = (long) n * (n + 1) / 2;
-        if (Math.abs(target) > mx || (mx - target) % 2 != 0) {
+        long maxSum = (long) n * (n + 1) / 2;
+        if (Math.abs(target) > maxSum || (maxSum - target) % 2 != 0) {
             return new int[]{};
         }
-        long negS = (mx - target) / 2; // 取负号的元素（的绝对值）之和
+        long negSum = (maxSum - target) / 2; // 计算出所有需要变成负数的数之和
 
         int[] ans = new int[n];
         int l = 0;
         int r = n - 1;
-        // 从 1,2,...,n 中选一些数，元素和等于 negS
+        // 从 1,2,...,n 中选一些数，元素和等于 negSum
         // 为了让负数部分的字典序尽量小，从大往小选
         for (int x = n; x > 0; x--) {
-            if (negS >= x) {
-                negS -= x;
+            if (negSum >= x) {
+                negSum -= x; 
                 ans[l++] = -x;
             } else {
                 // 大的正数填在末尾
