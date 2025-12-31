9b02de7ef20853a80266b6db75669defeb42875f
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-31 11:23:16 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,44 +1,44 @@
 class Solution {
     public int numMagicSquaresInside(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int ans = 0;
-
-        for (int i = 0; i <= m - 3; i++) {
-            for (int j = 0; j <= n - 3; j++) {
-                if (isMagic(grid, i, j)) {
-                    ans++;
-                }
-            }
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                if (isMagic(grid, i, j)) res++;
+            } 
         }
-        return ans;
+        return res;
     }
 
-    private boolean isMagic(int[][] g, int r, int c) {
-        // 1️⃣ 中心必须是 5
-        if (g[r + 1][c + 1] != 5) return false;
+    private boolean isMagic(int[][] grid, int r, int c) {
+        // check center is 5
+        if (grid[r+1][c+1] != 5) return false;
 
-        // 2️⃣ 数字必须是 1~9 且不重复
+        // check none repeating
         boolean[] seen = new boolean[10];
         for (int i = r; i < r + 3; i++) {
             for (int j = c; j < c + 3; j++) {
-                int x = g[i][j];
-                if (x < 1 || x > 9 || seen[x]) return false;
+                int x = grid[i][j];
+                if (seen[x]) return false;
                 seen[x] = true;
             }
         }
 
-        // 3️⃣ 只检查前两行
+        // check sumRow = 15;
         for (int i = 0; i < 2; i++) {
-            int sum = g[r + i][c] + g[r + i][c + 1] + g[r + i][c + 2];
+            int sum = grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2];
             if (sum != 15) return false;
         }
 
-        // 4️⃣ 只检查前两列
+        // check sumCol = 15
         for (int j = 0; j < 2; j++) {
-            int sum = g[r][c + j] + g[r + 1][c + j] + g[r + 2][c + j];
+            int sum = grid[r][c+j] + grid[r+1][c+j] + grid[r+2][c+j];
             if (sum != 15) return false;
         }
-
         return true;
     }
+
+
 }
