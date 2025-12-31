56a1c9a8dec66da5ee20614d35c7365d2b4912bc
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-30 22:00:50 -0800

    Time: 2 ms (35.1%), Space: 47.2 MB (72.21%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int ans = 0;
-        int curRight = 0; // 已建造的桥的右端点
-        int nextRight = 0; // 下一座桥的右端点的最大值
-        for (int i = 0; i < nums.length - 1; i++) {
-            // 遍历的过程中，记录下一座桥的最远点
+        int res = 0;
+        int curRight = 0;
+        int nextRight = 0;
+
+        for (int i= 0; i < nums.length; i++) {
+            if (curRight >= nums.length-1) return res;
             nextRight = Math.max(nextRight, i + nums[i]);
-            if (i == curRight) { // 无路可走，必须建桥
-                curRight = nextRight; // 建桥后，最远可以到达 next_right
-                ans++;
-                // if (curRight >= nums.length-1) return ans;
+            if (i == curRight) {
+                res++;
+                curRight = nextRight;
             }
-        }
-        return ans;
+            
+        } 
+        return res;
+        
     }
 }
