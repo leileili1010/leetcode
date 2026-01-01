4961118e3c38e4846cfde8370537dcff33cee622
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-01 08:30:07 -0800

    Time: 11 ms (64.42%), Space: 43.6 MB (85.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,23 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
-        Set<Character> set = new HashSet<>();
+        Map<Character, Character> map = new HashMap<>();
+        boolean[] used = new boolean[128];
 
         for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            
-            if (!map.containsKey(c1)) {
-                if (set.contains(c2)) {
-                    return false;
-                }
-                map.put(c1, c2);
-                set.add(c2);
-            } else {
-                if (map.get(c1) != c2) {
-                    return false;
-                }
-            }
+
+            if (map.containsKey(c1) && map.get(c1) == c2) continue;
+            if (map.containsKey(c1) || used[c2]) return false;
+
+            map.put(c1, c2);
+            used[c2] = true;
         }
         return true;
     }
