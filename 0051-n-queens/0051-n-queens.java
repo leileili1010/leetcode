3823032fcdcf870d80b83177420459385484c55d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-01 13:10:37 -0800

    Time: 1 ms (99.84%), Space: 46.4 MB (90.14%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,36 +1,47 @@
 class Solution {
+    boolean[] col, posDiag, negDiag;
+    List<List<String>> res;
+    char[][] board;
+
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        int[] queens = new int[n]; // 皇后放在 (r,queens[r])
-        boolean[] col = new boolean[n];
-        boolean[] diag1 = new boolean[n * 2 - 1];
-        boolean[] diag2 = new boolean[n * 2 - 1];
-        dfs(0, queens, col, diag1, diag2, ans);
-        return ans;
+        col = new boolean[n];
+        posDiag = new boolean[2 * n];
+        negDiag = new boolean[2 * n];
+        res = new ArrayList<>();
+        board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(0, n);
+        return res;
     }
 
-    private void dfs(int r, int[] queens, boolean[] col, boolean[] diag1, boolean[] diag2, List<List<String>> ans) {
-        int n = col.length;
+    private void backtrack(int r, int n) {
         if (r == n) {
-            List<String> board = new ArrayList<>(n); // 预分配空间
-            for (int c : queens) {
-                char[] row = new char[n];
-                Arrays.fill(row, '.');
-                row[c] = 'Q';
-                board.add(new String(row));
+            List<String> copy = new ArrayList<>();
+            for (char[] row : board) {
+                copy.add(new String(row));
             }
-            ans.add(board);
+            res.add(copy);
             return;
         }
-        // 在 (r,c) 放皇后
         for (int c = 0; c < n; c++) {
-            int rc = r - c + n - 1;
-            if (!col[c] && !diag1[r + c] && !diag2[rc]) { // 判断能否放皇后
-                queens[r] = c; // 直接覆盖，无需恢复现场
-                col[c] = diag1[r + c] = diag2[rc] = true; // 皇后占用了 c 列和两条斜线
-                dfs(r + 1, queens, col, diag1, diag2, ans);
-                col[c] = diag1[r + c] = diag2[rc] = false; // 恢复现场
+            if (col[c] || posDiag[r + c] || negDiag[r - c + n]) {
+                continue;
             }
+            col[c] = true;
+            posDiag[r + c] = true;
+            negDiag[r - c + n] = true;
+            board[r][c] = 'Q';
+
+            backtrack(r + 1, n);
+
+            col[c] = false;
+            posDiag[r + c] = false;
+            negDiag[r - c + n] = false;
+            board[r][c] = '.';
         }
     }
 }
