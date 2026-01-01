62a740b626ff8d0ff6afee10e42f440504b44036
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2025-12-31 16:14:11 -0800

    Time: 3 ms (99.74%), Space: 163.3 MB (39.79%) - LeetHub

--- 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.java
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[] lexSmallestNegatedPerm(int n, long target) {
-        long maxSum = (long) n * (n + 1) / 2;
-        if (Math.abs(target) > maxSum || (maxSum - target) % 2 != 0) {
-            return new int[]{};
-        }
-        long negSum = (maxSum - target) / 2; // 计算出所有需要变成负数的数之和
+        // n = 3 1, 2, 3
+        // sum = target
+        // n = 4, target = 2
+        // [1, 2, 3, 4]
+        // [-4, 1,  2, 3]
+        // maxSum = 10, -10
+        // posS + negS = maxSum = 10
+        // posS - negS = target
+        // 2 * negS = maxSum - target
+        
+        // corner cases
+        // 1) Math.abs(target) > maxSum,
+        // 2)  (maxSum - target) % 2 == 0
 
-        int[] ans = new int[n];
-        int l = 0;
-        int r = n - 1;
-        // 从 1,2,...,n 中选一些数，元素和等于 negSum
-        // 为了让负数部分的字典序尽量小，从大往小选
+
+        long maxSum = (long)n * (n + 1) / 2;
+        if (Math.abs(target) > maxSum || (maxSum - target) % 2 != 0) return new int[]{};
+
+        int[] res = new int[n];
+        int left = 0;
+        int right = n-1;
+        long negSum =  (maxSum - target) / 2;
+
+        // x ~ [1, n]
         for (int x = n; x > 0; x--) {
             if (negSum >= x) {
-                negSum -= x; 
-                ans[l++] = -x;
+                res[left++] = -x;
+                negSum -= x;
             } else {
-                // 大的正数填在末尾
-                ans[r--] = x;
+                res[right--] = x;
             }
         }
-        return ans;
+        return res;
     }
 }
