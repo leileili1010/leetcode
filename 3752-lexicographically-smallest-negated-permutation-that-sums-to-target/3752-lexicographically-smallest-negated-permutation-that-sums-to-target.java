8adaa4935390e3ad530efd0b8d2eef1f7750713e
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-01 18:35:47 -0800

    Time: 3 ms (99.75%), Space: 163.5 MB (37.56%) - LeetHub

---
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] lexSmallestNegatedPerm(int n, long target) {
+        // n = 3 1, 2, 3
+        // sum = target
+        // n = 4, target = 2
+        // [1, 2, 3, 4]
+        // [-4, 1,  2, 3]
+        // maxSum = 10, -10
+        // posS + negS = maxSum = 10
+        // posS - negS = target
+        // 2 * negS = maxSum - target
+        
+        // corner cases
+        // 1) Math.abs(target) > maxSum,
+        // 2)  (maxSum - target) % 2 == 0
+
+        // corner cases
+        long maxSum = (long)n * (n + 1) / 2;
+        if (Math.abs(target) > maxSum || (maxSum - target) % 2 != 0) return new int[]{};
+
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        long negSum = (maxSum - target) / 2;
+
+        for (int x = n; x > 0; x--) {
+            if (negSum >= x) {
+                res[left++] = -x;
+                negSum -= x;  
+            } else {
+                res[right--] = x;
+            }
+        }
+        return res;
+    }
+}
