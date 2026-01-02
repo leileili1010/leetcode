297857c720cabe128f66834565d5817670ed3b0d
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-01 17:16:06 -0800

    Time: 1 ms (99.84%), Space: 46.7 MB (26.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,49 +1,45 @@
 class Solution {
-    boolean[] col, posDiag, negDiag;
-    List<List<String>> res;
-    char[][] board;
+    private boolean[] col, posDiag, negDiag;
 
     public List<List<String>> solveNQueens(int n) {
         col = new boolean[n];
         posDiag = new boolean[2 * n];
         negDiag = new boolean[2 * n];
-        res = new ArrayList<>();
-        board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
-            }
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (char[] row: board) {
+            Arrays.fill(row, '.');
         }
-        backtrack(0, n);
+        backtrack(0, n, res, board);
         return res;
+
     }
 
-    private void backtrack(int r, int n) {
+    private void backtrack(int r, int n, List<List<String>> res, char[][] board) {
         if (r == n) {
-            List<String> copy = new ArrayList<>();
-            for (char[] row : board) {
-                copy.add(new String(row));
+            List<String> list = new ArrayList<>();
+            for (char[] row: board) {
+                list.add(new String(row));
             }
-            res.add(copy);
-            return;
+            res.add(list);
         }
+
         for (int c = 0; c < n; c++) {
-            if (col[c] || posDiag[r + c] || negDiag[r - c + n]) {
-                continue;
-            }
+            if (col[c] || posDiag[r+c] || negDiag[r-c+n]) continue;
+
             col[c] = true;
-            posDiag[r + c] = true;
-            negDiag[r - c + n] = true;
+            posDiag[r+c] = true;
+            negDiag[r-c+n] = true;
             board[r][c] = 'Q';
 
-            backtrack(r + 1, n);
+            backtrack(r+1, n, res, board);
 
             col[c] = false;
-            posDiag[r + c] = false;
-            negDiag[r - c + n] = false;
+            posDiag[r+c] = false;
+            negDiag[r-c+n] = false;
             board[r][c] = '.';
         }
     }
+        
 }
-
-
