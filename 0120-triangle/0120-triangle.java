d3d9b48315678fa0ca9f22d23056fc5022534e15
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-02 10:41:33 -0800

    Time: 3 ms (83.27%), Space: 45.3 MB (84.88%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,22 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][];
+        int[] dp = new int[n];
 
-        // state: dp[i][j] from triangle[i][j] to bottom shortest path
-        for (int i = 0; i < n; i++) {
-            dp[i] = new int[i+1];
-        }
-
+        // state: dp[i] from triangle[i][j] to bottom shortest path
         // init: dp[n-1] row
         for (int j = 0; j < n; j++) {
-            dp[n-1][j] = triangle.get(n-1).get(j);
+            dp[j] = triangle.get(n-1).get(j);
         }
 
         // function
         // dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1])
         for (int i = n-2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
