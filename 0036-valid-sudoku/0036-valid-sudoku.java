8c169155e977cb144513fe7e40aa6933d45ffa78
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-02 13:34:02 -0800

    Time: 1 ms (99.77%), Space: 46.4 MB (68.29%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rowHas = new boolean[9][9]; // row i has num x
-        boolean[][] colHas = new boolean[9][9]; // col j has num x
-        boolean[][][] squareHas = new boolean[3][3][9]; // box [i][j] has num x
+        boolean[][] rowHas = new boolean[9][9]; // rowHas[i][x]: 第i行是否有x
+        boolean[][] colHas = new boolean[9][9]; // colHas[j][x]: 第j列是否有x
+        boolean[][][] squareHas = new boolean[3][3][9]; // square[i][j][x] square[i][j]是否有x
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
-                int x  = c - '1';
+                int x = c - '1'; // [1, 9] ==> [0, 8]
 
-                if (board[i][j] == '.') continue;
+                if (c == '.') continue;
 
                 if (rowHas[i][x] || colHas[j][x] || squareHas[i/3][j/3][x]) return false;
-                rowHas[i][x] = colHas[j][x] = squareHas[i/3][j/3][x] = true;
+
+                rowHas[i][x] = true;
+                colHas[j][x] = true;
+                squareHas[i/3][j/3][x] = true;
             }
         }
         return true;
