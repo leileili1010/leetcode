0dd8bc59a690d5ca101278403b88ce735db7e3cc
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-03 07:55:43 -0800

    Time: 0 ms (100%), Space: 42.6 MB (85.58%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -17,8 +17,7 @@ class Solution {
     // preorder
     public boolean isSameTree(TreeNode p, TreeNode q) {
         // base case (3)
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
+        if (p == null || q == null) return p == q;
         if (p.val != q.val) return false;
 
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
