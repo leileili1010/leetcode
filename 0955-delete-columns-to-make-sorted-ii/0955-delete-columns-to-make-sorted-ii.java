a6a65f9134b5af16960aa712f3b5cb3b471fc9d6
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-02 20:48:57 -0800

    Time: 0 ms (100%), Space: 43.6 MB (77.32%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -2,23 +2,33 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int n = strs.length;
         int m = strs[0].length();
-        String[] a = new String[n]; // 最终得到的字符串数组
-        Arrays.fill(a, "");
+        int size = n - 1;
+        int[] checkList = new int[size];
+        for (int i = 0; i < size; i++) {
+            checkList[i] = i;
+        }
 
         int ans = 0;
         next:
         for (int j = 0; j < m; j++) {
-            for (int i = 0; i < n - 1; i++) {
-                if ((a[i] + strs[i].charAt(j)).compareTo(a[i + 1] + strs[i + 1].charAt(j)) > 0) {
+            for (int t = 0; t < size; t++) {
+                int i = checkList[t];
+                if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
                     // j 列不是升序，必须删
                     ans++;
                     continue next;
                 }
             }
             // j 列是升序，不删更好
-            for (int i = 0; i < n; i++) {
-                a[i] += strs[i].charAt(j);
+            int newSize = 0;
+            for (int t = 0; t < size; t++) {
+                int i = checkList[t];
+                if (strs[i].charAt(j) == strs[i + 1].charAt(j)) {
+                    // 相邻字母相等，下一列 i 和 i+1 需要继续比大小
+                    checkList[newSize++] = i; // 原地覆盖
+                }
             }
+            size = newSize;
         }
         return ans;
     }
