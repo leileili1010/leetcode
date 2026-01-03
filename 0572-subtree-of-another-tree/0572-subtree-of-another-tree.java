e5dd5dd6bae8854aad411cf94c3520c4c3a0f619
Fiona Li <143532361+leileili1010@users.noreply.github.com>
2026-01-03 08:09:28 -0800

    Time: 1 ms (99.84%), Space: 46.7 MB (7.87%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Result {
+    int height;
+    boolean found;
+
+    Result(int height, boolean found) {
+        this.height = height;
+        this.found = found;
+    }
+}
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        int hs = getHeight(subRoot); // hs = 2
+
+        return dfs(root, subRoot, hs).found;
+    }
+
+    private Result dfs(TreeNode node, TreeNode subRoot, int hs) { // O(M*N), O(N + kM) ~ O(N)
+        if (node == null) return new Result(0, false);
+
+        Result left = dfs(node.left, subRoot, hs);
+        Result right = dfs(node.right, subRoot, hs);
+
+        if (left.found || right.found) return new Result(0, true);
+
+        int nodeH = Math.max(left.height, right.height) + 1;
+
+        return new Result(nodeH, nodeH == hs && isSameTree(node, subRoot));
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == q;
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
